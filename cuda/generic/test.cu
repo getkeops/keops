9aec7e16540fe2238426df3ea137a9f81b5d81f3
Joan Alexis Glaunes <alexis.glaunes@parisdescartes.fr>
2017-11-14 12:42:35 +0100

    Replace test.cu

--- cuda/generic/test.cu
+++ cuda/generic/test.cu
@@ -4,12 +4,12 @@
 //		nvcc -std=c++11 -o test test.cu
 
  
-#define __TYPE__ float
-#define __DIMPOINT__ 3 
-#define __DIMVECT__ 3 
+#define __TYPE__ double
+#define __DIMPOINT__ 3
+#define __DIMVECT__ 3
 #define KERNEL SCALARRADIAL 	// type of kernel. Others are VARSURF (varifolds surfaces) and NCSURF (for normal cycles surfaces)
-#define EVAL sEval 		// type of convolution for the type of kernel. For scalar radial kernels, others are sGrad1, sGrad, sHess, sDiff 
-#define RADIALFUN GaussFunction 	// Others are GaussFunction, Sum4GaussFunction, Sum4CauchyFunction
+#define EVAL sEval 	// type of convolution for the type of kernel. For scalar radial kernels, others are sGrad1, sGrad, sHess, sDiff 
+#define RADIALFUN GaussFunction 	// Others are CauchyFunction, Sum4GaussFunction, Sum4CauchyFunction
 
 
 
@@ -24,7 +24,7 @@
 int main()
 {
 			
-    int Nx=10000, Ny=10000;
+    int Nx=5000, Ny=5000;
 
 	
     typedef typename KER::EVAL::DIMSX DIMSX;   
@@ -37,6 +37,7 @@ int main()
     __TYPE__ *y[SIZEY];
 
 	vector< vector<__TYPE__> > vx(SIZEX), vy(SIZEY);
+	vector<__TYPE__> resgpu(Nx*DIMSX::VAL(0)), rescpu(Nx*DIMSX::VAL(0));
 	
 	// create random inputs
 	for(int k=0; k<SIZEX; k++)
@@ -44,7 +45,7 @@ int main()
 		vx[k].resize(Nx*DIMSX::VAL(k));	
 		x[k] = vx[k].data();	
 		for(int i=0; i<Nx*DIMSX::VAL(k); i++)
-			x[k][i] = ((__TYPE__)rand())/RAND_MAX;
+			x[k][i] = ((__TYPE__)rand())/RAND_MAX-.5;
 	}			
 	
 	for(int k=0; k<SIZEY; k++)
@@ -52,13 +53,14 @@ int main()
 		vy[k].resize(Ny*DIMSY::VAL(k));
 		y[k] = vy[k].data();
 		for(int i=0; i<Ny*DIMSY::VAL(k); i++)
-			y[k][i] = ((__TYPE__)rand())/RAND_MAX;
+			y[k][i] = ((__TYPE__)rand())/RAND_MAX-.5;
 	}
 
 	// set GPU device number
     int deviceID = 0;
     
     clock_t begin, end;
+    __TYPE__ s;
     
     begin = clock();
 	cudaSetDevice(deviceID);
@@ -71,26 +73,29 @@ int main()
 	
 	// compute
 	begin = clock();
-	GpuConv2D(KER(RadialFun()),funeval,Nx,Ny,x[0],x[1],y[0],y[1]);
+	GpuConv2D(KER(RadialFun()),funeval,Nx,Ny,x,y);
 	end = clock();
 	cout << "time for GPU computation (first run) : " << double(end - begin) / CLOCKS_PER_SEC << endl;
-	
-	// display sum of output values
-	__TYPE__ s = 0;
-	for(int i=0; i<Nx*DIMSX::VAL(0); i++)
-		s += x[0][i];
-	cout << "sum=" << s << endl;
-	
+		
 	begin = clock();
-	GpuConv2D(KER(RadialFun()),funeval,Nx,Ny,x[0],x[1],y[0],y[1]);
+	GpuConv2D(KER(RadialFun()),funeval,Nx,Ny,x,y);
 	end = clock();
 	cout << "time for GPU computation (second run) : " << double(end - begin) / CLOCKS_PER_SEC << endl;
+	
+	resgpu = vx[0];
+	
+	begin = clock();
+	CpuConv(KER(RadialFun()),funeval,Nx,Ny,x,y);
+	end = clock();
+	cout << "time for CPU computation : " << double(end - begin) / CLOCKS_PER_SEC << endl;
 
-	// display sum of output values
+	rescpu = vx[0];
+	
+	// display sum of errors
 	s = 0;
 	for(int i=0; i<Nx*DIMSX::VAL(0); i++)
-		s += x[0][i];
-	cout << "sum=" << s << endl;
+		s += abs(resgpu[i]-rescpu[i]);
+	cout << "mean abs error =" << s/Nx << endl;
 
 }
 
