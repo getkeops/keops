9c7d253ef6c07109c8da0193d1ac2040e728feab
Joan Alexis Glaunes <alexis.glaunes@parisdescartes.fr>
2017-12-12 00:27:18 +0100

    Update kernel_library.h

--- cuda/generic/autodiff/kernel_library.h
+++ cuda/generic/autodiff/kernel_library.h
@@ -16,7 +16,8 @@ template < class OOS2, class R2 >
 using EnergyFunction = Inv<Powf<Add<Inv<Constant<OOS2>>,R2>,IntInv<4>>>;
 
 template < int DIMPOINT, int DIMVECT >
-using GaussKernel = ScalarRadialKernel<GaussFunction,DIMPOINT,DIMVECT>;
+using GaussKernel_ = ScalarRadialKernel<GaussFunction,DIMPOINT,DIMVECT>;
+// remark : GaussKernel is already defined with different template arguments ; this one should replace the first
 
 template < int DIMPOINT, int DIMVECT >
 using LaplaceKernel = ScalarRadialKernel<LaplaceFunction,DIMPOINT,DIMVECT>;
@@ -25,9 +26,37 @@ template < int DIMPOINT, int DIMVECT >
 using EnergyKernel = ScalarRadialKernel<EnergyFunction,DIMPOINT,DIMVECT>;
 
 
-// Matrix-valued kernels
+// Matrix-valued kernels : implementations from Micheli/Glaunes paper
 
-//template < template<class,class> class FORTHO, template<class,class> class FTILDE, int DIMPOINT, int DIMVECT >
-//using TRI_Kernel = Add<Scal<FORTHO<Param<0>,SqDist<X<0,DIMPOINT>,Y<1,DIMPOINT>>>,X<2,DIMVECT>>,Scal<Scal<FTILDE<Param<1>,SqDist<X<0,DIMPOINT>,Y<1,DIMPOINT>>>,Scalprod<X<2,DIMVECT>,Subtract<X<0,DIMPOINT>,Y<1,DIMPOINT>>>>,Subtract<X<0,DIMPOINT>,Y<1,DIMPOINT>>>;
+// TRI kernel - general form :
+// k(x,y)b = k_ortho(r2)b + k_tilde(r2)<b,x-y>(x-y),   where r2=|x-y|^2
+// which gives the formula below. Drawback : x-y and r2 are computed several times when calling Eval()
+// Is there a way to avoid this by detecting several Eval() calls for the same class ??
+template < template<class,class> class FORTHO, template<class,class> class FTILDE, int DIM >
+using TRI_Kernel = Add<Scal<FORTHO<Param<0>,SqDist<X<0,DIM>,Y<1,DIM>>>,X<2,DIM>>,
+	Scal<Scal<FTILDE<Param<1>,SqDist<X<0,DIM>,Y<1,DIM>>>,Scalprod
+			<X<2,DIM>,Subtract<X<0,DIM>,Y<1,DIM>>>>,
+		Subtract<X<0,DIM>,Y<1,DIM>>>>;
 
-//DivFreeFunctionOrtho = F<OOS2,R2>::DiffT
+
+// Div-free and curl-free kernel with gaussian functions. 
+// k_df(x,y)b = exp(-r^2/s2)*(((d-1)/(2c)-r^2)b + <b,x-y>(x-y))
+// k_cf(x,y)b = exp(-r^2/s2)*(	   (1/(2c)) b   - <b,x-y>(x-y))
+// We do not use the previous template because exp(-r^2/s2) is factorized
+
+template < int DIM >
+using DivFreeGaussKernel = 
+Scal<GaussFunction<P<0>,SqDist<X<0,DIM>,Y<1,DIM>>>,
+Add<Scal<Subtract<
+Divide<IntConstant<DIM-1>,Scal<IntConstant<2>,Constant<P<0>>>>,
+SqDist<X<0,DIM>,Y<1,DIM>>>,Y<2,DIM>>,
+Scal<Scalprod<Y<2,DIM>,Subtract<X<0,DIM>,Y<1,DIM>>>,Subtract<X<0,DIM>,Y<1,DIM>>>>>;
+
+template < int DIM >
+using CurlFreeGaussKernel = 
+Scal<GaussFunction<P<0>,SqDist<X<0,DIM>,Y<1,DIM>>>,
+Subtract<Scal<
+Divide<IntConstant<1>,Scal<IntConstant<2>,Constant<P<0>>>>,
+Y<2,DIM>>,
+Scal<Scalprod<Y<2,DIM>,Subtract<X<0,DIM>,Y<1,DIM>>>,Subtract<X<0,DIM>,Y<1,DIM>>>>>;
+
