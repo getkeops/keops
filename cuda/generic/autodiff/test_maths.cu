0980ce999b46bb0779392e055e1a1875a75eda1e
Jean Feydy <jean.feydy@gmail.com>
2017-12-12 18:04:05 +0100

    Trying to fix a C++11 + nvcc7.5 compiling error on Ubuntu 16.04.

---
+++ cuda/generic/autodiff/test_maths.cu
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <assert.h>
+#include <cuda.h>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
+
+int main(int argc, char **argv)
+{
+    float A_1 = 1./( std::pow(1,2) * std::exp(1.0));
+    return A_1;
+}
+
