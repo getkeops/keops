05405292b660a435017642731a2ba114317dd604
Jean Feydy <jean.feydy@gmail.com>
2017-12-14 23:03:12 +0100

    Implemented the generic backward (nothing tested yet).

--- cuda/generic/autodiff/python_bindings/pytorch/kernel_product_generic.py
+++ cuda/generic/autodiff/python_bindings/pytorch/kernel_product_generic.py
@@ -56,7 +56,7 @@ class GenericKernelProduct(torch.autograd.Function):
 		With the values defined above,
 		```
 		genconv = GenericKernelProduct().apply
-		R = genconv( aliases, formula, signature, C, X, Y, U, V, B )
+		R = genconv( aliases, formula, signature, 0, C, X, Y, U, V, B )
 		```
 		is a legal call, where :
 		- C is a scalar              (torch Variable)
@@ -94,242 +94,67 @@ class GenericKernelProduct(torch.autograd.Function):
 		ctx.sum_index = sum_index
 		
 		# Get the size nx by looping on the signature until we've found an "x_i" ----------------
-		nx = -1
+		n = -1
 		for ( index, (dim,cat) ) in enumerate(signature[1:]) : # Omit the output
-			if cat == 0 :
-				nx = len( args[index] ) # Lengths compatibility is done by cuda_conv_generic
+			if cat == sum_index :
+				n = len( args[index] ) # Lengths compatibility is done by cuda_conv_generic
 				break
-		if nx == -1 : raise ValueError("The signature should contain at least on indexing argument x_i.")
+		if n == -1 and sum_index == 0: raise ValueError("The signature should contain at least one indexing argument x_i.")
+		if n == -1 and sum_index == 1: raise ValueError("The signature should contain at least one indexing argument y_j.")
 		
 		# Data Conversion (only CPU via numpy implented at the moment) --------------------------
 		
 		args_conv = [ arg.numpy() for arg in args]
 		
-	                  
 		# Actual computation --------------------------------------------------------------------
-		result  = torch.zeros( nx * signature[0][0] ).type(dtype) # Init the output of the convolution
-		cuda_conv_generic(formula, result, *args_conv,            # Inplace CUDA routine
-		                  aliases = aliases, sum_index = sum_index,
+		result  = torch.zeros( n * signature[0][0] ).type(dtype) # Init the output of the convolution
+		cuda_conv_generic(formula, result, *args_conv,           # Inplace CUDA routine
+		                  aliases   = aliases, sum_index   = sum_index,
 		                  cuda_type = "float", grid_scheme = "2D") 
-		result  = result.view( nx, signature[0][0] )
+		result  = result.view( n, signature[0][0] )
 		return result
 	
 	@staticmethod
-	def backward(ctx, a):
-		""" Backward scheme: 
-			given a dual output vector "a_i" represented by a N-by-D array (i from 1 to N),
-			outputs :
-			- \partial_s K(s,x,y,b) . a, which is a float number (NOT IMPLEMENTED YET)
-			- \partial_x K(s,x,y,b) . a, which is a N-by-D array
-			- \partial_y K(s,x,y,b) . a, which is a M-by-D array
-			- \partial_b K(s,x,y,b) . a, which is a M-by-E array, equal to K(s,y,x,a).		
+	def backward(ctx, G):
 		"""
-		(ss, xx, yy, bb) = ctx.saved_variables # Unwrap the saved variables
-		kernel = ctx.kernel
-		# In order to get second derivatives, we encapsulated the cudagradconv.cuda_gradconv
-		# routine in another torch.autograd.Function object:
-		kernelproductgrad_x = KernelProductGrad_x().apply 
-		
-		# Compute \partial_s K(s,x,y,b) . a   -------------NOT IMPLEMENTED YET-------------------
-		grad_s = None
-		
-		# Compute \partial_x K(s,x,y,b) . a   --------------------------------------------------- 
-		# We're looking for the gradient with respect to x of
-		# < a, K(s,x,y,b) >  =  \sum_{i,j} f_s( |x_i-y_j|^2 ) < a_i, b_j >
-		# kernelproductgrad_x computes the gradient, with respect to the 3rd variable x, of trace(
-		grad_x = kernelproductgrad_x( ss, a,  #     a^T
-								      xx, yy, #   @ K(x,y)
-								      bb,     #   @ b )
-									  kernel)
-		# Compute \partial_y K(s,x,y,b) . a   --------------------------------------------------- 
-		# We're looking for the gradient with respect to y of
-		# < a, K(s,x,y,b) >  =  \sum_{i,j} f_s( |x_i-y_j|^2 ) < a_i, b_j >
-		# Thanks to the symmetry in (x,a) and (y,b) of the above formula,
-		# we can use the same cuda code as the one that was used for grad_x:
-		# kernelproductgrad_x computes the gradient, with respect to the 3rd variable y, of trace(
-		grad_y = kernelproductgrad_x( ss, bb, #     b^T
-								      yy, xx, #   @ K(y,x)
-								      a,      #   @ a )
-									  kernel) 
-		# Compute \partial_b K(s,x,y,b) . a   --------------------------------------------------- 
-		Kt = KernelProduct().apply # Will be used to compute the kernel "transpose"
-		grad_b = Kt(    ss,      # We use the same kernel scale
-		                yy, xx,  # But we compute K_yx, instead of K_xy
-		                a,       # And multiply it with a         
-		                kernel)
-		return (grad_s, grad_x, grad_y, grad_b, None)
-
-class KernelProductGrad_x(torch.autograd.Function):
-	""" This class implements the gradient of the above operator
-		'KernelProduct' with respect to its second variable, 'x'.
-		If:
-		- s   is a scale   ("sigma")
-		- a_i is an N-by-E array  (i from 1 to N)
-		- x_i is an N-by-D array  (i from 1 to N)
-		- y_j is an M-by-D array  (j from 1 to M)
-		- b_j is an M-by-E array  (j from 1 to M)
-		
-		Then:
-		KernelProductGrad_x( s, a, x, y, b) is an N-by-D array,
-		whose i-th line is given by
-		KernelProduct( s, a, x, y, b)_i = \sum_j f_s'( |x_i-y_j|^2 ) * < a_i, b_j> * 2(x_i-y_j).
-		
-		This class wasn't designed to be used by end-users, but to provide the second derivatives
-		of the operator 'KernelProduct', encoded in KernelProductGrad_x's backwars operator.
-		
-		f is a real-valued function which encodes the kernel operation:
-		
-		                 k_s(x,y)  =  f_s( |x_i-y_j|^2 )
-		
-		Computations are performed with CUDA on the GPU, using the 'libkp' files.                 
-		This code was designed for memory efficiency: the kernel matrix is computed "one tile
-		after another", and never stored in memory, be it on the GPU or on the RAM.
-		
-		N.B.: f and its derivatives f', f'' are hardcoded in the 'libkp/kernels.cx' file.
-		
-		Author: Jean Feydy
-	"""
-	
-	@staticmethod
-	def forward(ctx, s, a, x, y, b, kernel):
-		""" 
-		KernelProduct(s, a, x, y, b)_i = \sum_j f_s'( |x_i-y_j|^2 ) * < a_i, b_j> * 2(x_i-y_j).
+		Backward scheme.
+		G has the same shape (and thus, dim-cat signature) as the formula's output.
 		"""
-		# save everything to compute the gradient
-		#ctx.ss = s ; ctx.aa = a ; ctx.xx = x # TOO NAIVE!
-		#ctx.yy = y ; ctx.bb = b              # We should save variables explicitly
-		# N.B.: relying on the "ctx.saved_variables" attribute is necessary
-		#       if you want to be able to differentiate the output of the backward
-		#       once again. It helps pytorch to keep track of "who is who".
-		#       As we haven't implemented the "3rd" derivative of KernelProduct,
-		#       this formulation is not strictly necessary here... 
-		#       But I think it is good practice anyway.
-		ctx.save_for_backward( s, a, x, y, b )   # Call at most once in the "forward".
-		ctx.kernel = kernel
+		aliases   = ctx.aliases
+		formula   = ctx.formula
+		signature = ctx.signature
+		sum_index = ctx.sum_index
+		args      = ctx.saved_variables # Unwrap the saved variables
 		
-		# init grad_x which contains the output
-		grad_x = torch.zeros(x.numel()).type(dtype) #0d array
-		# We're looking for the gradient with respect to x of
-		# < a, K(s,x,y,b) >  =  \sum_{i,j} f_s( |x_i-y_j|^2 ) < a_i, b_j >
-		# Cudagradconv computes the gradient, with respect to x, of trace(
-		cudagrad1conv.cuda_grad1conv( a.numpy(),            #     a^T
-								      x.numpy(), y.numpy(), #   @ K(x,y)
-								      b.numpy(),            #   @ b )
-								      grad_x.numpy(),       # Output array
-								      s.numpy(),            # Kernel scale parameter
-								      kernel = kernel)
-		grad_x = grad_x.view(x.shape)
+		# Compute the number of arguments which are not parameters
+		nargs = 0 ; 
+		for sig in signature[1:] : 
+			if sig[1] != 2 : nargs += 1
 		
-		return grad_x
-	
-	@staticmethod
-	def backward(ctx, e):
-		""" Backward scheme: 
-			given a dual output vector "e_i" represented by a N-by-D array (i from 1 to N),
-			outputs :
-			- \partial_s Kx(s,a,x,y,b) . e, which is a float number (NOT IMPLEMENTED YET)
-			- \partial_a Kx(s,a,x,y,b) . e, which is a N-by-E array
-			- \partial_x Kx(s,a,x,y,b) . e, which is a N-by-D array
-			- \partial_y Kx(s,a,x,y,b) . e, which is a M-by-D array
-			- \partial_b Kx(s,a,x,y,b) . e, which is a M-by-E array, equal to K(s,y,x,a).		
-		"""
-		(ss, aa, xx, yy, bb) = ctx.saved_variables # Unwrap the saved variables
-		kernel = ctx.kernel
+		# If formula takes 5 arguments (numbered from 0 to 4), then the gradient
+		# wrt. the output, G, should be given as a 6-th variable (numbered 5),
+		# with the same dim-cat as the formula's output. 
+		eta   = "Var<"+str(nargs)+","+str(signature[0][0])+","+str(signature[0][1])+">"
+		grads = [] ; ind = 0
+		for sig in signature[1:] :
+			if sig[1] == 2 : # we're referring to a parameter
+				grads.append(None)
+			else :
+				# adding new aliases is waaaaay too dangerous if we want to compute
+				# second derivatives, etc. So we make explicit references to Var<ind,dim,cat> instead.
+				var         = "Var<"+str(ind)+","+str(sig[0])+","+str(sig[1])+">"
+				formula_g   = "Grad< "+ formula +","+ var +","+ eta +">"
+				signature_g = [ sig ] + signature[1:] + signature[:1]
+				sumindex_g  = 1 - sig[1]
+				args_g      = args + [G]
+				
+				# N.B.: if I understand PyTorch's doc, we should redefine this function every time we use it?
+				genconv = GenericKernelProduct().apply  
+				grads.append( genconv( aliases, formula_g, signature_g, sumindex_g, *args_g )  )
+				ind += 1 # increment the variable count
 		
-		
-		
-		# Compute \partial_s Kx(s,a,x,y,b) . e   ------------NOT IMPLEMENTED YET-----------------
-		grad_xs = None
-		
-		# Compute \partial_a Kx(s,a,x,y,b) . e   ------------------------------------------------ 
-		# We're looking for the gradient with respect to a of
-		#
-		# < e, K(s,a,x,y,b) >  =  \sum_{i,j} f_s'( |x_i-y_j|^2 ) * < a_i, b_j > * 2 < e_i, x_i-y_j>,
-		#
-		# which is an N-by-E array g_i (i from 1 to N), where each line is equal to
-		#
-		# g_i  =  \sum_j 2* f_s'( |x_i-y_j|^2 ) * < e_i, x_i-y_j> * b_j
-		#
-		# This is what cuda_gradconv_xa is all about:
-		
-		grad_xa = torch.zeros( aa.numel() ).type(dtype) #0d array
-		cudagradgradconv.cuda_gradconv_xa( e.data.numpy(),
-										   aa.data.numpy(),
-										   xx.data.numpy(), yy.data.numpy(),
-										   bb.data.numpy(),
-										   grad_xa.numpy(),  # Output array
-										   ss.data.numpy(),
-										   kernel = kernel ) 
-		grad_xa  = Variable(grad_xa.view( aa.size()[0], aa.size()[1] ))
-		
-		# Compute \partial_x Kx(s,a,x,y,b) . e   ------------------------------------------------ 
-		# We're looking for the gradient with respect to x of
-		#
-		# < e, K(s,a,x,y,b) >  =  \sum_{i,j} f_s'( |x_i-y_j|^2 ) * < a_i, b_j > * 2 < e_i, x_i-y_j>,
-		#
-		# which is an N-by-D array g_i (i from 1 to N), where each line is equal to
-		#
-		# g_i  =  2* \sum_j < a_i, b_j > * [                       f_s'(  |x_i-y_j|^2 ) * e_i
-		#                                  + 2* < x_i-y_j, e_i > * f_s''( |x_i-y_j|^2 ) * (x_i-y_j) ]
-		#
-		# This is what cuda_gradconv_xx is all about:
-		
-		grad_xx = torch.zeros( xx.numel() ).type(dtype) #0d array
-		cudagradgradconv.cuda_gradconv_xx(  e.data.numpy(),
-										   aa.data.numpy(),
-										   xx.data.numpy(), yy.data.numpy(),
-										   bb.data.numpy(),
-										   grad_xx.numpy(),  # Output array
-										   ss.data.numpy(),
-										   kernel = kernel  ) 
-		grad_xx  = Variable(grad_xx.view( xx.size()[0], xx.size()[1] ))
-		
-		# Compute \partial_y Kx(s,a,x,y,b) . e   ------------------------------------------------ 
-		# We're looking for the gradient with respect to y of
-		#
-		# < e, K(s,a,x,y,b) >  =  \sum_{i,j} f_s'( |x_i-y_j|^2 ) * < a_i, b_j > * 2 < e_i, x_i-y_j>,
-		#
-		# which is an M-by-D array g_j (j from 1 to M), where each line is equal to
-		#
-		# g_j  = -2* \sum_i < a_i, b_j > * [                       f_s'(  |x_i-y_j|^2 ) * e_i
-		#    "don't forget the -2 !"       + 2* < x_i-y_j, e_i > * f_s''( |x_i-y_j|^2 ) * (x_i-y_j) ]
-		#
-		# This is what cuda_gradconv_xy is all about:
-		
-		grad_xy = torch.zeros( yy.numel() ).type(dtype) #0d array
-		cudagradgradconv.cuda_gradconv_xy(  e.data.numpy(),
-										   aa.data.numpy(),
-										   xx.data.numpy(), yy.data.numpy(),
-										   bb.data.numpy(),
-										   grad_xy.numpy(),  # Output array
-										   ss.data.numpy(),
-										   kernel = kernel ) 
-		grad_xy  = Variable(grad_xy.view( yy.size()[0], yy.size()[1] ))
-		
-		# Compute \partial_b Kx(s,a,x,y,b) . e   ------------------------------------------------ 
-		# We're looking for the gradient with respect to b of
-		#
-		# < e, K(s,a,x,y,b) >  =  \sum_{i,j} f_s'( |x_i-y_j|^2 ) * < a_i, b_j > * 2 < e_i, x_i-y_j>,
-		#
-		# which is an M-by-E array g_j (j from 1 to M), where each line is equal to
-		#
-		# g_j  =  \sum_i 2* f_s'( |x_i-y_j|^2 ) * < e_i, x_i-y_j> * a_i
-		#
-		# This is what cuda_gradconv_xb is all about:
-		
-		grad_xb = torch.zeros( bb.numel() ).type(dtype) #0d array
-		cudagradgradconv.cuda_gradconv_xb(  e.data.numpy(),
-										   aa.data.numpy(),
-										   xx.data.numpy(), yy.data.numpy(),
-										   bb.data.numpy(),
-										   grad_xb.numpy(),  # Output array
-										   ss.data.numpy(),
-										   kernel = kernel ) 
-		grad_xb  = Variable(grad_xb.view( bb.size()[0], bb.size()[1] ))
-		
-		
-		return (grad_xs, grad_xa, grad_xx, grad_xy, grad_xb, None)
-
+		# Grads wrt. aliases, formula, signature, sum_index, *args
+		return      (   None,    None,      None,      None, *grads )
 
 
 if __name__ == "__main__":
