cf978a39b75a99d69da9b7f404f609dd8c196250
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2017-12-17 22:04:39 +0100

    Remove loops in numpy checks.

--- python/pykp/convolutions/cudaconv_od.py
+++ python/pykp/convolutions/cudaconv_od.py
@@ -11,7 +11,7 @@ def get_cuda_convs_od():
     Loads the convolution routine from the compiled .so file.
     """
     dll_name = 'cuda_conv.so'
-    dllabspath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep+ 'build' + os.path.sep + dll_name
+    dllabspath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + '..' + os.path.sep + 'build' + os.path.sep + dll_name
     dll = ctypes.CDLL(dllabspath, mode=ctypes.RTLD_GLOBAL)
 
     func = dll.GaussGpuEvalConv_onDevice
@@ -57,41 +57,41 @@ if __name__ == '__main__':
     """
         testing, benchmark convolution with two naive python implementations of the Gaussian convolution
         """
-import torch
+    import torch
 
-N = 9000 ; M = 12000; D = 3; E = 3
+    N = 900 ; M = 120; D = 3; E = 3
 
-# declare numpy 
-x = np.random.randn(N,D).astype('float32')
-y = np.random.randn(M,D).astype('float32')
-b = np.random.randn(M,E).astype('float32')
-s = np.array([2.4]).astype('float32')
+    # declare numpy 
+    x = np.random.randn(N,D).astype('float32')
+    y = np.random.randn(M,D).astype('float32')
+    b = np.random.randn(M,E).astype('float32')
+    s = np.array([2.4]).astype('float32')
 
-xc = torch.from_numpy(x).cuda()
-yc = torch.from_numpy(y).cuda()
-bc = torch.from_numpy(b).cuda()
-sc = torch.from_numpy(s).cuda()
+    xc = torch.from_numpy(x).cuda()
+    yc = torch.from_numpy(y).cuda()
+    bc = torch.from_numpy(b).cuda()
+    sc = torch.from_numpy(s).cuda()
 
-def np_kernel(x, y, s, kernel) :
-    sq = np.sum( (x[:,np.newaxis,:] - y[np.newaxis,:,:]) **2, axis=2)
-    if   kernel == "gaussian"  : return np.exp( -sq / (s*s))
-    elif kernel == "laplacian" : return np.exp( -np.sqrt(sq + s*s))
-    elif kernel == "energy"    : return 1. / ( s*s + sq ) **.25 
-
-# declare the torch counterpart
-
-def torch_kernel(x, y, s, kernel) :
-    sq = torch.sum( (x[:,None]-y[None])**2 , 2 ) 
-    if   kernel == "gaussian"  : return torch.exp( -sq / (s*s))
-    elif kernel == "laplacian" : return torch.exp( -torch.sqrt(sq + s*s))
-    elif kernel == "energy"    : return torch.pow( 1. / ( s*s + sq ), .25 )
-
-k='gaussian'
-
-g0 = torch.mm(torch_kernel(xc,yc,sc,kernel=k),bc).cpu().numpy()
-
-g1 = torch.from_numpy(np.zeros([N*E]).astype('float32')).cuda()
-cuda_conv_od(xc, yc, bc, g1, s, kernel=k)
-
-print(g0)
-print(g1.cpu().view(N,E))
+    def np_kernel(x, y, s, kernel) :
+        sq = np.sum( (x[:,np.newaxis,:] - y[np.newaxis,:,:]) **2, axis=2)
+        if   kernel == "gaussian"  : return np.exp( -sq / (s*s))
+        elif kernel == "laplacian" : return np.exp( -np.sqrt(sq + s*s))
+        elif kernel == "energy"    : return 1. / ( s*s + sq ) **.25 
+    
+    # declare the torch counterpart
+    
+    def torch_kernel(x, y, s, kernel) :
+        sq = torch.sum( (x[:,None]-y[None])**2 , 2 ) 
+        if   kernel == "gaussian"  : return torch.exp( -sq / (s*s))
+        elif kernel == "laplacian" : return torch.exp( -torch.sqrt(sq + s*s))
+        elif kernel == "energy"    : return torch.pow( 1. / ( s*s + sq ), .25 )
+    
+    k='gaussian'
+    
+    g0 = torch.mm(torch_kernel(xc,yc,sc,kernel=k),bc).cpu().numpy()
+    
+    g1 = torch.from_numpy(np.zeros([N*E]).astype('float32')).cuda()
+    cuda_conv_od(xc, yc, bc, g1, s, kernel=k)
+    
+    print(g0)
+    print(g1.cpu().view(N,E))
