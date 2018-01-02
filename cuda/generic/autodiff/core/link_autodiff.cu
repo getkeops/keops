42ae9e58db5a472c5de24e783f3c066a79911120
Joan Glaunès <joan.glaunes@gmail.com>
2018-01-02 18:53:32 +0100

    new formula syntax

--- cuda/generic/autodiff/core/link_autodiff.cu
+++ cuda/generic/autodiff/core/link_autodiff.cu
@@ -7,28 +7,35 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#ifdef USENEWSYNTAX
+	#include "core/newsyntax.h"
+	using F = decltype(FORMULA);
+#else
+	using F = FORMULA;
+#endif
+
 ///////////////////////////////////////////////
 // Convolutions on Gpu device from host data //
 ///////////////////////////////////////////////
 
 // sum over j : gamma_i = sum_j F(X_i,Y_j)
 extern "C" int GpuConv2D(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv2D(Generic<FORMULA>::sEval(), params, nx, ny, gamma, args);
+    return GpuConv2D(Generic<F>::sEval(), params, nx, ny, gamma, args);
 }
 
 // sum over i : gamma_j = sum_i F(X_i,Y_j)
 extern "C" int GpuTransConv2D(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv2D(Generic<FORMULA,1>::sEval(), params, ny, nx, gamma, args);
+    return GpuConv2D(Generic<F,1>::sEval(), params, ny, nx, gamma, args);
 }
 
 // sum over j : gamma_i = sum_j F(X_i,Y_j)
 extern "C" int GpuConv1D(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv1D(Generic<FORMULA>::sEval(), params, nx, ny, gamma, args);
+    return GpuConv1D(Generic<F>::sEval(), params, nx, ny, gamma, args);
 }
 
 // sum over i : gamma_j = sum_i F(X_i,Y_j)
 extern "C" int GpuTransConv1D(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv1D(Generic<FORMULA,1>::sEval(), params, ny, nx, gamma, args);
+    return GpuConv1D(Generic<F,1>::sEval(), params, ny, nx, gamma, args);
 }
 
 //////////////////////////////////////////////////////////
@@ -37,23 +44,23 @@ extern "C" int GpuTransConv1D(__TYPE__* params, int nx, int ny, __TYPE__* gamma,
 
 // sum over j : gamma_i = sum_j F(X_i,Y_j)
 extern "C" int GpuConv2D_FromDevice(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv2D_FromDevice(Generic<FORMULA>::sEval(), params, nx, ny, gamma, args);
+    return GpuConv2D_FromDevice(Generic<F>::sEval(), params, nx, ny, gamma, args);
 }
 
 // sum over i : gamma_j = sum_i F(X_i,Y_j)
 extern "C" int GpuTransConv2D_FromDevice(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv2D_FromDevice(Generic<FORMULA,1>::sEval(), params, ny, nx, gamma, args);
+    return GpuConv2D_FromDevice(Generic<F,1>::sEval(), params, ny, nx, gamma, args);
 }
 
 
 // sum over j : gamma_i = sum_j F(X_i,Y_j)
 extern "C" int GpuConv1D_FromDevice(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv1D_FromDevice(Generic<FORMULA>::sEval(), params, nx, ny, gamma, args);
+    return GpuConv1D_FromDevice(Generic<F>::sEval(), params, nx, ny, gamma, args);
 }
 
 // sum over i : gamma_j = sum_i F(X_i,Y_j)
 extern "C" int GpuTransConv1D_FromDevice(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return GpuConv1D_FromDevice(Generic<FORMULA,1>::sEval(), params, ny, nx, gamma, args);
+    return GpuConv1D_FromDevice(Generic<F,1>::sEval(), params, ny, nx, gamma, args);
 }
 
 
@@ -69,12 +76,12 @@ extern "C" int GpuTransConv1D_FromDevice(__TYPE__* params, int nx, int ny, __TYP
 
 // sum over j : gamma_i = sum_j F(X_i,Y_j)
 extern "C" int CpuConv(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return CpuConv(Generic<FORMULA>::sEval(), params, nx, ny, gamma, args);
+    return CpuConv(Generic<F>::sEval(), params, nx, ny, gamma, args);
 }
 
 // sum over i : gamma_j = sum_i F(X_i,Y_j)
 extern "C" int CpuTransConv(__TYPE__* params, int nx, int ny, __TYPE__* gamma, __TYPE__** args) {
-    return CpuConv(Generic<FORMULA,1>::sEval(), params, ny, nx, gamma, args);
+    return CpuConv(Generic<F,1>::sEval(), params, ny, nx, gamma, args);
 }
 
 
