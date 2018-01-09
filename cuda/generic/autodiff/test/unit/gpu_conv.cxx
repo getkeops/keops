ee601b84e4068086c79f09a44f940bc9338aa6fe
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2018-01-09 11:42:31 +0100

    unit tests are working

--- cuda/generic/autodiff/test/unit/gpu_conv.cxx
+++ cuda/generic/autodiff/test/unit/gpu_conv.cxx
@@ -20,10 +20,21 @@ template < class V > void fillrandom(V& v) {
     generate(v.begin(), v.end(), floatrand);    // fills vector with random values
 }
 
-namespace {
-TEST(GpuConv, medium){
-    int Nx=50000, Ny=2000;
+float one() {
+    return ((float) 1.0); 
+}
 
+template < class V > void fillones(V& v) {
+    generate(v.begin(), v.end(), one);    // fills vector with random values
+}
+
+struct vuple{
+    std::vector<float> rescpu;
+    std::vector<float> resgpu1D;
+    std::vector<float> resgpu2D;
+};
+
+vuple compute_convs(int Nx, int Ny){
     std::vector<float> vf(Nx*3);
     fillrandom(vf);
     float *f = vf.data();
@@ -33,106 +44,91 @@ TEST(GpuConv, medium){
     std::vector<float> vy(Ny*3);
     fillrandom(vy);
     float *y = vy.data();
-    std::vector<float> vu(Nx*4);
-    fillrandom(vu);
+    std::vector<float> vu(Nx*3);
+    fillones(vu);
     float *u = vu.data();
-    std::vector<float> vv(Ny*4);
-    fillrandom(vv);
+    std::vector<float> vv(Ny*3);
+    fillones(vv);
     float *v = vv.data();
     std::vector<float> vb(Ny*3);
     fillrandom(vb);
     float *b = vb.data();
 
-    std::vector<float*> vargs(5);
+    std::vector<float*> vargs(3);
     vargs[0] = x;
     vargs[1]=y;
     vargs[2]=u;
-    vargs[3]=v;
-    vargs[4]=b;
+    /*vargs[3]=v;*/
+    /*vargs[4]=b;*/
     float **args = vargs.data();
 
     std::vector<float> resgpu2D(Nx*3), resgpu1D(Nx*3), rescpu(Nx*3);
 
     float params[1];
-    float Sigma = 1;
+    float Sigma = .0000000001;
     params[0] = 1.0/(Sigma*Sigma);
 
     GpuConv2D(params, Nx, Ny, f, args);
     resgpu2D = vf;
 
+    fillrandom(vf);
     GpuConv1D(params, Nx, Ny, f, args);
     resgpu1D = vf;
 
+    fillrandom(vf);
     CpuConv(params, Nx, Ny, f, args);
     rescpu = vf;
 
+    vuple res = {rescpu,resgpu1D,resgpu2D};
+
+    return res;
+}
+
+namespace {
+
+TEST(GpuConv, medium){
+    int Nx=50000, Ny=2000;
+    vuple res_conv = compute_convs(Nx, Ny);
+
     float s2d = 0;
     for(int i=0; i<Nx*3; i++)
-        s2d += abs(resgpu2D[i]-rescpu[i]);
+        s2d += abs(res_conv.resgpu2D[i]-res_conv.rescpu[i]);
 
     float s1d = 0;
     for(int i=0; i<Nx*3; i++)
-        s1d += abs(resgpu1D[i]-rescpu[i]);
+        s1d += abs(res_conv.resgpu1D[i]-res_conv.rescpu[i]);
 
     EXPECT_LE(s1d,5e-5);
     EXPECT_LE(s2d,5e-5);
 }
 
 TEST(GpuConv, big){
-    int Nx=5000000, Ny=20000;
+    int Nx=500, Ny=200;
+    vuple res_conv = compute_convs(Nx, Ny);
 
-    std::vector<float> vf(Nx*3);
-    fillrandom(vf);
-    float *f = vf.data();
-    std::vector<float> vx(Nx*3);
-    fillrandom(vx);
-    float *x = vx.data();
-    std::vector<float> vy(Ny*3);
-    fillrandom(vy);
-    float *y = vy.data();
-    std::vector<float> vu(Nx*4);
-    fillrandom(vu);
-    float *u = vu.data();
-    std::vector<float> vv(Ny*4);
-    fillrandom(vv);
-    float *v = vv.data();
-    std::vector<float> vb(Ny*3);
-    fillrandom(vb);
-    float *b = vb.data();
-
-    std::vector<float*> vargs(5);
-    vargs[0] = x;
-    vargs[1]=y;
-    vargs[2]=u;
-    vargs[3]=v;
-    vargs[4]=b;
-    float **args = vargs.data();
-
-    std::vector<float> resgpu2D(Nx*3), resgpu1D(Nx*3), rescpu(Nx*3);
-
-    float params[1];
-    float Sigma = 1;
-    params[0] = 1.0/(Sigma*Sigma);
-
-    GpuConv2D(params, Nx, Ny, f, args);
-    resgpu2D = vf;
-
-    GpuConv1D(params, Nx, Ny, f, args);
-    resgpu1D = vf;
-
-    CpuConv(params, Nx, Ny, f, args);
-    rescpu = vf;
+    for(int i=0; i<Nx*3; i++){
+        std::cout << "Checks : " << res_conv.resgpu2D[i] << " " << res_conv.resgpu1D[i] << " " << res_conv.rescpu[i] << " : " << i << std::endl;
+    }
 
     float s2d = 0;
-    for(int i=0; i<Nx*3; i++)
-        s2d += abs(resgpu2D[i]-rescpu[i]);
+    for(int i=0; i<Nx*3; i++){
+        float t = abs(res_conv.resgpu2D[i]-res_conv.rescpu[i]);
+        s2d += t;
+        if (t > 5e-5) 
+            std::cout << res_conv.resgpu2D[i] << " " << res_conv.rescpu[i] << " : " << i << std::endl;
+    }
 
     float s1d = 0;
     for(int i=0; i<Nx*3; i++)
-        s1d += abs(resgpu1D[i]-rescpu[i]);
+        s1d += abs(res_conv.resgpu1D[i]-res_conv.rescpu[i]);
+
+    float s21d = 0;
+    for(int i=0; i<Nx*3; i++)
+        s21d += abs(res_conv.resgpu1D[i]-res_conv.resgpu2D[i]);
 
     EXPECT_LE(s1d,5e-5);
     EXPECT_LE(s2d,5e-5);
+    EXPECT_LE(s21d,5e-5);
 }
 
 }  // namespace
