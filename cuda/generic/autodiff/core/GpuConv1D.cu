a9e64fdde0548dcf8335996a3b08593fdbfb7b8c
Jean Feydy <jean.feydy@gmail.com>
2018-01-15 15:38:44 +0100

    Removed a useless bit from CudaConv1D_FromDevice.

--- cuda/generic/autodiff/core/GpuConv1D.cu
+++ cuda/generic/autodiff/core/GpuConv1D.cu
@@ -157,10 +157,12 @@ int GpuConv1D_FromDevice(FUN fun, PARAM param_d, int nx, int ny, TYPE** px_d, TY
     const int DIMY = DIMSY::SUM;
     const int DIMX1 = DIMSX::FIRST;
 
+    /* // (Jean :) This portion of code seems to be useless,
+    //             forgotten from a GpuConv2D copy-paste
     TYPE *out;
-
     cudaMalloc((void**)&out, sizeof(TYPE)*(nx*DIMX1));
     out = px_d[0]; // save the output location
+    */
 
     // Compute on device : grid and block are both 1d
     dim3 blockSize;
