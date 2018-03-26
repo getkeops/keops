ce2bee3336e516bbbfc991d075279e1f34934d84
Unknown <joan.glaunes@gmail.com>
2018-03-26 14:45:45 +0200

    updated LDDMM example

--- pykeops/examples/LDDMM_Surface.py
+++ pykeops/examples/LDDMM_Surface.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 25 00:38:18 2018
-
-@author: glaunes
+Example of diffeomorphic matching of surfaces using varifolds metrics
 """
-
-#%%
 
 import os.path
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
-import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -21,72 +16,57 @@ from torch.autograd import Variable, grad
 
 import time
 
-from pykeops.torch.generic_sum       import GenericSum
 from pykeops.torch.kernels import Kernel, kernel_product
 
-def GaussKernelKeopsGeneric(sigma):
-    def f(x,y,b):
-        dp = x.shape[1]
-        dv = b.shape[1]
-        aliases = ["P = Pm(0,1)","X=Vx(1,"+str(dp)+")","Y=Vy(2,"+str(dp)+")","B=Vy(3,"+str(dv)+")"]
-        formula = "Exp(-P*SqDist(X,Y))*B"
-        signature   =   [ (dv, 0), (1, 2), (dp, 0), (dp, 1), (dv, 1) ]
-        sum_index   = 0 # the result is indexed by "i"; for "j", use "1"
-        oos2 = Variable(torch.FloatTensor([1/sigma**2]))
-        return GenericSum.apply("auto",aliases,formula,signature,sum_index,oos2,x,y,b)
-    return f
 
-def GaussKernelKeops(sigma):
-    def f(x,y,b):
-        params = {
-            "id"      : Kernel("gaussian(x,y)"),
-            "gamma"   : Variable(torch.FloatTensor([1/sigma**2])),
-            "backend" : "auto"
-        }
-        return kernel_product( x,y,b, params)
-    return f
+lib = "keops" # "pytorch" or "keops"
 
-def GaussKernel(sigma):
-    oos2 = 1/sigma**2
-    def f(x,y,b):
-        return torch.exp(-oos2*torch.sum((x[:,None,:]-y[None,:,:])**2,dim=2))@b
-    return f
+use_cuda = torch.cuda.is_available()
+deviceId = 0    # id of Gpu device
 
-def GaussLinKernel(sigma):
-    oos2 = 1/sigma**2
-    def f(x,y,vx,vy,b):
-        Kxy = torch.exp(-oos2*torch.sum((x[:,None,:]-y[None,:,:])**2,dim=2))
-        Sxy = torch.sum(vx[:,None,:]*vy[None,:,:],dim=2)**2
-        return (Kxy*Sxy)@b
-    return f
+if use_cuda:
+    datafile = 'data/hippos_reduc.pt'
+else:
+    datafile = 'data/hippos_reduc_reduc.pt'
 
-def GaussLinKernelKeops(sigma):
-    def f(x,y,vx,vy,b):
-        params = {
-            "id"      : Kernel("gaussian(x,y) * linear(u,v)**2"),
-            "gamma"   : (Variable(torch.FloatTensor([1/sigma**2])),Variable(torch.FloatTensor([0]))),
-            "backend" : "auto"
-        }
-        return kernel_product( (x,vx),(y,vy),b, params)
-    return f
-#
-def GaussLinKernelKeopsGeneric(sigma):
-    def f(x,y,vx,vy,b):
-        dp = x.shape[1]
-        df = vx.shape[1]
-        dv = b.shape[1]
-        aliases = ["P = Pm(0,1)",
-                   "X=Vx(1,"+str(dp)+")","Y=Vy(2,"+str(dp)+")",
-                   "U=Vx(3,"+str(df)+")","V=Vy(4,"+str(df)+")",
-                   "B=Vy(5,"+str(dv)+")"]
-        formula = "Exp(-P*SqDist(X,Y))*Square((U,V))*B"
-        signature   =   [ (dv, 0), (1, 2), (dp, 0), (dp, 1), (df, 0), (df, 1), (dv, 1) ]
-        sum_index   = 0 # the result is indexed by "i"; for "j", use "1"
-        oos2 = Variable(torch.FloatTensor([1/sigma**2]))
-        return GenericSum.apply("auto",aliases,formula,signature,sum_index,oos2,x,y,vx,vy,b)
-    return f
+# define Gaussian kernel (K(x,y)b)_i = sum_j exp(-|xi-yj|^2)bj
+def GaussKernel(sigma,lib="keops"):
+    if lib=="pytorch":
+        oos2 = 1/sigma**2
+        def f(x,y,b):
+            return torch.exp(-oos2*torch.sum((x[:,None,:]-y[None,:,:])**2,dim=2))@b
+        return f
+    elif lib=="keops":
+        def f(x,y,b):
+            params = {
+                "id"      : Kernel("gaussian(x,y)"),
+                "gamma"   : Variable(torch.FloatTensor([1/sigma**2])),
+                "backend" : "auto"
+            }
+            return kernel_product( x,y,b, params)
+        return f
 
-def HeunsIntegrator(nt=10):
+# define "Gaussian-CauchyBinet" kernel (K(x,y,u,v)b)_i = sum_j exp(-|xi-yj|^2) <ui,vj>^2 bj
+def GaussLinKernel(sigma,lib="keops"):
+    if lib=="pytorch":
+        oos2 = 1/sigma**2
+        def f(x,y,u,v,b):
+            Kxy = torch.exp(-oos2*torch.sum((x[:,None,:]-y[None,:,:])**2,dim=2))
+            Sxy = torch.sum(u[:,None,:]*v[None,:,:],dim=2)**2
+            return (Kxy*Sxy)@b
+        return f
+    elif lib=="keops":
+        def f(x,y,u,v,b):
+            params = {
+                "id"      : Kernel("gaussian(x,y) * linear(u,v)**2"),
+                "gamma"   : (Variable(torch.FloatTensor([1/sigma**2])),Variable(torch.FloatTensor([0]))),
+                "backend" : "auto"
+            }
+            return kernel_product( (x,u),(y,v),b, params)
+        return f
+
+# custom ODE solver, for ODE systems which are defined on tuples
+def RalstonIntegrator(nt=10):
     def f(ODESystem,x0,deltat=1.0):
         x = tuple(map(lambda x:x.clone(),x0))
         dt = deltat/nt
@@ -98,6 +78,7 @@ def HeunsIntegrator(nt=10):
         return x
     return f
 
+# LDDMM implementation
 def Hamiltonian(K):
     def f(p,q):
         return .5*(p*K(q,q,p)).sum()
@@ -110,15 +91,24 @@ def HamiltonianSystem(K):
         return -Gq,Gp
     return f
 
-def Shooting(p0,q0,K,deltat=1.0,Integrator=HeunsIntegrator()):
+def Shooting(p0,q0,K,deltat=1.0,Integrator=RalstonIntegrator()):
     return Integrator(HamiltonianSystem(K),(p0,q0),deltat)
 
-def Flow(x0,p0,q0,K,deltat=1.0,Integrator=HeunsIntegrator()):
+def Flow(x0,p0,q0,K,deltat=1.0,Integrator=RalstonIntegrator()):
     HS = HamiltonianSystem(K)
     def FlowEq(x,p,q):
         return (K(x,q,p),)+HS(p,q)
     return Integrator(FlowEq,(x0,p0,q0),deltat)[0]
 
+def LDDMMloss(K,loss,gamma=0):
+    def f(p0,q0):
+        p,q = Shooting(p0,q0,K)
+        return gamma * Hamiltonian(K)(p0,q0) + loss(q)
+    return f
+
+# Varifold data attachment loss for surfaces
+# VT: vertices coordinates of target surface, 
+# FS,FT : Face connectivity of source and target surfaces
 def lossVarifoldSurf(FS,VT,FT,K):
     def CompCLNn(F,V):
         V0, V1, V2 = V.index_select(0,F[:,0]), V.index_select(0,F[:,1]), V.index_select(0,F[:,2])
@@ -132,67 +122,60 @@ def lossVarifoldSurf(FS,VT,FT,K):
         return cst + (LS*K(CS,CS,NSn,NSn,LS)).sum() - 2*(LS*K(CS,CT,NSn,NTn,LT)).sum()
     return f
 
-def LDDMMloss(K,loss,gamma=0):
-    def f(p0,q0):
-        p,q = Shooting(p0,q0,K)
-        return gamma * Hamiltonian(K)(p0,q0) + loss(q)
-    return f
-
+# function to transfer data on Gpu only if we use the Gpu
 def CpuOrGpu(x):
-    if torch.cuda.is_available():
-        with torch.cuda.device(1):
+    if use_cuda:
+        if type(x)==tuple:
             x = tuple(map(lambda x:x.cuda(),x))
+        else:
+            x = x.cuda()
     return x
 
-VS,FS,VT,FT = CpuOrGpu(torch.load('data/hippos_reduc_reduc.pt'))
 
-q0 = VS = Variable(VS,requires_grad=True)
-VT, FS, FT = Variable(VT), Variable(FS), Variable(FT)
+def RunExample(datafile=datafile,lib=lib):
+    # load dataset
+    VS,FS,VT,FT = CpuOrGpu(torch.load(datafile))
+    q0 = VS = Variable(VS,requires_grad=True)
+    VT, FS, FT = Variable(VT), Variable(FS), Variable(FT)
 
-lossData = lossVarifoldSurf(FS,VT,FT,GaussLinKernelKeopsGeneric(sigma=20))
+    # define data attachment and LDDMM functional
+    lossData = lossVarifoldSurf(FS,VT,FT,GaussLinKernel(sigma=20,lib=lib))
+    Kv = GaussKernel(sigma=20,lib=lib)
+    loss = LDDMMloss(Kv,lossData)
 
-n,d = q0.shape
-p0 = Variable(CpuOrGpu(torch.zeros(n,d)), requires_grad=True)
+    # initialize momentum vectors
+    p0 = Variable(CpuOrGpu(torch.zeros(q0.shape)), requires_grad=True)
 
-optimizer = torch.optim.LBFGS([p0])
+    # perform optimization
+    optimizer = torch.optim.LBFGS([p0])
+    N = 5
+    start = time.time()
+    for i in range(N):
+        def closure():
+            optimizer.zero_grad()
+            L = loss(p0,q0)
+            L.backward()
+            return L
+        optimizer.step(closure)
+    print('Optimization time : ',round(time.time()-start,2),' seconds')
 
-#%%
+    # display output    
+    fig = plt.figure();
+    plt.title('LDDMM matching example')  
+    p,q = Shooting(p0,q0,Kv)
+    q0np, qnp, FSnp = q0.data.cpu().numpy(), q.data.cpu().numpy(), FS.data.cpu().numpy()
+    VTnp, FTnp = VT.data.cpu().numpy(), FT.data.cpu().numpy()    
+    ax = Axes3D(fig)
+    ax.axis('equal')
+    ax.plot_trisurf(q0np[:,0],q0np[:,1],q0np[:,2],triangles=FSnp,alpha=.5)
+    ax.plot_trisurf(qnp[:,0],qnp[:,1],qnp[:,2],triangles=FSnp,alpha=.5)
+    ax.plot_trisurf(VTnp[:,0],VTnp[:,1],VTnp[:,2],triangles=FTnp,alpha=.5)
 
-Kv = GaussKernelKeopsGeneric(sigma=20)
+# run the example
+if use_cuda:
+    with torch.cuda.device(deviceId):
+        RunExample(datafile,lib)
+else:
+    RunExample(datafile,lib)
 
 
-
-loss = LDDMMloss(Kv,lossData)
-
-N = 5
-
-start = time.time()
-for i in range(N):
-    def closure():
-        optimizer.zero_grad()
-        L = loss(p0,q0)
-        L.backward()
-        return L
-    optimizer.step(closure)
-print('Optimization time : ',round(time.time()-start,2),' seconds')
-
-#%%
-
-nt = 10
-Q = np.zeros((n,d,nt+1))
-p,q = p0,q0
-for i in range(nt):
-    Q[:,:,i] = q.data.cpu()
-    p,q = Shooting(p,q,Kv,1/nt,HeunsIntegrator(1))
-Q[:,:,nt] = q.data.cpu()
-
-fig = plt.figure();
-plt.title('LDDMM matching example')  
-q0np, qnp, FSnp = q0.data.cpu().numpy(), q.data.cpu().numpy(), FS.data.cpu().numpy()
-VTnp, FTnp = VT.data.cpu().numpy(), FT.data.cpu().numpy()
-#ax = fig.gca(projection='3d')
-ax = Axes3D(fig)
-ax.axis('equal')
-ax.plot_trisurf(q0np[:,0],q0np[:,1],q0np[:,2],triangles=FSnp,alpha=.5)
-ax.plot_trisurf(qnp[:,0],qnp[:,1],qnp[:,2],triangles=FSnp,alpha=.5)
-ax.plot_trisurf(VTnp[:,0],VTnp[:,1],VTnp[:,2],triangles=FTnp,alpha=.5)
