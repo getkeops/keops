d57b4092f03e176942be65e301d03c03bb90340e
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2018-04-30 10:56:58 +0200

    Add generic sum to python unitary test

--- pykeops/test/unit_tests_pytorch.py
+++ pykeops/test/unit_tests_pytorch.py
@@ -8,7 +8,6 @@ import numpy as np
 
 import torch
 from torch.autograd import Variable, grad
-from pykeops.torch.kernels import Kernel, kernel_product
 
 from pykeops.numpy.utils import np_kernel, grad_np_kernel, differences, squared_distances
 from pykeops.numpy.convolutions.radial_kernels import radial_kernels_conv
@@ -25,6 +24,7 @@ class PytorchUnitTestCase(unittest.TestCase):
     a = np.random.rand(N,E).astype('float32')
     x = np.random.rand(N,D).astype('float32')
     y = np.random.rand(M,D).astype('float32')
+    f = np.random.rand(M,1).astype('float32')
     b = np.random.rand(M,E).astype('float32')
     sigma = np.array([0.4]).astype('float32')
 
@@ -34,10 +34,14 @@ class PytorchUnitTestCase(unittest.TestCase):
     ac = Variable(torch.from_numpy(a.copy()).type(dtype), requires_grad=True).type(dtype)
     xc = Variable(torch.from_numpy(x.copy()).type(dtype), requires_grad=True).type(dtype)
     yc = Variable(torch.from_numpy(y.copy()).type(dtype), requires_grad=True).type(dtype)
+    fc = Variable(torch.from_numpy(f.copy()).type(dtype), requires_grad=True).type(dtype)
     bc = Variable(torch.from_numpy(b.copy()).type(dtype), requires_grad=True).type(dtype)
     sigmac = torch.autograd.Variable(torch.from_numpy(sigma.copy()).type(dtype), requires_grad=False).type(dtype)
 
+#--------------------------------------------------------------------------------------
     def test_conv_kernels_feature(self):
+#--------------------------------------------------------------------------------------
+        from pykeops.torch.kernels import Kernel, kernel_product
         mode = "sum"
         params = {
             "gamma"   : 1./self.sigmac**2,
@@ -55,7 +59,10 @@ class PytorchUnitTestCase(unittest.TestCase):
                 # compare output
                 self.assertTrue( np.allclose(gamma.data.numpy(), gamma_py))
 
+#--------------------------------------------------------------------------------------
     def test_grad1conv_kernels_feature(self):
+#--------------------------------------------------------------------------------------
+        from pykeops.torch.kernels import Kernel, kernel_product
         mode = "sum"
         params = {
             "gamma"   : 1./self.sigmac**2,
@@ -77,6 +84,32 @@ class PytorchUnitTestCase(unittest.TestCase):
                 # compare output
                 self.assertTrue( np.allclose(gamma_keops.cpu().data.numpy(), gamma_py , atol=1e-6))
 
+#--------------------------------------------------------------------------------------
+    def test_generic_syntax(self):
+#--------------------------------------------------------------------------------------
+        from pykeops.torch.kernels import Kernel
+        from pykeops.torch.generic_sum import GenericSum
+        aliases = ["p=Pm(0,1)","a=Vy(1,1)","x=Vx(2,3)","y=Vy(3,3)"]
+        formula = "Square(p-a)*Exp(x+y)"
+        signature   =   [ (3, 0), (1, 2), (1, 1), (3, 0), (3, 1) ]
+        sum_index = 0       # 0 means summation over j, 1 means over i 
+
+        for b in ['auto','GPU_1D','GPU_2D','GPU']:
+            with self.subTest(b=b):
+
+                # Call cuda kernel
+                gamma_keops = GenericSum.apply(b,aliases,formula,signature,sum_index,self.sigmac,self.fc,self.xc,self.yc)
+
+                # Numpy version
+                gamma_py = np.sum((self.sigma - self.f)**2 *np.exp( (self.y.T[:,:,np.newaxis] + self.x.T[:,np.newaxis,:])),axis=1).T
+
+                # compare output
+                self.assertTrue( np.allclose(gamma_keops.cpu().data.numpy(), gamma_py , atol=1e-6))
+
+
 if __name__ == '__main__':
+    """
+    run tests
+    """
     unittest.main()
 
