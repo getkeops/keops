11f0088e79e2a6a61d7bd7a12decf5cc346b788a
Unknown <joan.glaunes@gmail.com>
2018-05-14 18:59:43 +0200

    testing use of namespace for new syntax, WIP

--- keops/core/formulas/newsyntax.h
+++ keops/core/formulas/newsyntax.h
@@ -33,51 +33,21 @@
 
 #define ElemT(p,k) ElemT<decltype(p),k>()
 
-template < class FA, class FB >
-Add<FA,FB> operator+(FA fa, FB fb) {
-    return Add<FA,FB>();
-}
-
-template < class FA, class FB >
-ScalOrMult<FA,FB> operator*(FA fa, FB fb) {
-    return ScalOrMult<FA,FB>();
-}
-
 #define Exp(f) Exp<decltype(f)>()
 
 #define Pow(f,M) Pow<decltype(f),M>()
 
 #define Square(f) Square<decltype(f)>()
 
-template < class F >
-Minus<F> operator-(F f) {
-    return Minus<F>();
-}
-
-template < class FA, class FB >
-Subtract<FA,FB> operator-(FA fa, FB fb) {
-    return Subtract<FA,FB>();
-}
-
 #define Inv(f) Inv<decltype(f)>()
 
 #define IntInv(N) IntInv<N>()
-
-template < class FA, class FB >
-Divide<FA,FB> operator/(FA fa, FB fb) {
-    return Divide<FA,FB>();
-}
 
 #define Log(f) Log<decltype(f)>()
 
 #define Powf(fa,fb) Powf<decltype(fa),decltype(fb)>()
 
 #define Sqrt(f) Sqrt<decltype(f)>()
-
-template < class FA, class FB >
-Scalprod<FA,FB> operator,(FA fa, FB fb) {
-    return Scalprod<FA,FB>();
-}
 
 #define SqNorm2(f) SqNorm2<decltype(f)>()
 #define SqDist(f,g) SqDist<decltype(f),decltype(g)>()
@@ -96,4 +66,36 @@ Scalprod<FA,FB> operator,(FA fa, FB fb) {
 #define DivFreeGaussKernel(C,X,Y,B) DivFreeGaussKernel<decltype(C),decltype(X),decltype(Y),decltype(B)>()
 #define CurlFreeGaussKernel(C,X,Y,B) CurlFreeGaussKernel<decltype(C),decltype(X),decltype(Y),decltype(B)>()
 #define TRIGaussKernel(L,C,X,Y,B) TRIGaussKernel<decltype(L),decltype(C),decltype(X),decltype(Y),decltype(B)>()
+
+namespace newsyntax_ops {
+    template < class FA, class FB >
+    Add<FA,FB> operator+(FA fa, FB fb) {
+        return Add<FA,FB>();
+    }
+
+    template < class FA, class FB >
+    ScalOrMult<FA,FB> operator*(FA fa, FB fb) {
+        return ScalOrMult<FA,FB>();
+    }
+
+    template < class F >
+    Minus<F> operator-(F f) {
+        return Minus<F>();
+    }
+
+    template < class FA, class FB >
+    Subtract<FA,FB> operator-(FA fa, FB fb) {
+        return Subtract<FA,FB>();
+    }
+
+    template < class FA, class FB >
+    Divide<FA,FB> operator/(FA fa, FB fb) {
+        return Divide<FA,FB>();
+    }
+
+    template < class FA, class FB >
+    Scalprod<FA,FB> operator,(FA fa, FB fb) {
+        return Scalprod<FA,FB>();
+    }
+}
 
