e284135610311664d168ad3a48bcbc0672005a87
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2018-05-28 16:46:36 +0200

    update compile_routine

--- pykeops/common/compile_routines.py
+++ pykeops/common/compile_routines.py
@@ -22,6 +22,26 @@ def compile_generic_routine(aliases, formula, dllname, cuda_type):
                    cwd=build_folder,stdout=stdout)
     print("Done. ", end='', flush=True)
 
+def compile_generic_routine2(aliases, formula, dllname, cuda_type):
+    stdout = subprocess.DEVNULL if not verbose else None
+
+    def process_alias(alias):
+        return "auto " + str(alias) + "; "
+
+    def process_disp_alias(alias):
+        return str(alias) + "; "
+
+    alias_string = "".join([process_alias(alias) for alias in aliases])
+    alias_disp_string = "".join([process_disp_alias(alias) for alias in aliases])
+
+    target = "pykeops_module"
+    script_folder2 = '/home/bcharlier/libkp/pykeops'
+
+    print("Compiling formula : " + formula + " with " + alias_disp_string + " ... ", end='', flush=True)
+    subprocess.run(["cmake", script_folder2, "-DUSENEWSYNTAX=TRUE" , "-DFORMULA_OBJ="+formula, "-DVAR_ALIASES="+alias_string, "-Dshared_obj_name="+dllname, "-D__TYPE__="+cuda_type], cwd=build_folder,stdout=stdout)
+    subprocess.run(["make", target], cwd=build_folder,stdout=stdout)
+    print("Done. ", end='', flush=True)
+
 def compile_specific_routine(dllname, cuda_type):
     #print('Tried to load ' + dllname + ", ", end='')
     #print("but could not find the DLL. Compiling it... ", end='')
