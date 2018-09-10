32bca111bcf5eb3bf7fe7017002f8f550d6c5c0e
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2018-09-10 17:02:36 +0200

    An attempt to fix linking problem with pytorch 0.4.1

--- keops/headers.cmake
+++ keops/headers.cmake
@@ -5,7 +5,7 @@
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -Wall -ferror-limit=2")
 else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -Wall -fmax-errors=2")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -Wall -fmax-errors=2 -D_GLIBCXX_USE_CXX11_ABI=0") #  fix for pytorch 0.4.1 ... 
 endif()
 
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g")
