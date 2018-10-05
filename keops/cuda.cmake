ec8fb71d721c00f8dfa13ef66cc33768acc0a68b
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2018-10-05 14:46:59 +0200

    Fix CUDA_HOST_CXX_COMPILER issue

--- keops/cuda.cmake
+++ keops/cuda.cmake
@@ -2,7 +2,7 @@
 #------------------------------FIND CUDA AND GPUs------------------------------------#
 #------------------------------------------------------------------------------------#
 
-# As of now, we use an ugly mix of old and new cmake method to properly detect cuda, nvcc and the gpu arch...
+# As of now, we use an ugly mix of old and new cmake methods to properly detect cuda, nvcc and the gpu arch...
 find_package(CUDA)
 
 if(NOT DEFINED USE_CUDA)
@@ -12,18 +12,25 @@ endif()
 
 if(CUDA_FOUND AND USE_CUDA)
    
-   set(CMAKE_CUDA_COMPILER ${CUDA_NVCC_EXECUTABLE})
-   enable_language(CUDA)
+    set(CMAKE_CUDA_COMPILER ${CUDA_NVCC_EXECUTABLE})
+    
+    enable_language(CUDA)
 
-   # Options for nvcc
-   CUDA_SELECT_NVCC_ARCH_FLAGS(out_variable "Auto")
-   
-   set (CUDA_PROPAGATE_HOST_FLAGS ON)
-   
-   List(APPEND CUDA_NVCC_FLAGS ${out_variable})
-   List(APPEND CUDA_NVCC_FLAGS "--use_fast_math")
-   List(APPEND CUDA_NVCC_FLAGS "--compiler-options=-fPIC")
-   List(APPEND CUDA_NVCC_FLAGS "-ccbin ${CMAKE_CUDA_HOST_COMPILER}")
+    if (CMAKE_CUDA_HOST_COMPILER)
+        message(STATUS "The CUDA Host CXX Compiler: ${CMAKE_CUDA_HOST_COMPILER}")
+    else()
+        message(STATUS "The CUDA Host CXX Compiler: ${CMAKE_CXX_COMPILER}")
+    endif()
+
+
+    # Options for nvcc
+    CUDA_SELECT_NVCC_ARCH_FLAGS(out_variable "Auto")
+    
+    set (CUDA_PROPAGATE_HOST_FLAGS ON)
+    
+    List(APPEND CUDA_NVCC_FLAGS ${out_variable})
+    List(APPEND CUDA_NVCC_FLAGS "--use_fast_math")
+    List(APPEND CUDA_NVCC_FLAGS "--compiler-options=-fPIC")
    
 else()
     set(USE_CUDA 0)
