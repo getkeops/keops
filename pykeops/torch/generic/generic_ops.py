e5b25c72a4a349ba4109b4fd138effff31c37669
Jean Feydy <jean.feydy@gmail.com>
2018-10-23 17:47:22 +0200

    Fixed the legacy generic backends.

--- pykeops/torch/generic/generic_ops.py
+++ pykeops/torch/generic/generic_ops.py
@@ -3,26 +3,26 @@ from pykeops.common.parse_type import get_type
 
 from pykeops.torch.generic.generic_red import Genred
 
-def generic_sum(formula, *aliases, **kwargs) :
-    _,cat,_,_ = get_type(aliases[0])
+def generic_sum(formula, output, *aliases, **kwargs) :
+    _,cat,_,_ = get_type(output)
     axis = cat2axis(cat)
-    return Genred(formula, aliases[1:], reduction_op='Sum', axis=axis, **kwargs)
+    return Genred(formula, aliases, reduction_op='Sum', axis=axis, **kwargs)
 
-def generic_logsumexp(formula, *aliases, **kwargs) :
-    _,cat,_,_ = get_type(aliases[0])
+def generic_logsumexp(formula, output, *aliases, **kwargs) :
+    _,cat,_,_ = get_type(output)
     axis = cat2axis(cat)
-    routine = Genred(formula, aliases[1:], reduction_op='LogSumExp', axis=axis,  **kwargs)
-    def red_routine(*args, **kwargs) :
-        tmp = routine(*args, **kwargs)
-        return tmp[:,0] + (tmp[:,1]).log()
+    routine = Genred(formula, aliases, reduction_op='LogSumExp', axis=axis,  **kwargs)
+    def red_routine(*args, **kwargs2) :
+        tmp = routine(*args, **kwargs2)
+        return (tmp[:,0] + (tmp[:,1]).log()).view(-1,1)
     return red_routine
 
-def generic_argkmin(formula, *aliases, **kwargs) :
-    _,cat,_,_ = get_type(aliases[0])
+def generic_argkmin(formula, output, *aliases, **kwargs) :
+    _,cat,_,_ = get_type(output)
     axis = cat2axis(cat)
-    return Genred(formula, aliases[1:], reduction_op='ArgKMin', axis=axis,  **kwargs)
+    return Genred(formula, aliases, reduction_op='ArgKMin', axis=axis,  **kwargs)
 
-def generic_argmin(formula, *aliases, **kwargs) :
-    _,cat,_,_ = get_type(aliases[0])
+def generic_argmin(formula, output, *aliases, **kwargs) :
+    _,cat,_,_ = get_type(output)
     axis = cat2axis(cat)
-    return Genred(formula, aliases[1:], reduction_op='ArgMin', axis=axis,  **kwargs)
+    return Genred(formula, aliases, reduction_op='ArgMin', axis=axis,  **kwargs)
