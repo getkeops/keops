4d167a169aa19b4788025a3cec029061ed6c20cb
Unknown <joan.glaunes@gmail.com>
2019-02-25 11:47:07 +0100

    XIP

--- pykeops/tutorials/interpolation/test2.py
+++ pykeops/tutorials/interpolation/test2.py
@@ -46,19 +46,18 @@ class g_Impl(torch.autograd.Function):
         with torch.enable_grad():
             e = g(x,grad_output)
             gfx = grad(f(x,abar),x,create_graph=True)[0]
-            return link(-e*gfx,abar,a), e
+            return -e*link(gfx,x,abar,a), e
 
 class link_Impl(torch.autograd.Function):
     @staticmethod
-    def forward(ctx,f,abar,a):
-        ctx.save_for_backward(f,abar,a)
+    def forward(ctx,f,x,abar,a):
+        ctx.save_for_backward(f,x,abar,a)
         return f
     @staticmethod
     def backward(ctx, grad_output):
-        f,abar,a = ctx.saved_tensors
-        with torch.enable_grad():
-            ga = grad(f,abar,grad_output,create_graph=True,allow_unused=True)[0]
-        return link(grad_output,abar,a), None, link(ga,abar,a)
+        f,x,abar,a = ctx.saved_tensors
+        gx,ga = grad(f,[x,abar],grad_output,create_graph=True,allow_unused=True)
+        return None, link(gx,x,abar,a), link(ga,x,abar,a), link(ga,x,abar,a)
 
 g = g_Impl.apply
 def link(*args):
@@ -71,18 +70,36 @@ x = torch.randn(1,requires_grad=True)
 b = torch.randn(1)
 
 gx = grad(g(x,b),x,create_graph=True)[0]
+gx_ = grad(b/x**3,x,create_graph=True)[0]
 print("gx = ",gx.data)
-print("-3b/x**4 = ",-3*b/x.data**4)
+print("gx_ = ",gx_.data)
 
 ggx = grad(gx,x,create_graph=True)[0]
+ggx_ = grad(gx_,x,create_graph=True)[0]
 print("ggx = ",ggx.data)
-print("12b/x**5 = ",12*b/x.data**5)
+print("ggx_ = ",ggx_.data)
 
 gggx = grad(ggx,x,create_graph=True)[0]
+gggx_ = grad(ggx_,x,create_graph=True)[0]
 print("gggx = ",gggx.data)
-print("-60b/x**6 = ",-60*b/x.data**6)
+print("gggx_ = ",gggx_.data)
 
 ggggx = grad(gggx,x,create_graph=True)[0]
+ggggx_ = grad(gggx_,x,create_graph=True)[0]
 print("ggggx = ",ggggx.data)
-print("360b/x**7 = ",360*b/x.data**7)
+print("ggggx_ = ",ggggx_.data)
+
+# x = torch.randn(1,requires_grad=True)
+# a = x**2
+# abar = torch.tensor(a.data,requires_grad=True)
+# s = x**2*a*abar # s=x^6
+# gs = grad(link(s,x,abar,a),x,create_graph=True)[0] # 6x^5
+# print("gs = ",gs.data)
+# print("6x^5 = ",6*x.data**5)
+# ggs = grad(gs,x,create_graph=True)[0] # 30*x^4
+# print("ggs = ",ggs.data)
+# print("30x^4 = ",30*x.data**4)
+# gggs = grad(ggs,x,create_graph=True)[0] # 120*x^3
+# print("gggs = ",gggs.data)
+# print("120x^3 = ",120*x.data**3)
 
