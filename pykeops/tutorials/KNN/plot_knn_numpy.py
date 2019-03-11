3eb3f2f2f14101944e982264317fddaaba29cbf5
Joan Glaunès <joan.glaunes@gmail.com>
2019-03-11 17:17:28 +0100

    improved example

--- pykeops/tutorials/KNN/plot_knn_numpy.py
+++ pykeops/tutorials/KNN/plot_knn_numpy.py
@@ -26,7 +26,7 @@ type = 'float32'  # May be 'float32' or 'float64'
 #  We wrap this example into a function
 #
 
-def KNN_Example(N,D,Ktab):
+def KNN_Example(N,D,Ng,Ktab):
 
     #####################
     # Define our dataset
@@ -35,21 +35,22 @@ def KNN_Example(N,D,Ktab):
     x = np.random.rand(N, D).astype(type)
     def fth(x):
         return 3*x*(x-.5)*(x-1)+x
-    cl = x[:,1] < fth(x[:,0])
-    x = x + .01*np.random.randn(N, D).astype(type)
+    cl = (x[:,1]+.03*np.random.randn(N).astype(type)) < fth(x[:,0])
 
     # building the grid for evaluation
-    NG = 500
-    tmp = np.linspace(0,1,NG).astype(type)
+    tmp = np.linspace(0,1,Ng).astype(type)
     g1, g2 = np.meshgrid(tmp,tmp)
     g = np.concatenate((g1.reshape(-1,1),g2.reshape(-1,1)),axis=1)
+
+    WarmUpGpu()
         
-    plt.ion()
-    plt.clf()    
+    if D==2:
+        plt.ion()
+        plt.clf()    
     for i in range(len(Ktab)):
         K = Ktab[i]
         print("")
-        print('k-NN example with ' + str(N) + ' points in ' + str(D) + '-D, and K=' + str(K))
+        print('K-NN example with ' + str(N) + ' points in ' + str(D) + '-D, and K=' + str(K))
 
         #######################
         # Define the kernel
@@ -65,35 +66,37 @@ def KNN_Example(N,D,Ktab):
         ##########################
         # Perform the computations
         #
-        WarmUpGpu()
         start = time.time()
         # calling keops to compute the K nearest neighbours
         indKNN = my_routine(g,x).astype(int)
         # classify grid points
         clg = np.mean(cl[indKNN],axis=1)>.5
         end = time.time()
-        print('Time to perform k-NN classification:', round(end - start, 5), 's')
+        print('Time to perform K-NN classification over ',Ng*Ng,' test points:', round(end - start, 5), 's')
 
-        plt.subplot(np.ceil(len(Ktab)/4),np.min([len(Ktab),4]),i+1)
-        plt.scatter(x[:, 0], x[:, 1], c=cl, s=10)
-        #plt.scatter(g[:, 0], g[:, 1], c=clg, s=2, alpha=.5)
-        # reshaping grid classes for display as image
-        clg = np.reshape(clg,(NG,NG))
-        plt.imshow(clg, extent=(0,1,0,1), origin='lower', alpha=.5)
-    print('Close the figure to continue.')
-    plt.show(block=(__name__ == '__main__'))
+        if D==2:
+            plt.subplot(np.ceil(len(Ktab)/4),np.min([len(Ktab),4]),i+1)
+            plt.scatter(x[:, 0], x[:, 1], c=cl, s=3)
+            # reshaping grid classes for display as image
+            clg = np.reshape(clg,(Ng,Ng))
+            plt.imshow(clg, extent=(0,1,0,1), origin='lower', alpha=.5)
+    if D==2:
+        print('Close the figure to continue.')
+        plt.show(block=(__name__ == '__main__'))
     
-###############################################################
-# First experiment with 5000 points, dimension 2 and 50 classes
+#######################################################
+# First experiment with 1000 data points in dimension 2
+# with evalaution over a grid of size 1000*1000
 #
 
-KNN_Example(N=1000,D=2,Ktab=(1,3,5,7,9,11,13,15))
+KNN_Example(N=1000,D=2,Ng=1000,Ktab=(1,3,5,7,9,11,13,15))
 
-####################################################################
-# Second experiment with 500000 points, dimension 60 and 5000 classes
+####################################################
+# Second experiment with 10000 points in dimension 2
+# with evalaution over a grid of size 2000*2000
 # (only when GPU is available)
 #
 
 if IsGpuAvailable():
-    KNN_Example(N=500000,D=2,K=5)
+    KNN_Example(N=10000,D=2,Ng=2000,Ktab=(1,3,5,7,9,11,13,15))
 print("Done.")
