381e429bfcb7986b424a9e3384c9d258fbb932d0
Joan Glaunès <joan.glaunes@gmail.com>
2019-03-11 18:27:49 +0100

    improved and created PyTorch version

--- pykeops/tutorials/KNN/plot_knn_torch.py
+++ pykeops/tutorials/KNN/plot_knn_torch.py
@@ -1,12 +1,12 @@
 """
-=======================================================================
-K Nearest Neighbour algorithm using the generic syntax (NumPy bindings)
-=======================================================================
+=========================================================================
+K Nearest Neighbour algorithm using the generic syntax (PyTorch bindings)
+=========================================================================
 
 We define a dataset of :math:`N` points in :math:`\mathbb R^D` with two classes, 
 then apply a simple k-NN algorithm to evaluate the classifier over a grid.
-This example uses a pure NumPy framework (without Pytorch).
-See :ref:`here<sphx_glr__auto_tutorials_KNN_plot_knn_torch.py>` for the equivalent script using PyTorch bindings.
+This example uses the PyTorch framework.
+See :ref:`here<sphx_glr__auto_tutorials_KNN_plot_knn_numpy.py>` for the equivalent script using NumPy bindings.
 """
 
 #############################
@@ -15,13 +15,21 @@ See :ref:`here<sphx_glr__auto_tutorials_KNN_plot_knn_torch.py>` for the equivale
 
 import time
 import numpy as np
-from pykeops.numpy import Genred
-from pykeops.numpy.utils import IsGpuAvailable, WarmUpGpu
+import torch
+from pykeops.torch import Genred
+from pykeops.torch.utils import WarmUpGpu
 
 from matplotlib import pyplot as plt
 
 dtype = 'float32'  # May be 'float32' or 'float64'
+torchtype = torch.float32 if dtype == 'float32' else torch.float64
 
+if torch.cuda.is_available():
+    torchdeviceId = torch.device('cuda:0')
+    KeOpsdeviceId = torchdeviceId.index
+else:
+    torchdeviceId = torch.device('cpu')
+    
 #######################################
 #  We wrap this example into a function
 #
@@ -32,15 +40,16 @@ def KNN_Example(N,D,Ng,Ktab):
     # Define our dataset
     # cl gives class for each point in x
     #
-    x = np.random.rand(N, D).astype(dtype)
+    x = torch.rand(N, D,dtype=torchtype,device=torchdeviceId)
     def fth(x):
         return 3*x*(x-.5)*(x-1)+x
-    cl = (x[:,1]+.03*np.random.randn(N).astype(dtype)) < fth(x[:,0])
+    cl = (x[:,1]+.03*torch.randn(N,dtype=torchtype,device=torchdeviceId)) < fth(x[:,0])
 
     # building the grid for evaluation
-    tmp = np.linspace(0,1,Ng).astype(dtype)
-    g1, g2 = np.meshgrid(tmp,tmp)
-    g = np.concatenate((g1.reshape(-1,1),g2.reshape(-1,1)),axis=1)
+    tmp = torch.linspace(0,1,Ng,dtype=torchtype,device=torchdeviceId)
+    g1, g2 = torch.meshgrid(tmp,tmp)
+    g1, g2 = g1.t(), g2.t()
+    g = torch.cat((g1.reshape(-1,1),g2.reshape(-1,1)),dim=1)
 
     WarmUpGpu()
         
@@ -48,7 +57,7 @@ def KNN_Example(N,D,Ng,Ktab):
         plt.ion()
         plt.clf()  
         plt.subplot(np.ceil((len(Ktab)+1)/4),np.min([(len(Ktab)+1),4]),1)  
-        plt.scatter(x[:, 0], x[:, 1], c=cl, s=2)
+        plt.scatter(x.cpu().numpy()[:, 0], x.cpu().numpy()[:, 1], c=cl.cpu().numpy(), s=2)
         plt.imshow(np.ones((2,2)), extent=(0,1,0,1), alpha=0)
         plt.axis('off')
         plt.title('data points')
@@ -73,16 +82,16 @@ def KNN_Example(N,D,Ng,Ktab):
         #
         start = time.time()
         # calling keops to compute the K nearest neighbours
-        indKNN = my_routine(g,x).astype(int)
+        indKNN = my_routine(g,x).long()
         # classify grid points
-        clg = np.mean(cl[indKNN],axis=1)>.5
+        clg = torch.mean(cl.float()[indKNN],dim=1)>.5
         end = time.time()
         print('Time to perform K-NN classification over',Ng*Ng,'test points:', round(end - start, 5), 's')
 
         if D==2:
             plt.subplot(np.ceil((len(Ktab)+1)/4),np.min([(len(Ktab)+1),4]),i+2)
             # reshaping grid classes for display as image
-            clg = np.reshape(clg,(Ng,Ng))
+            clg = np.reshape(clg.cpu().numpy(),(Ng,Ng))
             plt.imshow(clg, extent=(0,1,0,1), origin='lower')
             plt.axis('off')
             plt.title(str(K)+'-NN classifier')
@@ -90,9 +99,7 @@ def KNN_Example(N,D,Ng,Ktab):
         print('Close the figure to continue.')
         plt.show(block=(__name__ == '__main__'))
     
-
-
-if IsGpuAvailable():
+if torch.cuda.is_available():
     ######################################################
     # On Gpu : experiment with 10000 points in dimension 2
     # with evalaution over a grid of size 2000*2000
