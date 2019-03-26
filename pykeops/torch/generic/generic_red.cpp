e674059694ec4523e30695e619445253328786ab
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2019-03-26 15:44:15 +0100

    torch/reformat generic_red.cpp

--- pykeops/torch/generic/generic_red.cpp
+++ pykeops/torch/generic/generic_red.cpp
@@ -11,22 +11,22 @@ namespace pykeops {
 /////////////////////////////////////////////////////////////////////////////////
 
 template <>
-int get_size(at::Tensor obj_ptri, int l){
+int get_size(at::Tensor obj_ptri, int l) {
     return obj_ptri.size(l);
 }
 
 template <>
-__TYPE__* get_data(at::Tensor obj_ptri){
+__TYPE__* get_data(at::Tensor obj_ptri) {
     return obj_ptri.data<__TYPE__>();
 }
 
 template <>
-__INDEX__* get_rangedata(at::Tensor obj_ptri){
+__INDEX__* get_rangedata(at::Tensor obj_ptri) {
     return obj_ptri.data<__INDEX__>();
 }
 
 template <>
-bool is_contiguous(at::Tensor obj_ptri){
+bool is_contiguous(at::Tensor obj_ptri) {
     return obj_ptri.is_contiguous();
 }
 
@@ -36,17 +36,17 @@ bool is_contiguous(at::Tensor obj_ptri){
 /////////////////////////////////////////////////////////////////////////////////
 
 #if USE_DOUBLE
-    #define AT_TYPE at::kDouble
+#define AT_TYPE at::kDouble
 #else
-    #define AT_TYPE at::kFloat
+#define AT_TYPE at::kFloat
 #endif
 
 template <>
 at::Tensor launch_keops(int tag1D2D, int tagCpuGpu, int tagHostDevice, int Device_Id,
                         int nx, int ny, int nout, int dimout,
                         int tagRanges, int nranges_x, int nranges_y, __INDEX__ **castedranges,
-                        __TYPE__ ** castedargs){
-    
+                        __TYPE__ ** castedargs) {
+
     if(tagHostDevice == 0) { // Data is located on Host
 
         auto result_array = torch::empty({nout, dimout}, at::device(at::kCPU).dtype(AT_TYPE).requires_grad(true));
@@ -58,12 +58,12 @@ at::Tensor launch_keops(int tag1D2D, int tagCpuGpu, int tagHostDevice, int Devic
                 CpuReduc_ranges(nx, ny, nranges_x, nranges_y, castedranges, get_data(result_array), castedargs);
             }
             return result_array;
-        } else if(tagCpuGpu==1) { // backend == "GPU", "GPU_1D", "GPU_2D"
+        } else if(tagCpuGpu == 1) { // backend == "GPU", "GPU_1D", "GPU_2D"
 #if USE_CUDA
             if (tagRanges == 0) { // Full M-by-N computation
-                if(tag1D2D==0) // "GPU_1D"
+                if(tag1D2D == 0) // "GPU_1D"
                     GpuReduc1D_FromHost(nx, ny, get_data(result_array), castedargs, Device_Id);
-                else if(tag1D2D==1) // "GPU_2D"
+                else if(tag1D2D == 1) // "GPU_2D"
                     GpuReduc2D_FromHost(nx, ny, get_data(result_array), castedargs, Device_Id);
             } else if (tagRanges == 1) {// Block sparsity
                 GpuReduc1D_ranges_FromHost(nx, ny, nranges_x, nranges_y, castedranges, get_data(result_array), castedargs, Device_Id);
@@ -74,16 +74,16 @@ at::Tensor launch_keops(int tag1D2D, int tagCpuGpu, int tagHostDevice, int Devic
 #endif
         }
     } else if(tagHostDevice == 1) { // Data is on the device
-#if USE_CUDA       
+#if USE_CUDA
         assert(Device_Id < std::numeric_limits<c10::DeviceIndex>::max());  // check that int will fit in a c10::DeviceIndex type
-       auto result_array = torch::empty({nout, dimout}, at::device({at::kCUDA,Device_Id}).dtype(AT_TYPE).requires_grad(true));
+        auto result_array = torch::empty({nout, dimout}, at::device({at::kCUDA,Device_Id}).dtype(AT_TYPE).requires_grad(true));
         if (tagRanges == 0) { // Full M-by-N computation
-            if(tag1D2D==0) // "GPU_1D"
+            if(tag1D2D == 0) // "GPU_1D"
                 GpuReduc1D_FromDevice(nx, ny, get_data(result_array), castedargs, Device_Id);
-            else if(tag1D2D==1) // "GPU_2D"
+            else if(tag1D2D == 1) // "GPU_2D"
                 GpuReduc2D_FromDevice(nx, ny, get_data(result_array), castedargs, Device_Id);
         } else if (tagRanges == 1) {// Block sparsity
-                GpuReduc1D_ranges_FromDevice(nx, ny, nranges_x, nranges_y, castedranges, get_data(result_array), castedargs, Device_Id);
+            GpuReduc1D_ranges_FromDevice(nx, ny, nranges_x, nranges_y, castedranges, get_data(result_array), castedargs, Device_Id);
         }
         return result_array;
 
