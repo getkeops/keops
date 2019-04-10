dd3f9669e34c6a3c1cb3cee572705ffa8f7c591a
Joan Glaunès <joan.glaunes@gmail.com>
2019-04-10 11:34:54 +0200

    ok, works on Gpu, tested softmax reduction

--- pykeops/examples/numpy/test_helper.py
+++ pykeops/examples/numpy/test_helper.py
@@ -1,31 +1,40 @@
 import numpy as np
 from pykeops import keops_formula as kf
+from pykeops.numpy.utils import WarmUpGpu
+
+import time
 
 # We use keops formula helper to compute a reduction
 # directly from data and using a syntax as close as possible to
 # its numpy counterpart
 
 # data
-x = np.random.rand(5,1,3)
-y = np.random.rand(1,10,3)
-b = np.random.rand(1,10,4)
+nx, ny = 5000, 10000
+x = np.random.rand(nx,1,3)
+y = np.random.rand(1,ny,3)
+b = np.random.rand(1,ny,4)
 sigmas = [4.1,2.3,1.05,0.5]
 
 # first with numpy
+start = time.time()
 dxy2 = np.sum(abs(x-y)**.5,axis=2)[..., np.newaxis]
 Kxy = 0
 for s in sigmas:
     Kxy += np.exp(-dxy2/s**2)
 res = (Kxy-b)[:,:,1:3].min(axis=1)
+print("Timing (Numpy implementation): ",round(time.time()-start,5),"s")
 print("numpy output : ",res)
 
 # same with keops
+WarmUpGpu()
+start = time.time()
 x, y, b = kf(x), kf(y), kf(b)
 dxy2 = kf.sum(abs(x-y)**.5,axis=2)
 Kxy = 0
 for s in sigmas:
     Kxy += kf.exp(-dxy2/s**2)
 kres = (Kxy-b)[:,:,1:3].min(axis=1)
+print("Timing (KeOps implementation): ",round(time.time()-start,5),"s")
 print("keops output : ",kres)
 
 # error
