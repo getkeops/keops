9148a69a8c9c2d9f61ea6486b76a14a972eb2860
Joan Glaunès <joan.glaunes@gmail.com>
2019-04-14 19:54:28 +0200

    tested gpu selection with container syntax, it works

---
+++ pykeops/examples/numpy/gpu_select_numpy_helper.py
@@ -0,0 +1,71 @@
+"""
+=========
+Multi GPU
+=========
+
+On multi-device clusters,
+let's see how to select the card on which a KeOps
+operation will be performed.
+
+ 
+"""
+
+###############################################################
+# Setup
+# -------------
+# Standard imports:
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+from pykeops import keops_formula as keops
+from pykeops import Vi, Vj, Pm 
+from pykeops.numpy.utils import IsGpuAvailable
+
+###############################################################
+# Define the list of gpu ids to be tested:
+
+# By default we assume that there are two GPUs available with 0 and 1 labels:
+gpuids = [0,1] if torch.cuda.device_count() > 1 else [0]
+
+
+dtype = 'float32'  # May be 'float32' or 'float64'
+
+###############################################################
+#  Generate some data, stored on the CPU (host) memory:
+#
+
+M = 1000
+N = 2000
+x = np.random.randn(M,3).astype(dtype)
+y = np.random.randn(N,3).astype(dtype)
+a = np.random.randn(N,1).astype(dtype)
+p = np.random.randn(1).astype(dtype)
+
+#########################################
+# Launch our routine on the CPU:
+#
+
+xi, yj, aj = Vi(x), Vj(y), Vj(a)
+c = ((p-aj)**2*(xi+yj).exp()).sum(axis=1, backend='CPU')
+
+#########################################
+# And on our GPUs, with copies between 
+# the Host and Device memories:
+#
+if IsGpuAvailable():
+    for gpuid in gpuids:
+        d = ((p-aj)**2*(xi+yj).exp()).sum(axis=1,backend='GPU', device_id=gpuid)
+        print('Relative error on gpu {}: {:1.3e}'.format( gpuid, 
+                float( np.sum(np.abs(c - d)) / np.sum(np.abs(c)) ) ))
+    
+        # Plot the results next to each other:
+        for i in range(3):
+            plt.subplot(3, 1, i+1)
+            plt.plot(c[:40,i],  '-', label='CPU')
+            plt.plot(d[:40,i], '--', label='GPU {}'.format(gpuid))
+            plt.legend(loc='lower right')
+
+        plt.tight_layout() ; plt.show()
+
