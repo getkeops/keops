dfc11af01dbbb9b5e1bc15a1936014b34977473e
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-06-17 16:13:59 +0200

    toward a compile time tensordot

--- keops/sandbox/test_compiletensordot.cpp
+++ keops/sandbox/test_compiletensordot.cpp
@@ -2,8 +2,6 @@
 #include <tuple>
 #include <array>
 
-#include <range/v3/all.hpp>
-using namespace ranges;
 
 constexpr std::size_t operator "" _z ( unsigned long long n )
     { return n; }
@@ -11,14 +9,12 @@ constexpr std::size_t operator "" _z ( unsigned long long n )
 # define Ind(...) std::index_sequence<__VA_ARGS__>
 
 // TensorDot(A,B,Ind(2,2,2), Ind(2,2), Ind(0,1), Ind(0,1))
+// TensorDot(A,B,{2,2,2}, {2,2}, {0,1}, {0,1))
 
 using DimFa  = Ind(2,2,2);
 using DimFb  = Ind(2,2);
 using ContFa = Ind(2);
 using ContFb = Ind(0);
-
-using DimKeepA = Ind(2,2);
-using DimKeepB = Ind(2);
 
 template<size_t... Is, size_t... is>
 static constexpr size_t get_columnmajor(std::index_sequence<is...>);
@@ -37,43 +33,77 @@ static constexpr size_t get_columnmajor(std::index_sequence<i, is...>) {
 
 
 template< size_t ... DIMFA, size_t ... DIMFB, size_t ... CONTFA, size_t ... CONTFB >
-static constexpr std::tuple<std::array< size_t, sizeof...(DIMFA)>,
-                            std::array< size_t, sizeof...(DIMFB)>,
-                            std::array< size_t, sizeof...(CONTFA)>,
-                            std::array< size_t, sizeof...(CONTFB)>> build_array4( std::index_sequence<DIMFA... >,
-                                                                                   std::index_sequence<DIMFB... >,
-                                                                                   std::index_sequence<CONTFA...>,
-                                                                                   std::index_sequence<CONTFB...> 
+static constexpr std::tuple<std::array< size_t, sizeof...(DIMFA)> ,
+                            std::array< size_t, sizeof...(DIMFB)> ,
+                            std::array< size_t, sizeof...(DIMFA)-sizeof...(CONTFA)>,
+                            std::array< size_t, sizeof...(DIMFB)-sizeof...(CONTFB)>,
+                            std::array< size_t, sizeof...(DIMFA) + sizeof...(DIMFB)- sizeof...(CONTFA) - sizeof...(CONTFB)>,
+                            std::array< size_t, sizeof...(DIMFA) + sizeof...(DIMFB)- sizeof...(CONTFA)                    > > build_array4( std::index_sequence<DIMFA... >,
+                                                                                  std::index_sequence<DIMFB... >,
+                                                                                  std::index_sequence<CONTFA...>,
+                                                                                  std::index_sequence<CONTFB...> 
                                                                                  ) noexcept {
-   return std::make_tuple(std::array<size_t, sizeof...(DIMFA ) > { DIMFA... },
-                          std::array<size_t, sizeof...(DIMFB ) > { DIMFB... },
-                          std::array<size_t, sizeof...(CONTFA) > { CONTFA... },
-                          std::array<size_t, sizeof...(CONTFB) > { CONTFB... });
+                                                           
+   // Cast index_sequence to array
+   auto dimfa  = std::array<size_t, sizeof...(DIMFA ) > { DIMFA... };
+   auto dimfb  = std::array<size_t, sizeof...(DIMFB ) > { DIMFB... };
+   auto contfa = std::array<size_t, sizeof...(CONTFA) > { CONTFA... };
+   auto contfb = std::array<size_t, sizeof...(CONTFB) > { CONTFB... };
+
+   // get size of the contraction
+   constexpr size_t number_of_dim_a = dimfa.size() - contfa.size() ;
+   constexpr size_t number_of_dim_b = dimfb.size() - contfb.size() ;
+
+   std::array<size_t, number_of_dim_b + number_of_dim_a> dim_keep{};
+   std::array<size_t, number_of_dim_a> dim_keep_a{};
+   std::array<size_t, number_of_dim_b> dim_keep_b{};
+
+   for(auto i=0; i < number_of_dim_a; i++) {
+        dim_keep_a[i]= dimfa[i];
+        dim_keep[i] = dimfa[i];
+   }
+
+   for(auto i=0; i < number_of_dim_b; i++) {
+        dim_keep_b[i]= dimfb[contfb.size() + i];
+        dim_keep[i+ number_of_dim_a] = dim_keep_b[i];
+   }
+                                                              
+   std::array<size_t, contfa.size()> dim_sum{};
+   for(auto i=0; i < contfa.size(); i++) {
+        dim_sum[i]= dimfa[number_of_dim_a + i];
+   }
+   
+
+   std::array<size_t, contfa.size() + dim_keep.size()> dim_tot{};
+   for(auto i=0; i < dim_keep.size(); i++) {
+        dim_tot[i]= dim_keep[i];
+   }
+   for(auto i=0; i < contfa.size(); i++) {
+        dim_tot[dim_keep.size() + i]= dim_sum[i];
+   }
+
+   return std::make_tuple(dimfa, dimfb, 
+                          dim_keep_a, dim_keep_b, dim_keep,
+                          dim_tot) ;
 }
 
 template <size_t shape_index, size_t shape_size>
-struct Looper
-{
+struct Looper {
     template <typename Functor>
-    void operator()(const std::array<size_t, shape_size>& shape, Functor functor)
-    {
-        for (size_t index = 0; index < shape[shape_index]; ++index)
-        {
-            Looper<shape_index + 1, shape_size>()
-                (
+    void operator()(const std::array<size_t, shape_size>& shape, Functor functor) {
+        for (size_t index = 0; index < shape[shape_index]; ++index) {
+            Looper<shape_index + 1, shape_size>() (
                     shape,
-                    [index, &functor](auto... tail){ functor(index, tail...); }
-                );
+                    [index, &functor](auto... tail){ functor(std::forward_as_tuple(index, tail...)); }
+                    );
         }
     }
 };
 
 template <size_t shape_size>
-struct Looper<shape_size, shape_size>
-{
+struct Looper<shape_size, shape_size> {
     template <typename Functor>
-    void operator()(const std::array<size_t, shape_size>&, Functor functor)
-    {
+    void operator()(const std::array<size_t, shape_size>&, Functor functor) {
         functor();
     }
 };
@@ -84,7 +114,7 @@ void loop(const std::array<size_t, shape_size>& shape, Functor functor)
     Looper<0, shape_size>()(shape, functor);
 }
 
-constexpr unsigned cilog2(unsigned val) { return val ? 1 + cilog2(val >> 1) : -1; }
+//constexpr unsigned cilog2(unsigned val) { return val ? 1 + cilog2(val >> 1) : -1; }
 
 template <size_t size1, size_t size2>
 constexpr std::tuple<size_t,size_t,size_t> kd(std::array<size_t, size1> dim_a, std::array<size_t, size2> dim_b, size_t i, size_t j , size_t k) {
@@ -94,17 +124,34 @@ constexpr std::tuple<size_t,size_t,size_t> kd(std::array<size_t, size1> dim_a, s
     return std::make_tuple(I,kda,kdb);
 }
 
+template <size_t NumberOfKeepDim>
+constexpr size_t dimout(std::array<size_t, NumberOfKeepDim> keep_dim){
+    size_t out = 1;  
+    for(size_t i : keep_dim)
+      out *= i;
+
+    return out;
+}
+
+//template< size_t ... A>
+//static constexpr std::array< size_t, sizeof...(A)> toArray( std::index_sequence<A... >) noexcept {
+    //return std::array<size_t, sizeof...(A ) > { A... };
+//}
+
+
+
 int main() {
+
+    double FA[8] = {4.4,5.4,6.2,6.5,7.5,6.1,8.7,1.3};
+    double FB[4] = {1.4,1.2,1.5,1.22};
 
     // generate tuple (at compile time)
     constexpr auto ma4 = build_array4( DimFa(),
-                                                                     DimFb(),
-                                                                     ContFa(),
-                                                                     ContFb() );
+                                       DimFb(),
+                                       ContFa(),
+                                       ContFb() );
      
      
-    double FA[8] = {4.4,5.4,6.2,6.5,7.5,6.1,8.7,1.3};
-    double FB[4] = {1.4,1.2,1.5,1.22};
  
     // constexpr size_t sum_dim_a = 2;
     // constexpr size_t keep_dim_a[2] = [0,1];
@@ -113,32 +160,23 @@ int main() {
     // constexpr size_t keep_dim_b[1] = [1];
 
     // print (at runtime)
-    double out[2*2*2] = {0,0,0,0,0,0,0,0};
-
-    size_t I = 0;
-    loop(std::array<size_t,4>{2,2,2,2},[&I,&out,&FA,&FB,&ma4](size_t i,size_t j,size_t k, size_t r){
+    
+    constexpr size_t dimout_var = dimout(std::get<4>(ma4)) ;
+    double out[dimout_var];
+    std::fill(out, out + dimout_var, 0);
+   
+    loop(std::get<5>(ma4),[&out,&FA,&FB,&ma4](std::tuple<size_t,size_t,size_t,size_t> it){
         const auto& dim_a = std::get<0>(ma4);
         const auto& dim_b = std::get<1>(ma4);
 
-        std::tuple KD = kd(dim_a,dim_b,i,j,k);
+        std::tuple KD = kd(dim_a,dim_b,std::get<0>(it),std::get<1>(it),std::get<2>(it));
         size_t I   = std::get<0>(KD);
         size_t kda = std::get<1>(KD);
         size_t kdb = std::get<2>(KD);
-        out[I] += FA[kda + r] * FB[dim_b[1]*r + kdb];
+        out[I] += FA[kda + std::get<3>(it)] * FB[dim_b[1]*std::get<3>(it) + kdb];
     });
  
-//    std::cout << "out = " << (out | view::all) << std::endl;
-
-    // constexpr size_t base2 = cilog2(8); 
-    // std::array<size_t, cilog2(8)> ijk;
-
-    // loop(std::array<size_t,2>{8,2},[&out,&FA,&FB,&ijk](size_t I, size_t r){
-    //     size_t i = I / 8;
-    //     size_t j = (I-i) / 4;
-    //     size_t k = (I-j-k) / 2;
-    
-    //     out[I] += FA[ 4*i + 2*j + r] * FB[ r*2 + k];
-    // });
+    // -------------------------------------------------------------------------------------------------------------------------------------
     double out2[2*2*2] = {0,0,0,0,0,0,0,0};
 
     for(size_t i=0; i<2; i++)
@@ -149,7 +187,12 @@ int main() {
         }
 
 
-
-   std::cout << "out  = " << (out | view::all) << std::endl;
-   std::cout << "out2 = " << (out2 | view::all) << std::endl;
-}
+   for (auto i=0; i< 8; i++) {
+       std::cout << "out  = " << out[i]  << std::endl;
+   }
+   
+   for (auto i=0; i< 8; i++) {
+      std::cout << "out2 = " << out2[i]  << std::endl;
+   }
+   
+   }
