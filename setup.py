16e92f1ae0aeb60cd2cb825090ddd9fc3fab3c77
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2019-07-09 16:43:07 +0200

    Add scikit-learn to optional dep of pykeops

--- setup.py
+++ setup.py
@@ -160,6 +160,7 @@ setup(
                      'imageio',
                      'torch',
                      'gpytorch',
+                     'scikit-learn',
                      ],
             },
 )
