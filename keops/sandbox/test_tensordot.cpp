d0b70f7c893fac3c6890fc70c615aa76afe0862e
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-07-19 12:07:01 +0200

    tensordot working!

--- keops/sandbox/test_tensordot.cpp
+++ keops/sandbox/test_tensordot.cpp
@@ -2,30 +2,42 @@
 #include <tuple>
 #include <array>
 
-constexpr std::size_t operator "" _z(unsigned long long n) {
-  return n;
-}
 
 #define Ind(...) std::index_sequence<__VA_ARGS__>
 
-// TensorDot(A,B,Ind(2,2,2), Ind(2,2), Ind(0,1), Ind(0,1))
-// TensorDot(A,B,{2,2,2}, {2,2}, {0,1}, {0,1))
-
-using DimFa = Ind(2, 2, 2);
-using DimFb = Ind(2, 2);
-using ContFa = Ind(2);
-using ContFb = Ind(0);
-
-using ContFa4 = Ind(1,2);
-using ContFb4 = Ind(0,1);
 
 
-using DimFa6 = Ind(5, 4, 3);
-using DimFb6 = Ind(4, 3, 2);
-using ContFa6 = Ind(1,2);
-using ContFb6 = Ind(0,1);
+//----------------------------------------------------------------------------------------------------------------------
 
-using ContFa8 = Ind(1,2,3);
+// Standard loop
+template<size_t shape_index, size_t shape_size>
+struct Looper {
+  template<typename Functor>
+  void operator()(const std::array<size_t, shape_size> &shape, Functor functor) {
+    for (size_t index = 0; index < shape[shape_index]; ++index) {
+      Looper<shape_index + 1, shape_size>()(
+          shape,
+          [index, &functor](auto... tail) { functor(std::tuple_cat(std::tuple<size_t>(index), tail...)); });
+    }
+  }
+};
+
+// Stopping condition
+template<size_t shape_size>
+struct Looper<shape_size, shape_size> {
+  template<typename Functor>
+  void operator()(const std::array<size_t, shape_size> &, Functor functor) {
+    functor();
+  }
+};
+
+template<size_t shape_size, typename Functor>
+void loop(const std::array<size_t, shape_size> &shape, Functor functor) {
+  Looper<0, shape_size>()(shape, functor);
+}
+
+
+
 
 template<size_t... DIMFA, size_t... DIMFB, size_t... CONTFA, size_t... CONTFB>
 static constexpr std::tuple<std::array<size_t, sizeof...(DIMFA)>,
@@ -89,45 +101,6 @@ build_array4(std::index_sequence<DIMFA...>,
                          dim_tot);
 }
 
-template<size_t shape_index, size_t shape_size>
-struct Looper {
-  template<typename Functor>
-  void operator()(const std::array<size_t, shape_size> &shape, Functor functor) {
-    for (size_t index = 0; index < shape[shape_index]; ++index) {
-      Looper<shape_index + 1, shape_size>()(
-          shape,
-          [index, &functor](auto... tail) { functor(std::tuple_cat(std::tuple<size_t>(index), tail...)); });
-    }
-  }
-};
-
-template<size_t shape_size>
-struct Looper<shape_size, shape_size> {
-  template<typename Functor>
-  void operator()(const std::array<size_t, shape_size> &, Functor functor) {
-    functor();
-  }
-};
-
-template<size_t shape_size, typename Functor>
-void loop(const std::array<size_t, shape_size> &shape, Functor functor) {
-  Looper<0, shape_size>()(shape, functor);
-}
-
-//constexpr unsigned cilog2(unsigned val) { return val ? 1 + cilog2(val >> 1) : -1; }
-
-template<size_t size1, size_t size2>
-constexpr std::tuple<size_t, size_t, size_t> kd(std::array<size_t, size1> dim_a,
-                                                std::array<size_t, size2> dim_b,
-                                                size_t i,
-                                                size_t j,
-                                                size_t k) {
-  size_t kda = dim_a[1] * dim_a[2] * i + dim_a[2] * j;
-
-  size_t kdb = k;
-  size_t I = kda + kdb;
-  return std::make_tuple(I, kda, kdb);
-}
 
 template<size_t size_list_dim_a, size_t size_list_dim_b, size_t size_list_contdim>
 constexpr std::tuple<size_t, size_t, size_t> kdvar(std::array<size_t, size_list_dim_a> list_dim_a,
@@ -201,34 +174,12 @@ constexpr std::tuple<size_t, size_t, size_t> kdvar(std::array<size_t, size_list_
   }
 
   for (size_t i = 0; i < size_keep_dim_a + size_keep_dim_b  ; i++) {
-    //I += ((i == 0) ? 1 : list_dim_b[i]) * (list_indices_keepdim_b[i] + I);
     I += list_stride_keepdim[i] * (( i < size_keep_dim_a) ? list_indices_keepdim_a[i] : list_indices_keepdim_b[i - size_keep_dim_a]);
   }
 
-  std::cout << "(" << list_indices_tot[0] << "," << list_indices_tot[1] <<  "," <<list_indices_tot[2] <<  "," <<list_indices_tot[3] <<")     " << kda << " " << kdb << " " << I << std::endl;
+  // std::cout << "(" << list_indices_tot[0] << "," << list_indices_tot[1] <<  "," <<list_indices_tot[2] <<  "," <<list_indices_tot[3] <<")     " << kda << " " << kdb << " " << I << std::endl;
 
   return std::make_tuple(I, kda, kdb);
-}
-
-template<size_t NumberOfKeepDim>
-constexpr size_t dimout(std::array<size_t, NumberOfKeepDim> keep_dim) {
-  size_t out = 1;
-  for (size_t i : keep_dim)
-    out *= i;
-
-  return out;
-}
-
-//template< size_t ... A>
-//static constexpr std::array< size_t, sizeof...(A)> toArray( std::index_sequence<A... >) noexcept {
-//return std::array<size_t, sizeof...(A ) > { A... };
-//}
-//
-
-template<typename tuple_t>
-constexpr auto get_array_from_tuple(tuple_t &&tuple) {
-  constexpr auto get_array = [](auto &&... x) { return std::array{std::forward<decltype(x)>(x) ...}; };
-  return std::apply(get_array, std::forward<tuple_t>(tuple));
 }
 
 template<size_t, class T>
@@ -240,11 +191,43 @@ auto gen(std::index_sequence<Is...>) { return std::tuple<T_<Is, T>...>{}; }
 template<class T, size_t N>
 auto gen() { return gen<T>(std::make_index_sequence<N>{}); }
 
-struct KD_T {
+template<size_t NumberOfKeepDim>
+constexpr size_t dimout(std::array<size_t, NumberOfKeepDim> keep_dim) {
+  size_t out = 1;
+  for (size_t i : keep_dim)
+    out *= i;
 
-};
+  return out;
+}
 
-// constexpr std::tuple<size_t,size_t,size_t> kd(std::array<size_t, size1> dim_a, std::array<size_t, size2> dim_b, size_t i, size_t j , size_t k)
+
+template<typename tuple_t>
+constexpr auto get_array_from_tuple(tuple_t &&tuple) {
+  constexpr auto get_array = [](auto &&... x) { return std::array{std::forward<decltype(x)>(x) ...}; };
+  return std::apply(get_array, std::forward<tuple_t>(tuple));
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+using DimFa = Ind(2, 2, 2);
+using DimFb = Ind(2, 2);
+
+using ContFa = Ind(2);
+using ContFb = Ind(0);
+using ContFa4 = Ind(1,2);
+using ContFb4 = Ind(0,1);
+
+
+using DimFa6 = Ind(5, 4, 3);
+using DimFb6 = Ind(4, 3, 2);
+
+using ContFa6 = Ind(1,2);
+using ContFb6 = Ind(0,1);
+
+using ContFa8 = Ind(1,2,3);
+
+
 
 int main() {
 
@@ -257,44 +240,17 @@ int main() {
                                     ContFa(),
                                     ContFb());
 
-  // constexpr size_t sum_dim_a = 2;
-  // constexpr size_t keep_dim_a[2] = [0,1];
-
-  // constexpr size_t sum_dim_b[1] = 0;
-  // constexpr size_t keep_dim_b[1] = [1];
-
-  // print (at runtime)
 
   constexpr size_t dimout_var = dimout(std::get<4>(ma4));
-  double out[dimout_var];
-  std::fill(out, out + dimout_var, 0);
-
-  const auto &my_lambda = [&out, &FA, &FB, &ma4](decltype(gen<size_t, std::get<6>(ma4).size()>()) it) {
-    const auto &dim_a = std::get<0>(ma4);
-    const auto &dim_b = std::get<1>(ma4);
-
-    std::tuple KD = kd(dim_a, dim_b, std::get<0>(it), std::get<1>(it), std::get<2>(it));
-    size_t I = std::get<0>(KD);
-    size_t kda = std::get<1>(KD);
-    size_t kdb = std::get<2>(KD);
-
-    out[I] += FA[kda + std::get<3>(it)] * FB[kdb + dim_b[1] * std::get<3>(it)];
-  };
-
-  loop(std::get<6>(ma4), my_lambda);
-
-  for (auto i = 0; i < 8; i++) {
-    std::cout << "out  = " << out[i] << std::endl;
-  }
 
   // -------------------------------------------------------------------------------------------------------------------------------------
 
-  double out3[dimout_var];
-  std::fill(out3, out3 + dimout_var, 0);
+  double out_td[dimout_var];
+  std::fill(out_td, out_td + dimout_var, 0);
 
   constexpr const size_t indices_number = std::get<6>(ma4).size();
 
-  const auto &my_lambda2 = [&out3, &FA, &FB, &ma4, indices_number](decltype(gen<size_t, indices_number>()) it) {
+  const auto &my_lambda2 = [&out_td, &FA, &FB, &ma4, indices_number](decltype(gen<size_t, indices_number>()) it) {
     const auto &dim_a = std::get<0>(ma4);
     const auto &dim_b = std::get<1>(ma4);
 
@@ -304,49 +260,38 @@ int main() {
     size_t kda = std::get<1>(KD);
     size_t kdb = std::get<2>(KD);
 
-    out3[I] += FA[kda] * FB[kdb];
+    out_td[I] += FA[kda] * FB[kdb];
   };
 
   loop(std::get<6>(ma4), my_lambda2);
 
-  for (auto i = 0; i < 8; i++) {
-    std::cout << "out3 = " << out3[i] << std::endl;
-  }
+  // --------
 
-  // -------------------------------------------------------------------------------------------------------------------------------------
-  double out2[2 * 2 * 2] = {0, 0, 0, 0, 0, 0, 0, 0};
+  double out_loop[2 * 2 * 2] = {0, 0, 0, 0, 0, 0, 0, 0};
 
   for (size_t i = 0; i < 2; i++)
     for (size_t j = 0; j < 2; j++)
       for (size_t k = 0; k < 2; k++)
         for (size_t l = 0; l < 2; l++) {
-          size_t kda = 4 * i + 2 * j + l;
-          size_t kdb = l * 2 + k;
-          size_t I = 4 * i + 2 * j + k;
-          std::cout << "(" << i << "," << j <<  "," << k <<  "," << l <<")     " << kda << " " << kdb << " " << I << std::endl;
-          out2[4 * i + 2 * j + k] += FA[4 * i + 2 * j + l] * FB[l * 2 + k];
+          // size_t kda = 4 * i + 2 * j + l;
+          // size_t kdb = l * 2 + k;
+          // size_t I = 4 * i + 2 * j + k;
+          // std::cout << "(" << i << "," << j <<  "," << k <<  "," << l <<")     " << kda << " " << kdb << " " << I << std::endl;
+          out_loop[4 * i + 2 * j + k] += FA[4 * i + 2 * j + l] * FB[l * 2 + k];
         }
 
+
+  double res = 0;
   for (auto i = 0; i < 8; i++) {
-    std::cout << "out2 = " << out2[i] << std::endl;
+    // std::cout << "out_tensordot = " << out_td[i] << std::endl;
+    // std::cout << "out_loop = " << out_loop[i] << std::endl;
+    res += (out_td[i] - out_loop[i]) * (out_td[i] - out_loop[i]);
   }
 
-  std::cout << std::endl;
+  std::cout << "error = " << res << std::endl<< std::endl;
 
 
-  // -------------------------------------------------------------------------------------------------------------------------------------
-  double out4[2] = {0, 0};
-
-  for (size_t i = 0; i < 2; i++)
-      for (size_t k = 0; k < 2; k++)
-        for (size_t l = 0; l < 2; l++) {
-
-          out4[ i ] += FA[4 * i + 2 * k + l] * FB[k * 2 + l];
-        }
-
-  for (auto i = 0; i < 2; i++) {
-    std::cout << "out4 = " << out4[i] << std::endl;
-  }
+  // -------------------------------------------------------------------------------------------------------------------
 
   double out5[2] = {0, 0};
   constexpr auto ma5 = build_array4(DimFa(),
@@ -371,33 +316,31 @@ int main() {
 
   loop(std::get<6>(ma5), my_lambda4);
 
-  for (auto i = 0; i < 2; i++) {
-    std::cout << "out5 = " << out5[i] << std::endl;
-  }
+  // -------------
 
+  double out4[2] = {0, 0};
 
-  // ----------------------------------------
-  double FAA[60] =  {7, 9, 9, 5, 8, 3, 6, 9, 6, 0, 5, 7, 3, 4, 3, 5, 3, 3,0, 9, 9,6, 0, 3,3, 7, 0,8, 6, 0,6, 1, 3,1, 4, 7,3, 9, 8,8, 3, 7,2, 3, 1,9, 5, 7,7, 5, 9,7, 0, 1,9, 7, 5,0, 3, 8};
-  double FBB[24] =  {6, 4,2, 9,9, 5,1, 6,7, 8,2, 4,1, 9,7, 8,5, 4,3, 2,3, 8,5, 7};
+  for (size_t i = 0; i < 2; i++)
+    for (size_t k = 0; k < 2; k++)
+      for (size_t l = 0; l < 2; l++) {
 
-  double out7[10] =  {0, 0, 0, 0 ,0, 0, 0, 0, 0 ,0};
-
-  for (size_t i = 0; i < 5; i++)
-    for (size_t j = 0; j < 2; j++)
-      for (size_t k = 0; k < 4; k++)
-        for (size_t l = 0; l < 3; l++) {
-
-          size_t kda = 12 * i + 3 * k + l;
-          size_t kdb = 6 * k + 2 * l + j;
-          size_t I = 2 * i + j;
-          std::cout << "(" << i << "," << j <<  "," << k <<  "," << l <<")     " << kda << " " << kdb << " " << I << std::endl;
-          out7[2 * i + j] += FAA[ 12 * i + 3 * k + l] * FBB[6 * k + 2 * l + j];
+        out4[ i ] += FA[4 * i + 2 * k + l] * FB[k * 2 + l];
       }
 
-  for (auto i = 0; i < 10; i++) {
-    std::cout << "out7 = " << out7[i] << std::endl;
+  double res2 = 0;
+  for (auto i = 0; i < 2; i++) {
+    // std::cout << "out4 = " << out4[i] << std::endl;
+    // std::cout << "out5 = " << out5[i] << std::endl;
+    res2 += (out4[i] - out5[i]) * (out4[i] - out5[i]);
   }
 
+  std::cout << "error = " << res2 << std::endl<< std::endl;
+
+
+  // -------------------------------------------------------------------------------------------------------------------
+
+  double FAA[60] =  {7, 9, 9, 5, 8, 3, 6, 9, 6, 0, 5, 7, 3, 4, 3, 5, 3, 3,0, 9, 9,6, 0, 3,3, 7, 0,8, 6, 0,6, 1, 3,1, 4, 7,3, 9, 8,8, 3, 7,2, 3, 1,9, 5, 7,7, 5, 9,7, 0, 1,9, 7, 5,0, 3, 8};
+  double FBB[24] =  {6, 4,2, 9,9, 5,1, 6,7, 8,2, 4,1, 9,7, 8,5, 4,3, 2,3, 8,5, 7};
 
 
   double out6[10] = {0, 0, 0, 0 ,0, 0, 0, 0, 0 ,0};
@@ -408,8 +351,8 @@ int main() {
 
   constexpr const size_t indices_number6 = std::get<6>(ma6).size();
   const auto &my_lambda6 = [&out6, &FAA, &FBB, &ma6, indices_number6](decltype(gen<size_t, indices_number6>()) it) {
-  const auto &dim_a6 = std::get<0>(ma6);
-  const auto &dim_b6 = std::get<1>(ma6);
+    const auto &dim_a6 = std::get<0>(ma6);
+    const auto &dim_b6 = std::get<1>(ma6);
 
     std::tuple KD = kdvar<3, 3, 2>(dim_a6,
                                    dim_b6,
@@ -423,11 +366,30 @@ int main() {
 
   loop(std::get<6>(ma6), my_lambda6);
 
+  // -------------
+
+  double out7[10] =  {0, 0, 0, 0 ,0, 0, 0, 0, 0 ,0};
+
+  for (size_t i = 0; i < 5; i++)
+    for (size_t j = 0; j < 2; j++)
+      for (size_t k = 0; k < 4; k++)
+        for (size_t l = 0; l < 3; l++) {
+          // size_t kda = 12 * i + 3 * k + l;
+          // size_t kdb = 6 * k + 2 * l + j;
+          // size_t I = 2 * i + j;
+          // std::cout << "(" << i << "," << j <<  "," << k <<  "," << l <<")     " << kda << " " << kdb << " " << I << std::endl;
+          out7[2 * i + j] += FAA[ 12 * i + 3 * k + l] * FBB[6 * k + 2 * l + j];
+        }
+
+  double res3=0;
   for (auto i = 0; i < 10; i++) {
-    std::cout << "out6 = " << out6[i] << std::endl;
+    // std::cout << "out7 = " << out7[i] << std::endl;
+    // std::cout << "out6 = " << out6[i] << std::endl;
+    res3 += (out6[i] - out7[i]) * (out6[i] - out7[i]);
   }
 
-  // -----------------
+  std::cout << "error = " << res3 << std::endl<< std::endl;
+  // -------------------------------------------------------------------------------------------------------------------
 
   double out8[1] = {0};
   constexpr auto ma8 = build_array4(DimFa6(),
@@ -436,7 +398,7 @@ int main() {
                                     ContFa8());
 
   constexpr const size_t indices_number8 = std::get<6>(ma8).size();
-  const auto &my_lambda8 = [&out8, &FAA, &FBB, &ma8, indices_number8](decltype(gen<size_t, indices_number8>()) it) {
+  const auto &my_lambda8 = [&out8, &FAA, &ma8, indices_number8](decltype(gen<size_t, indices_number8>()) it) {
     const auto &dim_a8 = std::get<0>(ma8);
     const auto &dim_b8 = std::get<1>(ma8);
 
@@ -452,32 +414,31 @@ int main() {
 
   loop(std::get<6>(ma8), my_lambda8);
 
+
+  // -------------
+
+  double out9[1] = {0};
+
+  for (size_t i = 0; i < 5; i++)
+    for (size_t j = 0; j < 4; j++)
+      for (size_t k = 0; k < 3; k++) {
+          // size_t kda = 12 * i + 3 * j + k;
+          // size_t kdb = 12 * i + 3 * j + k;
+          // size_t I = 0;
+          // std::cout << "(" << i << "," << j <<  "," << k <<  "," << l <<")     " << kda << " " << kdb << " " << I << std::endl;
+          out9[0] += FAA[ 12 * i + 3 * j + k] * FAA[12 * i + 3 * j + k];
+        }
+
+  double res4=0;
   for (auto i = 0; i < 1; i++) {
-    std::cout << "out8 = " << out8[i] << std::endl;
+    // std::cout << "out7 = " << out7[i] << std::endl;
+    // std::cout << "out6 = " << out6[i] << std::endl;
+    // std::cout << "out8 = " << out8[i] << std::endl;
+    res4 += (out8[i] - out9[i]) * (out8[i] - out9[i]);
   }
+
+  std::cout << "error = " << res4 << std::endl<< std::endl;
+
 }
 
 
-/*
-
- import numpy as np
- a = np.array([4.4, 5.4, 6.2, 6.5, 7.5, 6.1, 8.7, 1.3]).reshape(2,2,2)
- b = np.array([1.4, 1.2, 1.5, 1.22]).reshape(2,2)
- print(np.tensordot(a,b,axes=([2],[0])))
-
- print(np.tensordot(a,b,axes=([1,2],[0,1]))) # out4
-
-
- a= np.array([[[7, 9, 9], [5, 8, 3], [6, 9, 6], [0, 5, 7]], [[3, 4, 3], [5, 3, 3],[0, 9, 9],[6, 0, 3]],[[3, 7, 0],[8, 6, 0],[6, 1, 3],[1, 4, 7]],
-[[3, 9, 8],[8, 3, 7],[2, 3, 1],[9, 5, 7]],[[7, 5, 9],[7, 0, 1],[9, 7, 5],[0, 3, 8]]])
-
-b = np.array([[[6, 4],[2, 9],[9, 5]],[[1, 6],[7, 8],[2, 4]],[[1, 9],[7, 8],[5, 4]],[[3, 2],[3, 8],[5, 7]]])
-
-np.tensordot(a,b,axes=([1,2],[0,1]))
-array([[357, 499],
-       [226, 270],
-       [160, 328],
-       [256, 386],
-       [274, 401]])
-
- */
