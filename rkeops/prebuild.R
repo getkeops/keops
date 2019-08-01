70d07a5682c9da6fe7dbbed0030c70faefc29759
GD <gd.dev@libertymail.net>
2019-08-01 15:44:28 +0200

    check for R files with actual code

--- rkeops/prebuild.R
+++ rkeops/prebuild.R
@@ -23,9 +23,19 @@ prebuild <- function() {
     # remove previous Collate field and listed files
     command <- paste0("sed -i '/^Collate:/,$ d' ", filename)
     tmp <- system(command)
-    # add new Collate field at end of file with currant file list
-    files <- paste(c("Collate:", 
-                     list.files(path="rkeops/R", recursive = TRUE)), 
+    # R file list
+    filename_list <- list.files(path=file.path(pkgdir, "R"), recursive = TRUE)
+    full_filename_list <- list.files(path=file.path(pkgdir, "R"), 
+                                     recursive = TRUE, full.names = TRUE)
+    R_files <- which(grepl(pattern=".R$", filename_list))
+    filename_list <- filename_list[R_files]
+    full_filename_list <- full_filename_list[R_files]
+    # check for empty files (or files with only commented lines)
+    commands <- paste0("grep -vc '^#' ", full_filename_list, " || true")
+    tmp <- as.numeric(unlist(lapply(commands, system, intern=TRUE)))
+    nonempty_files <- which(tmp>0)
+    # add new Collate field at end of file with current file list
+    files <- paste(c("Collate:", filename_list[nonempty_files]), 
                    collapse = "\n    ")
     command <- paste0("printf '%b' \"", files, "\n\" >> ", filename)
     tmp <- system(command)
