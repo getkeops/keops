cd7274c16b6c5f2d75e5e77048df9f95dedee276
Dougal J. Sutherland <dougal@gmail.com>
2019-08-01 15:17:22 +0100

    set_path: include python version in cache dir name

--- pykeops/common/set_path.py
+++ pykeops/common/set_path.py
@@ -1,5 +1,8 @@
 import os.path
+import sys
+
 import pykeops
+
 
 ###########################################################
 #             Set build_folder
@@ -10,17 +13,20 @@ def set_build_folder():
     This function set a default build folder that contains the python module compiled
     by pykeops.
     """
-    bf_source = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + '..' + os.path.sep + 'build' + os.path.sep
+    bf_source = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'build')
     bf_home = os.path.expanduser('~')
+    name = 'pykeops-{}-{}'.format(pykeops.__version__, sys.implementation.cache_tag)
 
     if os.path.isdir(bf_source): # assume we are loading from source
-        build_folder  = bf_source
-    elif os.path.isdir(bf_home): # assume we are ussing wheel and home is accessible
-       build_folder = bf_home + os.path.sep + '.cache' + os.path.sep + 'pykeops-' + pykeops.__version__ + os.path.sep 
+        build_folder = bf_source
+    elif os.path.isdir(bf_home): # assume we are using wheel and home is accessible
+        build_folder = os.path.join(bf_home, '.cache', name)
     else: 
         import tempfile
-        build_folder = tempfile.mkdtemp(prefix='pykeops-' + pykeops.__version__) + os.path.sep
-        
+        build_folder = tempfile.mkdtemp(prefix=name)
+    
+    if not build_folder.endswith(os.path.sep):
+        build_folder += os.path.sep
     os.makedirs(build_folder, exist_ok=True)
 
     return build_folder
