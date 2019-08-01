39c261bfc1118a5617a65494b4d443f9bd91af35
GD <gd.dev@libertymail.net>
2019-08-01 17:08:41 +0200

    typo in test

--- rkeops/tests/testthat/test-utils.R
+++ rkeops/tests/testthat/test-utils.R
@@ -1,16 +1,16 @@
 context("utils functions")
 
 test_that("get_pkg_dir", {
-    expect_match(get_pkg_dir(), find.package("rkeops"))
+    expect_equal(get_pkg_dir(), find.package("rkeops"))
 })
 
 test_that("get_src_dir", {
-    expect_match(get_src_dir(), 
+    expect_equal(get_src_dir(), 
                  file.path(find.package("rkeops"), "include"))
 })
 
 test_that("get_build_dir", {
-    expect_match(get_build_dir(), 
+    expect_equal(get_build_dir(), 
                  file.path(find.package("rkeops"), "build"))
     expect_true(dir.exists(file.path(find.package("rkeops"), "build")))
 })
