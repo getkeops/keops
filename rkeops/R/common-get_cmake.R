f0c066030f739ec48a99194ae21b1cbd5ec8c36d
GD <gd.dev@libertymail.net>
2019-08-02 15:48:51 +0200

    functions to get cmake executable and check its version

--- rkeops/R/common-get_cmake.R
+++ rkeops/R/common-get_cmake.R
@@ -1,63 +1,92 @@
-# function cmake = getcmake()
-#     % check wether cmake is available from Matlab. Since matlab overrides
-#     % default search path, we may need to ask the user to enter its 
-#     % location manually the first time and then save the path in a local file
-#     S = dbstack('-completenames');
-#     pathtocmakefile = [fileparts(S(1).file),'/pathtocmake'];
-#     if exist(pathtocmakefile,'file')
-#         fid = fopen(pathtocmakefile);
-#         pathtocmake = fgetl(fid);
-#         fclose(fid);
-#     else
-#         pathtocmake = '';
-#     end
-#     [testcmake,~]=system([pathtocmake,'cmake']);
-#     if testcmake==127 % cmake is not found
-#         system(['rm -f ',pathtocmakefile]);
-#         [testcmake,~]=system('cmake');
-#         if testcmake==127
-#             pathtocmake = '';
-#         else
-#             pathtocmake = input('cmake command is required but was not found. Enter path to cmake command here : ','s');
-#             if length(pathtocmake)>4 && strcmp(pathtocmake(end-4:end),'cmake')
-#                 pathtocmake = pathtocmake(1:end-5);
-#             end
-#             if ~isempty(pathtocmake) && pathtocmake(end)~='/'
-#                 pathtocmake = [pathtocmake,'/'];
-#             end
-#             [testcmake,~]=system([pathtocmake,'cmake']);
-#             if testcmake==127
-#                 error('cmake command not found.')
-#             end
-#             if ~isempty(pathtocmake)
-#                 fid = fopen(pathtocmakefile,'w');
-#                 fprintf(fid,pathtocmake);
-#                 fclose(fid);
-#             end
-#         end
-#     end
-#     % now cmake is found, but it may not work
-#     % usually the problem comes from wrong paths to shared libraries
-#     % we try several workarounds
-#     [testcmake,~]=system([pathtocmake,'cmake']);
-#     if testcmake==1        
-#         tmp = getenv('LD_PRELOAD');
-#         % first workaround
-#         setenv('LD_PRELOAD','')
-#         [testcmake,~]=system([pathtocmake,'cmake']);
-#         if testcmake==1
-#             % second workaround
-#             setenv('LD_PRELOAD','/usr/lib/x86_64-linux-gnu/libstdc++.so.6')
-#             [testcmake,~]=system([pathtocmake,'cmake']);
-#             if testcmake==1
-#                 % problem is elsewhere, we come back to original config and
-#                 % issue an error
-#                 setenv('LD_PRELOAD',tmp)
-#                 system([pathtocmake,'cmake']) % to display the error message
-#                 error('There was a problem with the cmake command.')
-#             end
-#         end
-#     end
-#     cmake = [pathtocmake,'cmake'];
-# end
-# 
+#' Return path to cmake executable
+#' @keywords internal
+#' @description
+#' This function returns the path to the cmake executable.
+#' @details
+#' If no path is supplied, the function searches for the 
+#' `cmake` command in the standard PATH (on Unix system).
+#' If a path is supplied (e.g. `/path/to/dir` on Unix 
+#' system), the function search for the `cmake` command 
+#' in this path.
+#' 
+#' **Note**: for the moment, this does not work on 
+#' Windows OS.
+#' 
+#' TODO: use <https://github.com/stnava/cmaker> to 
+#' install cmake if not available on the system .
+#' @param path text string (optional), path where to search 
+#' `cmake` (e.g. `/path/to/dir` on Unix system). Default value 
+#' is NULL and the function searches for `cmake` in the 
+#' standard PATH (on unix system).
+#' @return path to cmake executable.
+#' @author Ghislain Durif
+#' @importFrom stringr str_detect
+#' @export
+get_cmake <- function(path = NULL) {
+    # check
+    if(!is.null(path)) {
+        if(!is.character(path))
+            stop("`path` input parameter should be a text string.")
+        if(!dir.exists(path))
+            stop("`path` input parameter should be a path to an existing directory.")
+    }
+    # OS
+    os_type <- .Platform$OS.type
+    # find executable
+    cmake_executable <- switch(os_type,
+        unix = {
+            if(!is.null(path)) {
+                if(any(str_detect(string = list.files(path), 
+                                  pattern = "cmake")))
+                    file.path(path, "cmake")
+                else
+                    stop(paste0("`cmake` not found in path ", path))
+            } else {
+                system("which cmake", intern = TRUE)
+            }
+        },
+        windows = {
+            stop("Windows not supported at the moment")
+        })
+    # out
+    return(cmake_executable)
+}
+
+#' Check cmake version
+#' @keywords internal
+#' @description
+#' This function calls the cmake executable whose path is
+#' given in input and check if its version is higher than 
+#' 3.10 which is the minimum required for `rkeops` to work.
+#' @details
+#' The function check for the existence of the input file
+#' `cmake_executable`, if it is actually a cmake executable
+#' and its version (`rkeops` requires cmake >= 3.10).
+#' @param cmake_executable text string, path to cmake 
+#' (e.g. `/path/to/cmake` on Unix system).
+#' @return a null value if all checks are ok.
+#' @author Ghislain Durif
+#' @importFrom stringr str_extract
+#' @export
+check_cmake <- function(cmake_executable) {
+    # check if string
+    if(!is.character(cmake_executable))
+        stop("`cmake_executable` input parameter should be a text string.")
+    # check if file exists
+    if(!file.exists(cmake_executable))
+        stop("`cmake_executable` input parameter does not correspond to an existing file.")
+    # get cmake version
+    tmp <- system(paste0(cmake_executable, " --version"), intern = TRUE)
+    tmp <- paste0(tmp, collapse = "\n")
+    # check if it is cmake
+    if(!str_detect(string = tmp, pattern = "cmake"))
+        stop("`cmake_executable` input parameter is not a path to a cmake executable.")
+    # check version number (requirement >= 3.10)
+    current_version <- str_extract(string = tmp, 
+                                   pattern = "([0-9]+.?)+")
+    expected_version <- "3.10"
+    if(compareVersion(current_version, expected_version)<0) {
+        stop("cmake version is too old, version >= 3.10 is required")
+    }
+    return(0)
+}
--- rkeops/tests/testthat/test-common.R
+++ rkeops/tests/testthat/test-common.R
@@ -1,8 +1,31 @@
 context("common functions")
 
+test_that("get_cmake", {
+    out <- get_cmake()
+    expect_match(out, "cmake")
+    expect_error(get_cmake(0), 
+                 "`path` input parameter should be a text string.")
+    expect_error(get_cmake("/nopath/to/test"), 
+                 "`path` input parameter should be a path to an existing directory.")
+    expect_error(get_cmake("/bin"), 
+                 "`cmake` not found in path /bin")
+})
+
+test_that("check_cmake", {
+    out <- get_cmake()
+    expect_equal(check_cmake(out), 0)
+    expect_error(check_cmake(0), 
+                 "`cmake_executable` input parameter should be a text string.")
+    expect_error(check_cmake("/nopath/to/test"), 
+                 "`cmake_executable` input parameter does not correspond to an existing file.")
+    expect_error(check_cmake("/bin/ls"), 
+                 "`cmake_executable` input parameter is not a path to a cmake executable.")
+})
+
 test_that("string2hash", {
     out <- string2hash("test")
     expected_out <- "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
     expect_equal(as.character(out), 
                  expected_out)
 })
+
