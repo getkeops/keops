c0fb5cbc589128c684f7594593606fbeb65f39e1
GD <gd.dev@libertymail.net>
2019-08-07 14:28:31 +0200

    WORK IN PROGRESS: vectorized parser to format formula variables/parameters for compilation

--- rkeops/R/generic-format_var_aliase.R
+++ rkeops/R/generic-format_var_aliase.R
@@ -1,44 +1,75 @@
-# function [var_aliases,indijp] =format_var_aliase(var_options)
-# % format var_aliases to pass option to cmake
-# var_aliases = '';
-# indijp = [-1,-1,-1]; % indijp will be used to calculate nx, ny and np from input variables
-# 
-# for k=1:length(var_options)
-# 
-#     [varname,vartype] = sepeqstr(var_options{k});
-#     if ~isempty(varname)
-#         [vartype, pos] = vartype_cpp(vartype,k);
-#         var_aliases = [var_aliases,'decltype(',vartype,') ',varname,';'];
-#     end
-#     
-#     % analysing vartype : ex 'Vi(4)' means variable indexed by i
-#     %  with dimension 4. The position is given by the rank in
-#     % the function argument. Here we are interested in the type
-#     % and position, so 2nd and the variable 'pos'
-#     type = vartype(2);
-#     if (type=='i' || type=='x') && indijp(1)==-1   % we allow Vx instead of Vi for backward compatibility
-#         indijp(1) = pos;
-#     elseif (type=='j' || type=='y') && indijp(2)==-1   % we allow Vx instead of Vi for backward compatibility
-#         indijp(2) = pos;
-#     elseif type=='m' && indijp(3)==-1
-#         indijp(3) = pos;
-#     end
-# end
-# end
-# 
-# 
-# function [vartype, pos] = vartype_cpp(str,k)
-# % format the var aliases so that it has the
-# % form 'VarType(pos,dim)' where Vartype is 
-# % 'Pm', 'Vi', 'Vj', pos and dim are integer.
-# 
-# comma = find(str==',');
-# if isempty(comma) % assume 1 arg, ie we have Vi(3)
-#     vartype = [str(1:3),num2str(k-1),',',str(4:end)];
-#     pos = k;
-# 
-# else % assume 2 arg, ie we have Vi(0,3)
-#     vartype = str;
-#     pos  = str2num(str(4:(comma-1))) +1; %overwrite the position number
-# end
-# end
+#' Format formula argument list in triplet (type, dimension, position)
+#' @keywords internal
+#' @description
+#' The function `format_var_aliases` formats KeOps formula arguments to be 
+#' understood by the C++ code. 
+#' 
+#' @details
+#' `e^(lambda*||x_i - y_j||^2) * beta_j` where `x_i`, `y_j` 
+#' and `beta_j` are 3d vectors
+#' 
+#' ```
+#' formula = "Sum_Reduction(Exp(lambda * SqNorm2(x-y)) * beta, 0)"
+#' args = c("x=Vi(3)", "y=Vj(3)", 
+#'          "beta=Vj(3)", "lambda=Pm(1)")
+#' ```
+#' 
+#' |---------|-------------------------|-----------|
+#' | keyword | meaning                 | type      |
+#' |---------|-------------------------|-----------|
+#' | `Vi`    | variable indexed by `i` | `0`       |
+#' | `Vj`    | variable indexed by `j` | `1`       |
+#' | `Pm`    | parameter               | `2`       |
+#' |---------|-------------------------|-----------|
+#' 
+#' 
+#' @importFrom stringr str_detect str_count str_extract
+#' @export
+format_var_aliases <- function(args) {
+    
+    possible_var_type <- c("Vi", "Vj", "Pm")
+    possible_args <- paste0("[a-zA-Z0-9_-]+=", 
+                            paste0(possible_var_type, 
+                                   "\\([0-9]+(,[0-9]+)?\\)"))
+    
+    # check correctness of input args
+    args_check <- str_count(string = args, 
+                            pattern = paste0(possible_args,
+                                             collapse = "|")) == 1
+    if(!all(args_check)) {
+        stop(paste0("Issue with input argument '", 
+                    paste0(args[!args_check], collapse = "', '"),
+                    "'"))
+    }
+    
+    # check consistency of input args
+    args_check <- str_detect(string = args, 
+                             pattern = "(?<=[0-9]),(?=[0-9])")
+    
+    # parse
+    split_args <- Reduce("rbind", str_split(string = args, pattern = "="))
+    var_name <- split_args[,1]
+    var_type <- str_extract(string = split_args[,2],
+                            pattern = paste0(possible_var_type,
+                            collapse = "|"))
+    if(all(arg_check)) {
+        var_pos <- as.numeric(str_extract(string = split_args[,2], 
+                                          pattern = "[0-9]+(?=,)"))
+        var_dim <- as.numeric(str_extract(string = split_args[,2], 
+                                          pattern = "(?<=,)[0-9]+"))
+    } else if(all(1-args_check)) {
+        var_pos <- seq(0, length(args)-1, by=1)
+        var_dim <- as.numeric(str_extract(string = split_args[,2], 
+                                          pattern = "[0-9]+"))
+    } else {
+        stop(paste0("Issue with input argument consistency, use either ", 
+                    "'(dim)' or '(pos, dim)´ for 'Vi' and 'Vj' ",
+                    "(see help page or vignette for more details)."))
+        # FIXME
+    }
+    
+    ## format 
+    # FIXME
+    
+}
+
