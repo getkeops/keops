5fb78223591abbeab599b248fc3268b7867a814f
bcharlier <benjamin.charlier@umontpellier.fr>
2019-08-14 12:30:10 +0200

    Update test_gpytorch.cu

--- keops/examples/test_gpytorch.cu
+++ keops/examples/test_gpytorch.cu
@@ -1,13 +1,5 @@
-// test convolution 
-// compile with
-//		nvcc -I.. -Wno-deprecated-gpu-targets -std=c++11 -O2 -o build/test_simple test_simple.cu
-
-// we define an arbitrary function using available blocks,
-// then test its convolution on the GPU
-
-// Here we build the function f(x,y,u,v,beta) = <u,v>^2 * exp(-p*|x-y|^2) * beta
-// where p is a scalar parameter, x, y, beta are 3D vectors, and u, v are 4D vectors
-// and the convolution is res_i = sum_j f(x_i,y_j,u_i,v_j,beta_j)
+// test gpytorch compile with
+//		nvcc -I.. -std=c++14 -O2 -o build/test_gpytorch ./test_gpytorch.cu
 
 #include <algorithm>
 #include <keops_includes.h>
