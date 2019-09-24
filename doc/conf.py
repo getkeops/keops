fdcd2a13e7a5ae4caf7fc6cf57c7ffc02ed6124b
Benjamin Charlier <benjamin.charlier@umontpellier.fr>
2019-09-24 14:22:34 +0200

    Doc build use pykeops package if installed

--- doc/conf.py
+++ doc/conf.py
@@ -21,10 +21,14 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('sphinxext'))
 
-import pykeops
+try:
+  import pykeops
+except:
+  sys.path.insert(0, os.path.abspath('..'))
+  import pykeops
+
 from pykeops import __version__
 
 # -- General configuration ------------------------------------------------
