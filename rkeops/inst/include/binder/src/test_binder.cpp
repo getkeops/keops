226ef5a87ee43246cea4eba64e76128cfb132893
GD <gd.dev@libertymail.net>
2019-10-04 15:07:27 +0200

    test on type and list

--- rkeops/inst/include/binder/src/test_binder.cpp
+++ rkeops/inst/include/binder/src/test_binder.cpp
@@ -1,6 +1,19 @@
 #include <Rcpp.h>
+#include <RcppEigen.h>
 
 // [[Rcpp::export]]
 int test_binder() {
     return(1);
 }
+
+// // [[Rcpp::depends(RcppEigen)]]
+// // [[Rcpp::export]]
+// float test_double(Eigen::MatrixXf test) {
+//     return(test.sum());
+// }
+// 
+// // [[Rcpp::depends(RcppEigen)]]
+// // [[Rcpp::export]]
+// Eigen::MatrixXf test_list(Rcpp::List input) {
+//     return(Rcpp::as<Eigen::MatrixXf>(input[0]));
+// }
