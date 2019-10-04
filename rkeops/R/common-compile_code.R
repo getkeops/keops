cd15cf2c31768b006fb0531d8be3aeed32c323bd
GD <gd.dev@libertymail.net>
2019-10-04 18:54:24 +0200

    remove append of the cmake command to cmake

--- rkeops/R/common-compile_code.R
+++ rkeops/R/common-compile_code.R
@@ -6,7 +6,6 @@
 #' @author Ghislain Durif
 #' @export
 compile_code <- function(formula, var_aliases, dllname, cmake_dir) {
-    # FIXME
     
     ## cmake
     cmake_cmd <- paste0(shQuote(get_cmake()), " ", shQuote(cmake_dir), 
@@ -14,6 +13,7 @@ compile_code <- function(formula, var_aliases, dllname, cmake_dir) {
                         get_rkeops_option("use_cuda_if_possible"),
                         " -DCMAKE_BUILD_TYPE=Release",
                         " -D__TYPE__=", get_rkeops_option("precision"), 
+                        " -DC_CONTIGUOUS=0", 
                         " -DFORMULA_OBJ=", shQuote(formula),
                         " -DVAR_ALIASES=", shQuote(var_aliases),
                         " -Dshared_obj_name=", shQuote(dllname),
@@ -23,8 +23,9 @@ compile_code <- function(formula, var_aliases, dllname, cmake_dir) {
                         " -DRCPPEIGEN_INCLUDE=", 
                         shQuote(system.file("include", package = "RcppEigen")),
                         " -DR_LIB=", shQuote(R.home("lib")))
-    cmake_cmd <- paste0(cmake_cmd, 
-                        " -DcommandLine='", cmake_cmd, "'")
+    # FIXME
+    # cmake_cmd <- paste0(cmake_cmd,
+    #                     " -DcommandLine='", shQuote(cmake_cmd), "'")
     tmp <- system(cmake_cmd)
     
     ## make
