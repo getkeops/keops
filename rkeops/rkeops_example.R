494171cf7fd1f126131069d9ac9acb8c5d340783
Unknown <joan.glaunes@gmail.com>
2019-10-07 19:01:27 +0200

    found a bug, see example 2 in rkeops_example.R

--- rkeops/rkeops_example.R
+++ rkeops/rkeops_example.R
@@ -32,17 +32,17 @@ res <- op(input, nx=ncol(x), ny=ncol(y))
 expected_res <- apply(t(x) %*% y, 2, sum)
 sum(abs(res - expected_res))
 
-## exemple 2
-formula = "Sum_Reduction(Exp(lambda*SqNorm2(x-y))*beta, 0)"
+## exemple 2 : fails with malloc(): memory corruption.. Abandon (core dumped) !!!!!
+formula = "Sum_Reduction(Exp(lambda*SqNorm2(x-y))*beta, 1)"
 args = c("x=Vi(3)", "y=Vj(3)", "beta=Vj(3)", "lambda=Pm(1)")
 
 op <- keops_kernel(formula, args)
 
-n = 10
-x <- matrix(runif(n*3), ncol=n)
-y <- matrix(runif(n*3), ncol=n)
-beta <- matrix(runif(n*3), ncol=n)
-lambda <- 5e-3
+nx = 1000
+ny = 1500
+x <- matrix(runif(nx*3), ncol=nx)
+y <- matrix(runif(ny*3), ncol=ny)
+beta <- matrix(runif(ny*3), ncol=ny)
 
 lambda <- as.matrix(5)
 
