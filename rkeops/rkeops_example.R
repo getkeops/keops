24f708f71c187ca0b3a400b0fe5c91f48c339098
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-10-08 18:32:05 +0200

    rkeops_example.R

--- rkeops/rkeops_example.R
+++ rkeops/rkeops_example.R
@@ -1,8 +1,8 @@
 # install.packages("devtools") if necessary
-# devtools::install("rkeops") # or edit path to rkeops sub-directory
+devtools::install("../rkeops") # or edit path to rkeops sub-directory
 
 library(rkeops)
-# clean_rkeops()
+clean_rkeops()
 
 # rkeops options (compile and runtime)
 get_rkeops_options()
@@ -23,13 +23,14 @@ op <- keops_kernel(formula, args)
 
 n <- 10
 p <- 15
-x <- matrix(runif(n*3),3,n)
-y <- matrix(runif(p*3),3,p)
+x <- matrix(runif(n*3), nrow=3, ncol=n)
+y <- matrix(runif(p*3), nrow=3, ncol=p)
 
 input <- list(x, y)
 res <- op(input, nx=ncol(x), ny=ncol(y))
 str(res)
 expected_res <- apply(t(x) %*% y, 2, sum)
+expected_res
 sum(abs(res - expected_res))
 
 ## exemple 2
@@ -40,8 +41,8 @@ op <- keops_kernel(formula, args)
 
 n <- 10
 p <- 15
-x <- matrix(runif(n*3),3,n)
-y <- matrix(runif(p*3),3,p)
+x <- matrix(runif(n*3), nrow= 3, ncol=n)
+y <- matrix(runif(p*3), nrow=3, ncol=p)
 
 input <- list(x, y)
 res <- op(input, nx=ncol(x), ny=ncol(y))
@@ -50,20 +51,31 @@ expected_res <- apply(t(x) %*% y, 1, sum)
 sum(abs(res - expected_res))
 
 ## exemple 3
+
 formula = "Sum_Reduction(Exp(lambda*SqNorm2(x-y))*beta, 1)"
 args = c("x=Vi(3)", "y=Vj(3)", "beta=Vj(3)", "lambda=Pm(1)")
 
 op <- keops_kernel(formula, args)
 
-nx = 1000
-ny = 1500
-x <- matrix(runif(nx*3),3,nx)
-y <- matrix(runif(ny*3),3,ny)
-beta <- matrix(runif(ny*3),3,ny)
+nx = 10
+ny = 15
+x <- matrix(runif(nx*3), nrow=3, ncol=nx)
+y <- matrix(runif(ny*3),nrow=3,ncol=ny)
+beta <- matrix(runif(ny*3), nrow=3, ncol=ny)
 
 lambda <- as.matrix(5)
 
 input <- list(x, y, beta, lambda)
 
 res <- op(input, nx=ncol(x), ny=ncol(y))
-str(res)
+res
+
+
+expected_res <- matrix(0, 3, nx)
+for(i in 1:nx) {
+    for(j in 1:ny) {
+        expected_res[i,] <- expected_res[i,] + exp(lambda * sum((x[,i]-y[,j])^2) * beta[,j])
+    }
+    }
+res
+expected_res
