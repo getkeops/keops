b0dcd6cf6bccb4971f58aba273d09314d39c0575
GD <gd.dev@libertymail.net>
2019-10-29 18:20:45 +0100

    doc (work in proress)

--- rkeops/R/generic-keops_kernel.R
+++ rkeops/R/generic-keops_kernel.R
@@ -8,43 +8,67 @@
 #' 
 #' FIXME
 #' See <https://www.kernel-operations.io/keops/api/math-operations.html>
+#' 
+#' Important:
 #' @author Ghislain Durif
-#' @param formula text string, an operator formula.
-#' @param args vector of text string, operator parameters.
-#' @return
-#' FIXME
+#' @param formula text string, an operator formula (see Details).
+#' @param args vector of text string, formula arguments (see Details).
+#' @return a function that can be used to compute the value of the formula 
+#' on actual data. This function takes at input a list of data corresponding 
+#' to the formula arguments and return the computed values (generally a 
+#' vector or a matrix depending on the reduction).
 #' @importFrom stringr str_length
 #' @examples
 #' \dontrun{
-#' # Define and test a function that computes for each i the sum over j
-#' # of the square of the scalar products of `x_i` and `y_j` (both 3d vectors)
+#' ## Example 1
+#' # Define a function that computes for each i the sum over j
+#' # of the scalar products of `x_i` and `y_j` (both 3d vectors)
 #' F <- keops_kernel(formula = "Sum_Reduction((x|y), 1)",
 #'                   args = c("x=Vi(3)", "y=Vj(3)"))
+#' ## data
 #' nx <- 10
 #' ny <- 15
-#' # x_i = rows of the matrix x
+#' 
+#' # case 1
+#' # x_i = rows of the matrix x (not contiguous in memory)
 #' x <- matrix(runif(nx*3), nrow=nx, ncol=3)
-#' # y _j = rows of the matrix y
+#' # y _j = rows of the matrix y (not contiguous in memory)
 #' y <- matrix(runif(ny*3), nrow=ny, ncol=3)
+#' # compute the result
 #' res <- F(list(x,y))
 #' 
-#' # x_i = columns of the matrix x
+#' # case 2
+#' # x_i = columns of the matrix x (contiguous in memory)
 #' x <- matrix(runif(nx*3), nrow=3, ncol=nx)
-#' # y _j = rows of the matrix y
+#' # y _j = rows of the matrix y (contiguous in memory)
 #' y <- matrix(runif(ny*3), nrow=3, ncol=ny)
-#' res <- F(x,y)
+#' # compute the result
+#' res <- F(list(x,y))
 #' 
-#' # Define and test the convolution with a Gauss kernel i.e. the sum 
-#' # over j of `e^(lambda*||x_i - y_j||^2) * beta_j` where `x_i`, `y_j` 
-#' # and `beta_j` are 3d vectors
+#' ## Example 2
+#' # Define a function that computes the the convolution with a Gauss kernel 
+#' # i.e. the sum over j of `e^(lambda*||x_i - y_j||^2) * beta_j` where `x_i`, 
+#' # `y_j` and `beta_j` are 3d vectors, and `lambda` is a scalar
 #' F = keops_kernel(formula = "Sum_Reduction(Exp(lambda*SqNorm2(x-y))*beta,0)",
 #'                  args = c("x=Vi(3)", "y=Vj(3)", 
 #'                           "beta=Vj(3)", "lambda=Pm(1)"))
-#' x <- rnorm(3,2000)
-#' y <- rnorm(3,5000)
-#' beta <- rnorm(3,5000)
+#' 
+#' # data
+#' nx <- 10
+#' ny <- 15
+#' # x_i = rows of the matrix x (not contiguous in memory)
+#' x <- matrix(runif(nx*3), nrow=nx, ncol=3)
+#' # y _j = rows of the matrix y (not contiguous in memory)
+#' y <- matrix(runif(ny*3), nrow=ny, ncol=3)
+#' # beta_j = rows of the matrix y (not contiguous in memory)
+#' beta <- matrix(runif(ny*3), nrow=ny, ncol=3)
+#' # !! important !! y and beta should have the same dimension
+#' 
+#' # parameter
 #' lambda <- 0.25
-#' res <- F(x, y, beta, lambda)
+#' 
+#' # compute the result
+#' res <- F(list(x, y, beta, lambda))
 #' }
 #' @export
 keops_kernel <- function(formula, args) {
