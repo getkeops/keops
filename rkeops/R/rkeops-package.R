6daeecad1b2b717709c260ef263b1510a19349e5
GD <gd.dev@libertymail.net>
2019-10-31 11:47:25 +0100

    refactor general man page

--- rkeops/R/rkeops-package.R
+++ rkeops/R/rkeops-package.R
@@ -3,34 +3,33 @@
 #' @docType package
 #' @title rkeops
 #' 
-#' RKeOps: Kernel Operations on the GPU, with autodiff, without memory overflows
-#' 
-#' RKeOps contains the R bindings for the cpp/cuda library 
+#' RKeOps: kernel operations on GPU, with autodiff, without memory overflows in 
+#' R
+#'  
+#' @description
+#' RKeOps is the R package interfacing the cpp/cuda library 
 #' [KeOps](https://www.kernel-operations.io/). It provides 
 #' standard R functions that can be used in any R (>=3) codes.
 #' 
-#' For a full documentation you may read:
-#' * [Installation](https://www.kernel-operations.io/keops/introduction/installation.html)
-#' * [Documentation](https://www.kernel-operations.io/)
-#' * [Learning KeOps syntax with examples](https://www.kernel-operations.io/keops/_auto_examples/index.html)
-#' * [Tutorials gallery](https://www.kernel-operations.io/keops/_auto_tutorials/index.html)
-#' 
-#' @description
-#' The KeOps library lets you compute generic reductions of very large arrays whose 
-#' entries are given by a mathematical formula. It combines a tiled reduction scheme 
-#' with an automatic differentiation engine, and can be used through Matlab, NumPy or 
-#' PyTorch backends. It is perfectly suited to the computation of Kernel dot products 
-#' and the associated gradients, even when the full kernel matrix does not fit into 
-#' the GPU memory.
-#' 
 #' @author 
 #' - [Benjamin Charlier](http://imag.umontpellier.fr/~charlier/)
+#' - [Ghislain Durif](https://gdurif.perso.math.cnrs.fr/)
 #' - [Jean Feydy](https://www.math.ens.fr/~feydy/)
 #' - [Joan Alexis Glaunès](https://www.mi.parisdescartes.fr/~glaunes/)
-#' - [Ghislain Durif](https://gdurif.perso.math.cnrs.fr/)
 #' 
 #' @details
-#' See <https://www.kernel-operations.io/>
+#' The KeOps library provides seamless kernel operations on GPU, with 
+#' auto-differentiation and without memory overflows.
+#' 
+#' With RKeOps, you can compute generic reductions of very large arrays whose 
+#' entries are given by a mathematical formula. It combines a tiled reduction 
+#' scheme with an automatic differentiation engine. It is perfectly suited to 
+#' the computation of Kernel dot products and the associated gradients, even 
+#' when the full kernel matrix does not fit into the GPU memory.
+#' 
+#' For more information, please read the vignettes 
+#' (`browseVignettes("rkeops")`) and visit 
+#' <https://www.kernel-operations.io/>.
 #' 
 #' @import Rcpp
 #' @importFrom Rcpp sourceCpp
