b3fcebaff984ad415b08b9fc558da52b703b056c
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-11-05 10:10:41 +0100

    cosmit

--- keops/headers.cmake
+++ keops/headers.cmake
@@ -4,7 +4,7 @@
 
 set(CMAKE_CXX_STANDARD 14)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -ferror-limit=2")
 else()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unknown-pragmas -fmax-errors=2")
@@ -52,7 +52,6 @@ endif()
 if(USE_KAHAN)
   add_definitions(-DUSE_KAHAN=${USE_KAHAN})
 endif()
-
 
 
 # - Declare the templates formula if not provided by the user
