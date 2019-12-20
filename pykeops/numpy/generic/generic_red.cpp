197319bd1484fcfe61a3c08b7ac4334f8b59b97c
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-12-20 16:28:14 +0100

    fix template call

--- pykeops/numpy/generic/generic_red.cpp
+++ pykeops/numpy/generic/generic_red.cpp
@@ -67,7 +67,7 @@ int get_size(__RANGEARRAY__ obj_ptri, int l) {
 
 template<>
 __INDEX__ *get_rangedata(__RANGEARRAY__ obj_ptri) {
-  return (__INDEX__ *) obj_ptri.data();
+  return static_cast< __INDEX__ * >(obj_ptri.data());
 }
 
 void keops_error(std::basic_string< char > msg) {
