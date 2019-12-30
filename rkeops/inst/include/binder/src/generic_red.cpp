e69321027bb1907e72592c5087cfb6f03aaa4fa7
GD <gd.dev@libertymail.net>
2019-12-30 16:25:19 +0100

    remove useless dimension

--- rkeops/inst/include/binder/src/generic_red.cpp
+++ rkeops/inst/include/binder/src/generic_red.cpp
@@ -5,20 +5,18 @@
 #include "rkeops_matrix.h"
 
 // rkeops matrix type
-using rkeops_base_matrix_t = rkeops::base_matrix<rkeops::type_t>;
-using rkeops_matrix_t = rkeops::matrix<rkeops::type_t>;
+using rkeops_base_matrix_t = rkeops::base_matrix< rkeops::type_t >;
+using rkeops_matrix_t = rkeops::matrix< rkeops::type_t >;
 // list of raw input data
-using rkeops_list_t = std::vector<rkeops_base_matrix_t>;
+using rkeops_list_t = std::vector< rkeops_base_matrix_t >;
 
 // generic reduction
 rkeops_matrix_t genred(
         int tagCpuGpu, int tag1D2D, int tagHostDevice, 
-        int Device_Id, int nx, int ny,
+        int Device_Id,
         rkeops_list_t & input) {
     
-    rkeops_matrix_t output = keops_binders::generic_red<rkeops_matrix_t> (
-        nx,
-        ny,
+    rkeops_matrix_t output = keops_binders::generic_red< rkeops_base_matrix_t, rkeops_matrix_t, rkeops_matrix_t > (
         tagCpuGpu,
         tag1D2D,
         tagHostDevice,
