b92ef0dc8afeba356ec995c62a65e6c3c5a3173b
GD <gd.dev@libertymail.net>
2020-01-02 11:19:48 +0100

    test scripts

---
+++ rkeops/dev/test_keops.R
@@ -0,0 +1,83 @@
+# install.packages("devtools") if necessary
+#devtools::install("../../rkeops") # or edit path to rkeops sub-directory
+
+devtools::load_all("..")
+set_rkeops_options()
+clean_rkeops()
+
+# rkeops options (compile and runtime)
+get_rkeops_options()
+
+# option names
+rkeops_option_names()
+
+# to get a specific option: get_rkeops_option(<name>)
+# to set a specific option: set_rkeops_option(<name>, <value>)
+# in particular, to compute on GPU: 
+# set_rkeops_option("tagCpuGpu", 1)
+set_rkeops_option("precision", "double")
+
+
+## exemple 1
+formula = "Sum_Reduction((x|y), 1)"
+args = c("x=Vi(3)", "y=Vj(3)")
+
+op <- keops_kernel(formula, args)
+
+nx <- 10
+ny <- 15
+x <- matrix(runif(nx*3), nrow=3, ncol=nx)
+y <- matrix(runif(ny*3), nrow=3, ncol=ny)
+
+input <- list(x, y)
+res <- op(input)
+expected_res <- colSums(t(x) %*% y)
+sum(abs(res - expected_res))
+
+## exemple 2
+formula = "Sum_Reduction((x|y), 0)"
+args = c("x=Vi(3)", "y=Vj(3)")
+
+op <- keops_kernel(formula, args)
+
+nx <- 10
+ny <- 15
+x <- matrix(runif(nx*3), nrow= 3, ncol=nx)
+y <- matrix(runif(ny*3), nrow=3, ncol=ny)
+
+input <- list(x, y)
+tracemem(input)
+res <- op(input, nx=ncol(x), ny=ncol(y))
+str(res)
+expected_res <- apply(t(x) %*% y, 2, sum)
+sum(abs(res - expected_res))
+
+# ## exemple 3
+# 
+# formula = "Sum_Reduction(Exp(-lambda*SqNorm2(x-y))*beta, 1)"
+# args = c("x=Vi(3)", "y=Vj(3)", "beta=Vj(3)", "lambda=Pm(1)")
+# 
+# op <- keops_kernel(formula, args)
+# 
+# nx = 10
+# ny = 15
+# x <- matrix(runif(nx*3), nrow=3, ncol=nx)
+# y <- matrix(runif(ny*3), nrow=3, ncol=ny)
+# beta <- matrix(runif(ny*3), nrow=3, ncol=ny)
+# 
+# lambda <- as.matrix(.5)
+# 
+# input <- list(x, y, beta, lambda)
+# 
+# res <- op(input)
+# summary(t(res))
+# 
+# expected_res <- matrix(0, 3, nx)
+# for(i in 1:nx) {
+#     for(j in 1:ny) {
+#         expected_res[,i] <- expected_res[,i] + exp(- lambda[1] * sum((x[,i]-y[,j])^2)) * beta[,j]
+#     }
+# }
+# 
+# sum(abs(res - expected_res))
+# 
