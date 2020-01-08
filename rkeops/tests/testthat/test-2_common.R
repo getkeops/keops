cc76fb396d9d4e43a65d6df6b67101115422e8b9
GD <gd.dev@libertymail.net>
2020-01-08 16:56:57 +0100

    fix issue with test on get_cmake

--- rkeops/tests/testthat/test-2_common.R
+++ rkeops/tests/testthat/test-2_common.R
@@ -7,8 +7,8 @@ test_that("get_cmake", {
                  "`path` input parameter should be a text string.")
     expect_error(get_cmake("/nopath/to/test"), 
                  "`path` input parameter should be a path to an existing directory.")
-    expect_error(get_cmake("/bin"), 
-                 "`cmake` not found in path /bin")
+    expect_warning(get_cmake("/bin"), 
+                   "`cmake` not found in path /bin")
 })
 
 test_that("check_cmake", {
