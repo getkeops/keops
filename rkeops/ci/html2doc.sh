ad629694b2b1c20ddcc44ff20da3abb47d3f54e9
GD <gd.dev@libertymail.net>
2020-01-09 10:45:50 +0100

    undo modification of .gitignore

--- rkeops/ci/html2doc.sh
+++ rkeops/ci/html2doc.sh
@@ -5,6 +5,8 @@ WDIR=${RDIR}/rkeops/doc
 
 R -q -e "devtools::build_vignettes('"${RDIR}/rkeops"', install=FALSE)"
 
+git checkout HEAD -- ${RDIR}/rkeops/.gitignore
+
 for FILE in $(find $WDIR -name "*.html"); do
     echo $FILE
     R -q -e "knitr::pandoc('"${FILE}"', format = 'rst')"
