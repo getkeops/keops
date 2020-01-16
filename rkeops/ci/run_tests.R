e7f845c94b469578ba5ba8983fd6bd490d4195e8
GD <gd.dev@libertymail.net>
2020-01-16 10:22:55 +0100

    fix issue with testing

--- rkeops/ci/run_tests.R
+++ rkeops/ci/run_tests.R
@@ -1,3 +1,3 @@
 project_root_dir <- system("git rev-parse --show-toplevel", intern=TRUE)
 devtools::test(file.path(project_root_dir, "rkeops"), 
-               reporter = 'fail') #, stop_on_failure=TRUE)
+               reporter = c('Progress', 'fail'))
