f34b2fe49f58f34b31ab8d276d344d6dc393315b
MrHuff <roberthu666@gmail.com>
2020-01-29 19:03:53 +0100

    added some additional memory profiling features2

--- pykeops/examples/pytorch/test_float16.py
+++ pykeops/examples/pytorch/test_float16.py
@@ -1,10 +1,29 @@
 # Test for half precision support in KeOps
 # We perform a gaussian convolution with half, single and double precision
 # and compare timings and accuracy
+import GPUtil
+from threading import Thread
+import time
+
+
+class Monitor(Thread):
+    def __init__(self, delay):
+        super(Monitor, self).__init__()
+        self.stopped = False
+        self.delay = delay  # Time between calls to GPUtil
+        self.start()
+
+    def run(self):
+        while not self.stopped:
+            GPUtil.showUtilization()
+            time.sleep(self.delay)
+
+    def stop(self):
+        self.stopped = True
+
 
 backend = "torch"  # "torch" or "numpy", but only "torch" works for now
-device_id = 1
-
+device_id = 0
 if backend == "torch":
     import torch
     from pykeops.torch import LazyTensor
@@ -48,17 +67,20 @@ else:
     bh = b.astype(np.float16)
 
 Ntest_half, Ntest_float, Ntest_double = 10, 10, 10
-
+monitor = Monitor(1e-6)
 # computation using float32
 K_keops32 = K(xf,yf,bf)
 res_float = K_keops32()
 print("comp float, time : ",timeit.timeit("K_keops32()",number=Ntest_float,setup="from __main__ import K_keops32")/Ntest_float)
+monitor.stop()
 
+print('----')
 # computation using float64
+monitor = Monitor(1e-6)
 K_keops64 = K(x,y,b)
 res_double = K_keops64()
 print("comp double, time : ",timeit.timeit("K_keops64()",number=Ntest_double,setup="from __main__ import K_keops64")/Ntest_double)
-
+monitor.stop()
 if backend == "torch":
     print("relative mean error float / double : ",(res_float.double()-res_double).abs().mean()/res_double.abs().mean())
     print("relative max error float / double : ",(res_float.double()-res_double).abs().max()/res_double.abs().mean())
@@ -67,9 +89,11 @@ else:
     print("relative max error float / double : ",np.max(np.abs(res_float.astype(np.float64)-res_double))/np.mean(np.abs(res_double)))
 
 # computation using float16
+monitor = Monitor(1e-6)
 K_keops16 = K(xh,yh,bh)
 res_half = K_keops16()
 print("comp half, time : ",timeit.timeit("K_keops16()",number=Ntest_half,setup="from __main__ import K_keops16")/Ntest_half)
+monitor.stop()
 
 if backend == "torch":
     print("relative mean error half / double : ",(res_half.double()-res_double).abs().mean()/res_double.abs().mean())
@@ -77,4 +101,5 @@ if backend == "torch":
 else:
     print("relative mean error half / double : ",np.mean(np.abs(res_half.astype(np.float64)-res_double))/np.mean(np.abs(res_double)))
     print("relative max error half / double : ",np.max(np.abs(res_half.astype(np.float64)-res_double))/np.mean(np.abs(res_double)))
+
 
