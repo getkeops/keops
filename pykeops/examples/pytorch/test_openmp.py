e752433f4e517cd3daf90864cd2bf1075f7dbd7f
Unknown <joan.glaunes@gmail.com>
2020-02-13 15:50:09 +0100

    [ci skip] updated test script

--- pykeops/examples/pytorch/test_openmp.py
+++ pykeops/examples/pytorch/test_openmp.py
@@ -9,17 +9,19 @@ from pykeops.torch import LazyTensor
 print("test sans batchdim")
 
 # data
-N = 10000
+N = 100000
 x = torch.randn(N, 3)
 y = torch.randn(N, 3)
+b = torch.randn(N, 1)
 
 # test with keops
 start = time.time()
 x_i = LazyTensor( x[:,None,:] )
 y_j = LazyTensor( y[None,:,:] )
+b_j = LazyTensor( b[None,:,:] )
 D_ij = ((x_i - y_j)**2).sum(dim=2)
 K_ij = (- D_ij).exp()
-res_keops = K_ij.sum(dim=1, backend='CPU').flatten()
+res_keops = (K_ij*b_j).sum(dim=1, backend='CPU').flatten()
 print("time with KeOps : ", time.time()-start)
 
 if N < 15000:
@@ -27,9 +29,10 @@ if N < 15000:
     start = time.time()
     x_i = x[:,None,:]
     y_j = y[None,:,:]
-    D_ij = ((x_i - y_j)**2).sum(dim=2)
+    b_j = b[None,:,:]
+    D_ij = ((x_i - y_j)**2).sum(dim=2,keepdims=True)
     K_ij = (- D_ij).exp()
-    res_torch = K_ij.sum(dim=1)
+    res_torch = (K_ij*b_j).sum(dim=1).flatten()
     print("time with PyTorch : ", time.time()-start)
     print("error : ",torch.norm(res_keops-res_torch)/torch.norm(res_torch))
 
@@ -39,28 +42,31 @@ if N < 15000:
     start = time.time()
     x_i = x.numpy()[:,None,:]
     y_j = y.numpy()[None,:,:]
-    D_ij = ((x_i - y_j)**2).sum(axis=2)
+    b_j = b.numpy()[None,:,:]
+    D_ij = ((x_i - y_j)**2).sum(axis=2,keepdims=True)
     K_ij = np.exp(- D_ij)
-    res_numpy = K_ij.sum(axis=1)
+    res_numpy = (K_ij*b_j).sum(axis=1).flatten()
     print("time with NumPy : ", time.time()-start)
     print("error : ",np.linalg.norm(res_keops.numpy()-res_numpy)/np.linalg.norm(res_numpy))
 
 
-
+'''
 print("test avec batchdim")
 
 # data
 B, N = 10, 4000
 x = torch.randn(B,N, 3)
 y = torch.randn(B,N, 3)
+b = torch.randn(B,N, 1)
 
 # test with keops
 start = time.time()
 x_i = LazyTensor( x[:,:,None,:] )
 y_j = LazyTensor( y[:,None,:,:] )
+b_j = LazyTensor( b[:,None,:,:] )
 D_ij = ((x_i - y_j)**2).sum(dim=3)
 K_ij = (- D_ij).exp()
-res_keops = K_ij.sum(dim=2, backend='CPU').flatten()
+res_keops = (K_ij*b_j).sum(dim=2, backend='CPU').flatten()
 print("time with KeOps : ", time.time()-start)
 
 if N < 5000:
@@ -68,10 +74,11 @@ if N < 5000:
     start = time.time()
     x_i = x[:,:,None,:]
     y_j = y[:,None,:,:]
-    D_ij = ((x_i - y_j)**2).sum(dim=3)
+    b_j = b[:,None,:,:]
+    D_ij = ((x_i - y_j)**2).sum(dim=3,keepdim=True)
     K_ij = (- D_ij).exp()
-    res_torch = K_ij.sum(dim=2).flatten()
+    res_torch = (K_ij*b_j).sum(dim=2).flatten()
     print("time with PyTorch : ", time.time()-start)
     print("error : ",torch.norm(res_keops-res_torch)/torch.norm(res_torch))
-
+'''
 
