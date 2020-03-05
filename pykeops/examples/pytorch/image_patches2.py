edce533592dd24859a52fb00e3f946be9ecede2c
Unknown <joan.glaunes@gmail.com>
2020-03-05 15:47:43 +0100

    [ci skip] WIP

--- pykeops/examples/pytorch/image_patches2.py
+++ pykeops/examples/pytorch/image_patches2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
 # In[58]:
 
 
@@ -20,13 +26,13 @@ def torch_patches(I,s):
             k += 1
     return P.view(-1,d*s**2)
 
-def LazyTensor_patches(I,s,axis):
+def LazyTensor_patches(I,s,axis,use_ranges=True):
     # input : image I, torch tensor of shape (m,n,d)
     #         size of patches s, integer
     # output : LazyTensor representing all s-by-s patches of I, shape ((m-s+1)*n-s+1,d*s**2)
-    # N.B. there are (m-s)*(s-1) garbage patches, corresponding to indices (i,m-s+1),...(i,m-1) 
+    # N.B. there are (m-s)*(s-1) "garbage" patches, corresponding to indices (i,m-s+1),...(i,m-1) 
     # for each row i except the last one. These garbage patches will not be taken into account in computations
-    # but will remain in the output as zero valued rows.
+    # if use_ranges=True, but will remain in any case in the output as zero valued rows.
     m, n, d = I.shape
     I = I.view(m*n,d)
     P = ()
@@ -36,23 +42,28 @@ def LazyTensor_patches(I,s,axis):
             ind_shift = i*n + j
             P += (LazyTensor(torch.narrow(I,0,ind_shift,ind_last),axis=axis),)
     P = LazyTensor.cat(P,dim=-1)
-    rgm = torch.arange(m-s+1)[:,None].int()
-    ranges = torch.cat((rgm*n,rgm*n+n-s+1),dim=1).cuda()
-    slices = ((torch.arange(m-s+1).int()+1)*(m-s+1)).cuda()
-    red_ranges = (torch.cat((ranges,)*(m-s+1),dim=0)).cuda()
-    P.ranges = (ranges,slices,red_ranges,ranges,slices,red_ranges)
+    if use_ranges:
+        rgm = torch.arange(m-s+1)[:,None].int()
+        ranges = torch.cat((rgm*n,rgm*n+n-s+1),dim=1).cuda()
+        slices = ((torch.arange(m-s+1).int()+1)*(m-s+1)).cuda()
+        red_ranges = (torch.cat((ranges,)*(m-s+1),dim=0)).cuda()
+        P.ranges = (ranges,slices,red_ranges,ranges,slices,red_ranges)
     return P
 
 
-# In[60]:
+# In[2]:
 
 
-m, n, d, s, K = 1024, 1024, 3, 2, 4
+m, n, d, s, K = 1024, 1024, 3, 5, 4
 I = torch.rand(m,n,d).cuda()
+torch.cuda.memory_allocated(0)
 
+
+# In[3]:
+
+
+# testing with PyTorch
 import time
-
-# PyTorch
 if m*n<10000:
     start = time.time()
     P = torch_patches(I,s)
@@ -60,10 +71,21 @@ if m*n<10000:
     out_torch = D2.argsort(dim=1)[:,:4]
     print("elapsed torch : ",time.time()-start)
 
-# KeOps
+
+# In[4]:
+
+
+# testing with KeOps
+P_i = LazyTensor_patches(I,s,axis=0,use_ranges=False)
+torch.cuda.memory_allocated(0)
+
+
+# In[5]:
+
+
 start = time.time()
-P_i = LazyTensor_patches(I,s,axis=0)
-P_j = LazyTensor_patches(I,s,axis=1)
+P_i = LazyTensor_patches(I,s,axis=0,use_ranges=False)
+P_j = LazyTensor_patches(I,s,axis=1,use_ranges=False)
 D2 = P_i.sqdist(P_j)
 out_keops = D2.argKmin(dim=1,K=4)
 # now post-processing to get rid of the garbage rows of the output
@@ -78,6 +100,13 @@ print("elapsed keops : ",time.time()-start)
 
 if m*n<10000:
     print("erreur : ",(out_keops-out_torch).abs().sum().item())
+
+
+
+# In[6]:
+
+
+I.shape
 
 
 # In[ ]:
