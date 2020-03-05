5edec4244bf3bf9bfc14839d94ee4d65e8b5d743
Joan Glaunès <joan.glaunes@gmail.com>
2020-03-05 22:36:30 +0100

    [ci skip] WIP

--- pykeops/examples/pytorch/image_patches2.py
+++ pykeops/examples/pytorch/image_patches2.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[58]:
-
 
 import torch
 from pykeops.torch import LazyTensor
@@ -44,23 +33,24 @@ def LazyTensor_patches(I,s,axis,use_ranges=True):
     P = LazyTensor.cat(P,dim=-1)
     if use_ranges:
         rgm = torch.arange(m-s+1)[:,None].int()
-        ranges = torch.cat((rgm*n,rgm*n+n-s+1),dim=1).cuda()
-        slices = ((torch.arange(m-s+1).int()+1)*(m-s+1)).cuda()
-        red_ranges = (torch.cat((ranges,)*(m-s+1),dim=0)).cuda()
+        ranges = torch.cat((rgm*n,rgm*n+n-s+1),dim=1).to(I.device)
+        slices = ((torch.arange(m-s+1).int()+1)*(m-s+1)).to(I.device)
+        red_ranges = (torch.cat((ranges,)*(m-s+1),dim=0)).to(I.device)
         P.ranges = (ranges,slices,red_ranges,ranges,slices,red_ranges)
     return P
 
 
-# In[2]:
 
 
-m, n, d, s, K = 1024, 1024, 3, 5, 4
-I = torch.rand(m,n,d).cuda()
-torch.cuda.memory_allocated(0)
+m, n, d, s, K = 30, 30, 1, 5, 4
 
+id_device = 0
+device = 'cuda:'+str(id_device) if torch.cuda.is_available() else 'cpu'
 
-# In[3]:
+I = torch.rand(m,n,d).to(device)
 
+if 'cuda' in device:
+    torch.cuda.memory_allocated(id_device)
 
 # testing with PyTorch
 import time
@@ -72,20 +62,12 @@ if m*n<10000:
     print("elapsed torch : ",time.time()-start)
 
 
-# In[4]:
 
-
-# testing with KeOps
-P_i = LazyTensor_patches(I,s,axis=0,use_ranges=False)
-torch.cuda.memory_allocated(0)
-
-
-# In[5]:
-
+# testing with KeOps - with ranges
 
 start = time.time()
-P_i = LazyTensor_patches(I,s,axis=0,use_ranges=False)
-P_j = LazyTensor_patches(I,s,axis=1,use_ranges=False)
+P_i = LazyTensor_patches(I,s,axis=0)
+P_j = LazyTensor_patches(I,s,axis=1)
 D2 = P_i.sqdist(P_j)
 out_keops = D2.argKmin(dim=1,K=4)
 # now post-processing to get rid of the garbage rows of the output
@@ -103,14 +85,26 @@ if m*n<10000:
 
 
 
-# In[6]:
+# testing with KeOps - with padding and no ranges
 
+Ipad = torch.cat((I,1e20*torch.ones(m,1,d)),dim=1)
 
-I.shape
+start = time.time()
+P_i = LazyTensor_patches(Ipad,s,axis=0,use_ranges=False)
+P_j = LazyTensor_patches(Ipad,s,axis=1,use_ranges=False)
+D2 = P_i.sqdist(P_j)
+out_keops = D2.argKmin(dim=1,K=4)
+# now post-processing to get rid of the garbage rows of the output
+ind_keep = torch.arange(m*(n+1)).view(m,n+1)[:-s+1,:-s].flatten()
+# here the output corresponds to patch indices, for the list of patches that includes
+# garbage patches. So we need also to convert the indices
+out_keops = out_keops[ind_keep,:]
+q = out_keops // (n+1)
+r = out_keops % (n+1)
+out_keops = q*(n-s+1)+r
+print("elapsed keops : ",time.time()-start)
 
-
-# In[ ]:
-
-
+if m*n<10000:
+    print("erreur : ",(out_keops-out_torch).abs().sum().item())
 
 
