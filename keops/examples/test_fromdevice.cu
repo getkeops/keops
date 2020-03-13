97ffc0263f37b5bdd026541fbfbb614137c1c291
Joan Glaunès <joan.glaunes@gmail.com>
2020-03-13 15:26:18 +0100

    [ci skip] WIP

--- keops/examples/test_fromdevice.cu
+++ keops/examples/test_fromdevice.cu
@@ -1,6 +1,6 @@
 // test convolution
 // compile with
-//		nvcc -I.. -DCUDA_BLOCK_SIZE=192 -DMAXTHREADSPERBLOCK0=1024 -DSHAREDMEMPERBLOCK0=49152 -Wno-deprecated-gpu-targets -std=c++14 -O2 -o build/test_fromdevice test_fromdevice.cu
+//		nvcc -I../../../keops -DCUDA_BLOCK_SIZE=192 -DMAXTHREADSPERBLOCK0=1024 -DSHAREDMEMPERBLOCK0=49152 -Wno-deprecated-gpu-targets -std=c++14 --use_fast_math -O3 -o test_fromdevice test_fromdevice.cu
 
 // testing "from device" convolution, i.e. convolution which is performed on the device
 // directly from device data
@@ -10,8 +10,11 @@
 
 #include <keops_includes.h>
 
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
+
 #define DIMPOINT 3
-#define DIMVECT 2
+#define DIMVECT 1
 
 __TYPE__ floatrand() {
     return ((__TYPE__) std::rand())/RAND_MAX-.5;    // random value between -.5 and .5
@@ -44,12 +47,11 @@ int main() {
     cudaSetDevice(deviceID);
 
     // symbolic expression of the function : a gaussian kernel
-    auto c = Pm(0,1);
-    auto x = Vi(1,DIMPOINT);
-    auto y = Vj(2,DIMPOINT);
-    auto beta = Vj(3,DIMVECT);
+    auto x = Vi(0,DIMPOINT);
+    auto y = Vj(1,DIMPOINT);
+    auto beta = Vj(2,DIMVECT);
     
-    auto f = Exp(-c*SqNorm2(x-y)) * beta; 
+    auto f = Exp(-SqNorm2(x-y)) * beta; 
 
     std::cout << std::endl << "Function f : " << std::endl;
     std::cout << PrintFormula(f);
@@ -59,7 +61,7 @@ int main() {
 
     // now we test ------------------------------------------------------------------------------
 
-    int Nx=4000, Ny=60000;
+    int Nx=1000000, Ny=1000000;
 
     std::vector<__TYPE__> vx(Nx*x.DIM);    fillrandom(vx); __TYPE__ *px = vx.data();
     thrust::device_vector<__TYPE__> vx_d(vx);
@@ -76,104 +78,48 @@ int main() {
     thrust::device_vector<__TYPE__> vres_d(Nx*Sum_f.DIM);
     __TYPE__ *res_d = thrust::raw_pointer_cast(vres_d.data());
     
-    __TYPE__ param = 0.5;
-    thrust::device_vector<__TYPE__> vparam_d(c.DIM);
-    vparam_d[0] = param;
-    __TYPE__ *param_d = thrust::raw_pointer_cast(vparam_d.data());
-    
+
+
 
     clock_t begin, end;
 
     std::cout << "blank run 1" << std::endl;
     begin = clock();
-    EvalRed<GpuConv2D_FromDevice>(Sum_f,Nx, Ny, res_d, param_d, x_d, y_d, b_d);
+    EvalRed<GpuConv1D_FromDevice>(Sum_f,Nx, Ny, res_d, x_d, y_d, b_d);
     end = clock();
     std::cout << "time for blank run 1 : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
 
     std::cout << "blank run 2" << std::endl;
     begin = clock();
-    EvalRed<GpuConv2D_FromDevice>(Sum_f,Nx, Ny, res_d, param_d, x_d, y_d, b_d);
+    EvalRed<GpuConv1D_FromDevice>(Sum_f,Nx, Ny, res_d, x_d, y_d, b_d);
     end = clock();
     std::cout << "time for blank run 2 : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
 
 
 
 
-
+    int Ntest = 1;
 
     std::cout << "testing From_Device mode" << std::endl;
-    begin = clock();
-    for(int i=0; i<100; i++)
-        EvalRed<GpuConv2D_FromDevice>(Sum_f,Nx, Ny, res_d, param_d, x_d, y_d, b_d);
-    end = clock();
-    std::cout << "time for 100 GPU computations (2D scheme) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
 
-    std::vector<__TYPE__> resgpu2D(Nx*Sum_f.DIM);
-    cudaMemcpy(resgpu2D.data(), res_d, Nx*Sum_f.DIM*sizeof(__TYPE__), cudaMemcpyDeviceToHost);
+    //begin = clock();
+    auto start = Clock::now();
 
-    // display output
-    std::cout << std::endl << "resgpu2D_device =";
-    DispValues(resgpu2D.data(),5,Sum_f.DIM);
+    for(int i=0; i<Ntest; i++)
+        EvalRed<GpuConv1D_FromDevice>(Sum_f,Nx, Ny, res_d, x_d, y_d, b_d);
 
-    begin = clock();
-    for(int i=0; i<100; i++)
-        EvalRed<GpuConv1D_FromDevice>(Sum_f,Nx, Ny, res_d, param_d, x_d, y_d, b_d);
-    end = clock();
-    std::cout << "time for 100 GPU computations (1D scheme) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+    //end = clock();
+    //std::cout << "time for "<< Ntest <<" GPU computations (1D scheme) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+    auto stop = Clock::now();
+    std::cout << "time = " 
+                  << Ntest << "x "
+                  << (float) std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() / (float) (1000000 * Ntest)
+                  << " milliseconds" << std::endl;
 
     std::vector<__TYPE__> resgpu1D(Nx*Sum_f.DIM);
     cudaMemcpy(resgpu1D.data(), res_d, Nx*Sum_f.DIM*sizeof(__TYPE__), cudaMemcpyDeviceToHost);
 
-    // display output
-    std::cout << std::endl << "resgpu1D_device =";
-    DispValues(resgpu1D.data(),5,Sum_f.DIM);
 
-    // display mean of errors
-    __TYPE__ s = 0;
-    for(int i=0; i<Nx*Sum_f.DIM; i++)
-        s += std::abs(resgpu1D[i]-resgpu2D[i]);
-    std::cout << "mean abs error 1D/2D = " << s/Nx << std::endl;
-
-
-
-
-
-
-    std::cout << "testing From_Host mode" << std::endl;
-
-    std::vector<__TYPE__> resgpu2D_host(Nx*Sum_f.DIM);
-    begin = clock();
-    for(int i=0; i<100; i++)
-        EvalRed<GpuConv2D_FromHost>(Sum_f, Nx, Ny, resgpu2D_host.data(), &param, px, py, pb);
-    end = clock();
-    std::cout << "time for 100 GPU computations (2D scheme) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-
-    // display output
-    std::cout << std::endl << "resgpu2D_host =";
-    DispValues(resgpu2D_host.data(),5,Sum_f.DIM);
-
-    std::vector<__TYPE__> resgpu1D_host(Nx*Sum_f.DIM);
-    begin = clock();
-    for(int i=0; i<100; i++)
-        EvalRed<GpuConv1D_FromHost>(Sum_f, Nx, Ny, resgpu1D_host.data(), &param, px, py, pb);
-    end = clock();
-    std::cout << "time for 100 GPU computations (1D scheme) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-
-    // display output
-    std::cout << std::endl << "resgpu1D_host =";
-    DispValues(resgpu1D_host.data(),5,Sum_f.DIM);
-
-    // display mean of errors
-    s = 0;
-    for(int i=0; i<Nx*Sum_f.DIM; i++)
-        s += std::abs(resgpu1D_host[i]-resgpu2D_host[i]);
-    std::cout << "mean abs error 1D/2D = " << s/Nx << std::endl;
-
-    // display mean of errors
-    s = 0;
-    for(int i=0; i<Nx*Sum_f.DIM; i++)
-        s += std::abs(resgpu1D_host[i]-resgpu1D[i]);
-    std::cout << "mean abs error host/device = " << s/Nx << std::endl;
 
 
 
