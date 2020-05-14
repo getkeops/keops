79fe72bd8b6ddfe5e5e788b4d62223dbae936f0c
GD <gd.dev@libertymail.net>
2020-05-14 11:42:27 +0200

    check for cmake in path

--- rkeops/R/zzz.R
+++ rkeops/R/zzz.R
@@ -5,6 +5,7 @@
 }
 
 .onLoad <- function(libname, pkgname) {
+    check_cmake(get_cmake())
     if(is_installed() & check_os(onLoad=TRUE)) {
         # set up rkeops global options
         set_rkeops_options()
