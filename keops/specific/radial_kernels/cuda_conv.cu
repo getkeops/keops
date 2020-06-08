c3230879d4839d97caa32f985a1156000d2246aa
Joan Glaunès <joan.glaunes@gmail.com>
2020-06-08 16:12:46 +0200

    [ci skip] removing updates from branch tile_rect

--- keops/specific/radial_kernels/cuda_conv.cu
+++ keops/specific/radial_kernels/cuda_conv.cu
@@ -2,108 +2,117 @@
 #include <assert.h>
 #include <cuda.h>
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-#include <keops_includes.h>
-#include "core/mapreduce/GpuConv1D.cu"
-
 #include "specific/radial_kernels/radial_kernels.h"
 #include "specific/radial_kernels/cuda_conv.cx"
-#include "specific/radial_kernels/cuda_tile.cx"
-
-#define TOTO_DIM 128
-#define BLOCK_SIZE 64
-#define TILE_SIZE 32
-
-__TYPE__ floatrand() {
-    return ((__TYPE__) std::rand())/RAND_MAX-.5;    // random value between -.5 and .5
-}
-
-template < class V > void fillrandom(V& v) {
-    generate(v.begin(), v.end(), floatrand);    // fills vector with random values
-}
-using namespace keops;
-
-int main() {
-
-    // now we test ------------------------------------------------------------------------------
-
-    int Nx=10000, Ny=10000;
-
-    // here we define actual data for all variables and feed it it with random values
-    std::vector<__TYPE__> vx(Nx*TOTO_DIM); fillrandom(vx); __TYPE__ *px = vx.data();
-    std::vector<__TYPE__> vy(Ny*TOTO_DIM); fillrandom(vy); __TYPE__ *py = vy.data();
-    std::vector<__TYPE__> vb(Ny*TOTO_DIM); fillrandom(vb); __TYPE__ *pb = vb.data();
-
-    // also a vector for the output
-    //std::vector<__TYPE__> vres(Nx*DIM); fillrandom(vres); __TYPE__ *pres = vres.data();
-
-    // and three vectors to get copies of the output, for comparing Cpu vs Gpu/1D vs Gpu/2D computations
-    std::vector<__TYPE__> resgpu3D(Nx*TOTO_DIM), resgpu2D(Nx*TOTO_DIM), resgpu1D(Nx*TOTO_DIM);
-
-    // parameter variable
-    __TYPE__ ooSigma = 1.0;
-
-    clock_t begin, end;
-
-    std::cout << "blank run" << std::endl;
-    KernelGpuEvalConv<__TYPE__,GaussF, TOTO_DIM, BLOCK_SIZE>(ooSigma, px, py, pb, resgpu1D.data(), TOTO_DIM, TOTO_DIM, Nx, Ny);
 
 
-    begin = clock();
-    KernelGpuEvalConv<__TYPE__,GaussF, TOTO_DIM, BLOCK_SIZE>(ooSigma, px, py, pb, resgpu1D.data(), TOTO_DIM, TOTO_DIM, Nx, Ny);
-    end = clock();
-    std::cout << "time for GPU computation (1D scheme, specific) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+//////////////////////////////////////////////////////
+/////////// CPU -> GPU -> CPU routines ///////////////
+//////////////////////////////////////////////////////
 
+template < typename TYPE, KernelFun KernelF >
+int KernelGpuEvalConv(TYPE ooSigma2,
+                      TYPE* x_h, TYPE* y_h, TYPE* beta_h, TYPE* gamma_h,
+                      int dimPoint, int dimVect, int nx, int ny) {
 
+    // Data on the device.
+    TYPE* x_d;
+    TYPE* y_d;
+    TYPE* beta_d;
+    TYPE* gamma_d;
 
-    begin = clock();
-    KernelGpuEvalTile<__TYPE__,GaussF, TOTO_DIM, BLOCK_SIZE, TILE_SIZE>(ooSigma, px, py, pb, resgpu3D.data(), TOTO_DIM, TOTO_DIM, Nx, Ny);
-    end = clock();
-    std::cout << "time for GPU computation (1D tiled scheme, specific) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+    // Allocate arrays on device.
+    cudaMalloc((void**)&x_d,     sizeof(TYPE)*(nx*dimPoint));
+    cudaMalloc((void**)&y_d,     sizeof(TYPE)*(ny*dimPoint));
+    cudaMalloc((void**)&beta_d,  sizeof(TYPE)*(ny*dimVect ));
+    cudaMalloc((void**)&gamma_d, sizeof(TYPE)*(nx*dimVect ));
 
+    // Set values to zeros
+    cudaMemset(x_d,    0, sizeof(TYPE)*(nx*dimPoint));
+    cudaMemset(y_d,    0, sizeof(TYPE)*(ny*dimPoint));
+    cudaMemset(beta_d, 0, sizeof(TYPE)*(ny*dimVect ));
+    cudaMemset(gamma_d,0, sizeof(TYPE)*(nx*dimVect ));
 
+    // Send data from host to device.
+    cudaMemcpy(x_d,    x_h,    sizeof(TYPE)*(nx*dimPoint), cudaMemcpyHostToDevice);
+    cudaMemcpy(y_d,    y_h,    sizeof(TYPE)*(ny*dimPoint), cudaMemcpyHostToDevice);
+    cudaMemcpy(beta_d, beta_h, sizeof(TYPE)*(ny*dimVect ), cudaMemcpyHostToDevice);
 
+    // Compute on device.
+    dim3 blockSize;
+    blockSize.x = CUDA_BLOCK_SIZE; // number of threads in each block
+    dim3 gridSize;
+    gridSize.x =  nx / blockSize.x + (nx%blockSize.x==0 ? 0 : 1);
 
+    // Copy-paste templating, allowing us to pass the DIMPOINT and DIMVECT at compilation time :
+    if(     dimPoint==1 && dimVect==1)
+        KernelGpuConvOnDevice<TYPE,1,1,KernelF><<<gridSize,blockSize,blockSize.x*(dimVect+dimPoint)*sizeof(TYPE)>>>
+        (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    else if(dimPoint==3 && dimVect==1)
+        KernelGpuConvOnDevice<TYPE,3,1,KernelF><<<gridSize,blockSize,blockSize.x*(dimVect+dimPoint)*sizeof(TYPE)>>>
+        (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    else if(dimPoint==2 && dimVect==1)
+        KernelGpuConvOnDevice<TYPE,2,1,KernelF><<<gridSize,blockSize,blockSize.x*(dimVect+dimPoint)*sizeof(TYPE)>>>
+        (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    else if(dimPoint==2 && dimVect==2)
+        KernelGpuConvOnDevice<TYPE,2,2,KernelF><<<gridSize,blockSize,blockSize.x*(dimVect+dimPoint)*sizeof(TYPE)>>>
+        (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    else if(dimPoint==3 && dimVect==3)
+        KernelGpuConvOnDevice<TYPE,3,3,KernelF><<<gridSize,blockSize,blockSize.x*(dimVect+dimPoint)*sizeof(TYPE)>>>
+        (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    else {
+        printf("Error: dimensions of Gauss kernel not implemented in cuda. You probably just need a copy-paste in the conda_conv.cu file !");
+        cudaFree(x_d);
+        cudaFree(y_d);
+        cudaFree(beta_d);
+        cudaFree(gamma_d);
+        return(-1);
+    }
 
+    // block until the device has completed
+    cudaDeviceSynchronize();
 
+    // Send data from device to host.
+    cudaMemcpy(gamma_h, gamma_d, sizeof(TYPE)*(nx*dimVect),cudaMemcpyDeviceToHost);
 
-
-/*
-    // In this part we define the symbolic variables of the function
-    auto p = Pm(0,1);	 // p is the first variable and is a scalar parameter
-    auto x = Vi(1,TOTO_DIM); 	 // x is the second variable and represents a 3D vector, "i"-indexed.
-    auto y = Vj(2,TOTO_DIM); 	 // y is the third variable and represents a 3D vector, "j"-indexed.
-    auto beta = Vj(3,TOTO_DIM); // beta is the sixth variable and represents a 3D vector, "j"-indexed.
-    __TYPE__ params[1];
-    params[0] = ooSigma;
-    // symbolic expression of the function ------------------------------------------------------
-
-    // here we define f = <u,v>^2 * exp(-p*|x-y|^2) * beta in usual notations
-    auto f = Exp(-p*SqNorm2(x-y)) * beta;
-
-    // We define the reduction operation on f. Here a sum reduction, performed over the "j" index, and resulting in a "i"-indexed variable
-    auto Sum_f = Sum_Reduction(f,0);  // 0 means output of reduction will be "i"-indexed (0 means"i", 1 means "j")
-
-
-    begin = clock();
-    EvalRed<GpuConv1D_FromHost>(Sum_f, Nx, Ny, resgpu2D.data(), params, px, py, pb);
-    end = clock();
-    std::cout << "time for GPU computation (1D scheme, generic) : " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-*/
-
-    for(int i=0; i<50; i++)
-        std::cout << i << " = " << resgpu2D[i] << " " << resgpu1D[i] << " " << resgpu3D[i] << std::endl;
-
-    // display mean of errors
-    __TYPE__ s = 0;
-    for(int i=0; i<Nx*TOTO_DIM; i++)
-        s += std::abs(resgpu1D[i]-resgpu3D[i]);
-    std::cout << "mean abs error 2D =" << s << std::endl;
-
+    // Free memory.
+    cudaFree(x_d);
+    cudaFree(y_d);
+    cudaFree(beta_d);
+    cudaFree(gamma_d);
 
     return 0;
-
 }
+
+
+// Couldn't find a clean way to give a name to an explicit instantiation :-(
+
+// This instantiation bypass the function KernelGpuEvalConv as the pointers contain a address directly on the device
+extern "C" int GaussGpuEval_onDevice(__TYPE__ ooSigma2, __TYPE__* x_d, __TYPE__* y_d, __TYPE__* beta_d, __TYPE__* gamma_d, int dimPoint, int dimVect, int nx, int ny) {
+    dim3 blockSize (CUDA_BLOCK_SIZE,1,1); // number of threads in each block
+    dim3 gridSize (nx / blockSize.x + (nx%blockSize.x==0 ? 0 : 1));
+
+    KernelGpuConvOnDevice<__TYPE__,3,3,GaussF><<<gridSize,blockSize,blockSize.x*(3+3)*sizeof(__TYPE__)>>>
+    (ooSigma2, x_d, y_d, beta_d, gamma_d, nx, ny);
+    return 0;
+}
+
+extern "C" int GaussGpuEval(__TYPE__ ooSigma2, __TYPE__* x_h, __TYPE__* y_h, __TYPE__* beta_h, __TYPE__* gamma_h, int dimPoint, int dimVect, int nx, int ny) {
+    return KernelGpuEvalConv<__TYPE__,GaussF>(ooSigma2, x_h, y_h, beta_h, gamma_h, dimPoint, dimVect, nx, ny);
+}
+extern "C" int LaplaceGpuEval(__TYPE__ ooSigma2, __TYPE__* x_h, __TYPE__* y_h, __TYPE__* beta_h, __TYPE__* gamma_h, int dimPoint, int dimVect, int nx, int ny) {
+    return KernelGpuEvalConv<__TYPE__,LaplaceF>(ooSigma2, x_h, y_h, beta_h, gamma_h, dimPoint, dimVect, nx, ny);
+}
+extern "C" int InverseMultiquadricGpuEval(__TYPE__ ooSigma2, __TYPE__* x_h, __TYPE__* y_h, __TYPE__* beta_h, __TYPE__* gamma_h, int dimPoint, int dimVect, int nx, int ny) {
+    return KernelGpuEvalConv<__TYPE__,InverseMultiquadricF>(ooSigma2, x_h, y_h, beta_h, gamma_h, dimPoint, dimVect, nx, ny);
+}
+extern "C" int CauchyGpuEval(__TYPE__ ooSigma2, __TYPE__* x_h, __TYPE__* y_h, __TYPE__* beta_h, __TYPE__* gamma_h, int dimPoint, int dimVect, int nx, int ny) {
+    return KernelGpuEvalConv<__TYPE__,CauchyF>(ooSigma2, x_h, y_h, beta_h, gamma_h, dimPoint, dimVect, nx, ny);
+}
+
+
+
+void ExitFcn(void) {
+    cudaDeviceReset();
+}
+
