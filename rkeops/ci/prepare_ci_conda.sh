cf2e83ab69a9dfd85a8f0ef36225d0a633b5f3b6
GD <gd.dev@libertymail.net>
2020-09-03 18:05:52 +0200

    fix channel set up

--- rkeops/ci/prepare_ci_conda.sh
+++ rkeops/ci/prepare_ci_conda.sh
@@ -2,4 +2,4 @@
 
 # For MacOS VM where R is managed through conda
 # install required R packages from conda
-conda install r-devtools r-knitr r-rcpp r-rcppeigen r-rmarkdown r-roxygen2 r-stringr r-testthat
+conda install -c r r-devtools r-knitr r-rcpp r-rcppeigen r-rmarkdown r-roxygen2 r-stringr r-testthat
