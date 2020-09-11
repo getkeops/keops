2118ba28168f4076fb823e29d36cf23be300d159
Unknown <joan.glaunes@gmail.com>
2020-09-11 10:50:05 +0200

    again fix for Cuda 11

--- keops/core/autodiff/ChunkableUnaryOp.h
+++ keops/core/autodiff/ChunkableUnaryOp.h
@@ -10,9 +10,25 @@ namespace keops {
 template< template<class,int...> class OP, class F, int... NS >
 struct ChunkableUnaryOp : UnaryOp<OP, F, NS...> {
 	
-  template < int DIMCHK >
-  using CHUNKED_VERSION = OP < typename F::template CHUNKED_VERSION<DIMCHK>, NS... >;
-
+    // NB. The following commented code should be ok but it dos not compile with Cuda 11 as of 2020 aug 13th...
+    /*
+    template < int DIMCHK >
+    using CHUNKED_VERSION = OP<typename F::template CHUNKED_VERSION<DIMCHK>,NS...>;
+    */
+    // ... so we use an additional "_Impl" structure to specialize in case of empty PARAMS pack : 
+    template < int DIMCHK, int SIZE_NS >
+    struct CHUNKED_VERSION_Impl {
+      using type = OP<typename F::template CHUNKED_VERSION<DIMCHK>,NS...>;
+    };
+    
+    template < int DIMCHK >
+    struct CHUNKED_VERSION_Impl < DIMCHK, 0 > {
+      using type = OP<typename F::template CHUNKED_VERSION<DIMCHK> >;
+    };
+    
+    template < int DIMCHK >
+    using CHUNKED_VERSION = typename CHUNKED_VERSION_Impl<DIMCHK,sizeof...(NS)>::type;
+    
   template < int CAT >
   using CHUNKED_VARS = typename F::template CHUNKED_VARS<CAT>;
 
