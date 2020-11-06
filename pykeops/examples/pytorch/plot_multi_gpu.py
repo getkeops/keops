e05f1a056de9a3a0c6f80fa2ac1a4ee1324e2230
Joan Glaunès <joan.glaunes@gmail.com>
2020-11-06 15:41:06 +0100

    [ci skip] ok works now

--- pykeops/examples/pytorch/plot_multi_gpu.py
+++ pykeops/examples/pytorch/plot_multi_gpu.py
@@ -14,6 +14,7 @@ let's see how to make use of several Gpus for further speedups
 # -------------
 # Standard imports:
 
+import math
 import time
 import torch
 from pykeops.torch import LazyTensor
@@ -36,33 +37,58 @@ y = torch.randn(N, 3)
 #  Define a symbolic gaussian kernel 
 #  reduction using LazyTensor syntax
 #
-def GaussKernelSum(x,y):
+def GaussKernelSum(x,y,gpuid=-1):
     x_i = LazyTensor( x[:,None,:] )     # x_i.shape = (M, 1, 3)
     y_j = LazyTensor( y[None,:,:] )     # y_j.shape = (1, N, 3)
     D_ij = ((x_i - y_j)**2).sum(dim=2)  # Symbolic (M,N,1) matrix of squared distances
     K_ij = (- D_ij).exp()               # Symbolic (M,N,1) Gaussian kernel matrix
-    return K_ij.sum(dim=1)
+    return K_ij.sum(dim=1, device_id=gpuid)
     
-#########################################
-# Compute on GPUs in parallel, dividing
-# y data into several parts
+###############################################################
+#  dummy calls to the routine
+#
+for gpuid in range(ngpus):
+    GaussKernelSum(x[:100,:],y[:100,:],gpuid)
+
+###############################################################
+# Compute on several GPUs in parallel, 
+# (block parallelizing over the reduction index)
 #
 
 start = time.time()
 pool = ThreadPoolExecutor(ngpus)
-subsize_y = int(N/ngpus)
+subsize_y = math.ceil(N/ngpus)
 futures = []
 for gpuid in range(ngpus):
-    with torch.cuda.device(gpuid):
-        ygpu = y[gpuid*subsize_y:(gpuid+1)*subsize_y,:].cuda()
-        xgpu = x.cuda()
-        futures.append(pool.submit(GaussKernelSum, xgpu, ygpu))
+    y_chunk = y[gpuid*subsize_y:(gpuid+1)*subsize_y,:]
+    future = pool.submit(GaussKernelSum, x, y_chunk, gpuid)
+    futures.append(future)
 wait(futures)
-out_multi = 0
+out_multi_1 = 0
 for gpuid in range(ngpus):
-    out_multi += futures[gpuid].result().cpu()
+    out_multi_1 += futures[gpuid].result().cpu()
 elapsed = time.time() - start
-print('multi-Gpu : ',elapsed)
+print('time for multi-Gpu computation (block parallelized over reduction index):{:.2f} s'.format(elapsed))
+
+###############################################################
+# Compute on several GPUs in parallel, 
+# (block parallelizing over the output index)
+#
+
+start = time.time()
+pool = ThreadPoolExecutor(ngpus)
+subsize_y = math.ceil(N/ngpus)
+futures = []
+for gpuid in range(ngpus):
+    x_chunk = x[gpuid*subsize_y:(gpuid+1)*subsize_y,:]
+    future = pool.submit(GaussKernelSum, x_chunk, y, gpuid)
+    futures.append(future)
+wait(futures)
+out_multi_2 = torch.zeros(M, 3)
+for gpuid in range(ngpus):
+    out_multi_2[gpuid*subsize_y:(gpuid+1)*subsize_y,:] = futures[gpuid].result().cpu()
+elapsed = time.time() - start
+print('time for multi-Gpu computation (block parallelized over output index):{:.2f} s'.format(elapsed))
 
 #########################################
 # Compare with single Gpu computation
@@ -70,10 +96,12 @@ print('multi-Gpu : ',elapsed)
 start = time.time()
 out_single = GaussKernelSum(x,y)
 elapsed = time.time() - start
-print('single-Gpu : ',elapsed)
+print('time for single-Gpu computation: {:.2f} s'.format(elapsed))
 
 #########################################
 # Check outputs are the same
 #
-print('relative error : ',(torch.norm(out_single-out_multi)/torch.norm(out_single)).item())
+rel_err1 = (torch.norm(out_single-out_multi_1)/torch.norm(out_single)).item()
+rel_err2 = (torch.norm(out_single-out_multi_2)/torch.norm(out_single)).item()
+print('relative errors: {:.1e}, {:.1e}'.format(rel_err1, rel_err2))
 
