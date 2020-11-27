6ab856854f64021ed8886b7e0b8fd3c1915c956a
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2020-11-27 20:11:10 +0100

    [ci skip] WIP

--- keops/core/link_autodiff.cpp
+++ keops/core/link_autodiff.cpp
@@ -11,25 +11,41 @@ extern "C" int GetFormulaConstants(int *out) {
 	
 	out[0] = F::NMINARGS;
 	out[1] = F::tagI;
-	out[2] = F::POS_FIRST_ARGI;
-	out[3] = F::POS_FIRST_ARGJ;
+	if(F::tagI==0) {
+		out[2] = F::POS_FIRST_ARGI;
+		out[3] = F::POS_FIRST_ARGJ;
+		out[5] = F::NVARSI;
+		out[6] = F::NVARSJ;
+	}
+	else {
+		out[2] = F::POS_FIRST_ARGJ;
+		out[3] = F::POS_FIRST_ARGI;
+		out[5] = F::NVARSJ;
+		out[6] = F::NVARSI;
+	}
 	out[4] = F::NVARS;
-	out[5] = F::NVARSI;
-	out[6] = F::NVARSJ;
 	out[7] = F::NVARSP;
 	out[8] = F::DIM;
 	return 0;
 }
 
 extern "C" int GetIndsI(int *out) {
-	for (int k=0; k<F::NVARSI; k++)
-		out[k] = F::INDSI::VAL(k);
+	if(F::tagI==0)
+		for (int k=0; k<F::NVARSI; k++)
+			out[k] = F::INDSI::VAL(k);
+	else
+		for (int k=0; k<F::NVARSJ; k++)
+			out[k] = F::INDSJ::VAL(k);
 	return 0;
 }
 
 extern "C" int GetIndsJ(int *out) {
-	for (int k=0; k<F::NVARSJ; k++)
-		out[k] = F::INDSJ::VAL(k);
+	if(F::tagI==0)
+		for (int k=0; k<F::NVARSJ; k++)
+			out[k] = F::INDSJ::VAL(k);
+	else
+		for (int k=0; k<F::NVARSI; k++)
+			out[k] = F::INDSI::VAL(k);
 	return 0;
 }
 
@@ -40,14 +56,22 @@ extern "C" int GetIndsP(int *out) {
 }
 
 extern "C" int GetDimsX(int *out) {
-	for (int k=0; k<F::NVARSI; k++)
-		out[k] = F::DIMSX::VAL(k);
+	if(F::tagI==0)
+		for (int k=0; k<F::NVARSI; k++)
+			out[k] = F::DIMSX::VAL(k);
+	else
+		for (int k=0; k<F::NVARSJ; k++)
+			out[k] = F::DIMSY::VAL(k);
 	return 0;
 }
 
 extern "C" int GetDimsY(int *out) {
-	for (int k=0; k<F::NVARSJ; k++)
-		out[k] = F::DIMSY::VAL(k);
+	if(F::tagI==0)
+		for (int k=0; k<F::NVARSJ; k++)
+			out[k] = F::DIMSY::VAL(k);
+	else
+		for (int k=0; k<F::NVARSI; k++)
+			out[k] = F::DIMSX::VAL(k);
 	return 0;
 }
 
