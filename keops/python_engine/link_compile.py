a2fce35c911ddb908ac6d5c60250b8238d431c44
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2021-02-05 13:57:58 +0100

    [ci skip] reorganizing the code, WIP

--- keops/python_engine/link_compile.py
+++ keops/python_engine/link_compile.py
@@ -1,4 +1,5 @@
 from utils import *
+from sum_schemes import *
 import os
 from ctypes import c_int, c_float, c_double, c_void_p, CDLL, POINTER
 import time
@@ -7,22 +8,25 @@ base_dir_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
 template_path = base_dir_path + "templates"
 build_path = base_dir_path + "build" + os.path.sep
 os.makedirs(build_path, exist_ok=True)
+
+
+
     
 class genred:
     # base class for compiling and launching reductions
     
     library = {}
     
-    def __init__(self, red_formula, dtype, dtypeacc, nargs, sum_scheme):
+    def __init__(self, red_formula, dtype, dtypeacc, nargs, sum_scheme_string):
         # - red_formula is instance of Reduction class
         # - dtype and dtypeacc are strings 
-        self.gencode_filename = get_hash_name(type(self), red_formula, dtype, dtypeacc, nargs, sum_scheme)
+        self.gencode_filename = get_hash_name(type(self), red_formula, dtype, dtypeacc, nargs, sum_scheme_string)
         self.dllname = build_path + os.path.sep + self.gencode_filename + "_" + self.source_code_extension + ".so"
         self.red_formula = red_formula
         self.dtype = dtype
         self.dtypeacc = dtypeacc
         self.nargs = nargs
-        self.sum_scheme = sum_scheme
+        self.sum_scheme_string = sum_scheme_string
         if self.gencode_filename in genred.library:
             rec = genred.library[self.gencode_filename]  
             self.dll = rec["dll"]
@@ -32,17 +36,15 @@ class genred:
             self.load_dll()
     
     def get_code(self):  
-        self.template_source_file = template_path + os.path.sep + self.template_source_filename
         self.gencode_file = build_path + os.path.sep + self.gencode_filename + "." + self.source_code_extension
         self.compile_command = f"{self.compiler} {' '.join(self.compile_options)} {self.gencode_file} -o {self.dllname}"
-        sum_schemes_dict = {"direct_sum" : 0, "block_sum" : 1, "kahan_scheme" : 2}
-        self.compile_options += [f"-DSUM_SCHEME={sum_schemes_dict[self.sum_scheme]}"]
           
         red_formula = self.red_formula
         formula = red_formula.formula
         dtype = self.dtype
         dtypeacc = self.dtypeacc
         nargs = self.nargs
+        self.sum_scheme = eval(self.sum_scheme_string)(red_formula, dtype)
 
         self.varloader = varloader = Var_loader(red_formula)
         
@@ -57,17 +59,8 @@ class genred:
         argnames = new_c_varname("arg", nargs)
         self.args = args = c_variable(argnames, pointer(dtype))
         self.acc = acc = c_array("acc", dtypeacc, red_formula.dimred)
-        self.tmp = tmp = c_array("tmp", dtype, red_formula.dimred)
-        self.tmp_kahan = tmp_kahan = c_array("tmp", dtype, red_formula.dim_kahan)
         self.fout = fout = c_array("fout", dtype, formula.dim)
         self.outi = c_array(f"(out + i * {red_formula.dim})", dtype, red_formula.dim) 
-        
-    def get_template_code(self):
-        # reads the source_file associated with the class
-        f = open(self.template_source_file)
-        template_code = f.read()
-        f.close()
-        return template_code 
         
     def write_code(self):
         f = open(self.gencode_file,"w")
@@ -100,8 +93,7 @@ class genred:
             
 class CpuReduc(genred):
     # class for generating the final C++ code, Cpu version
-    
-    template_source_filename = "CpuReduc.cpp" 
+
     source_code_extension = "cpp"
     compiler = "g++"
     compile_options = ["-shared", "-O3"]
@@ -117,12 +109,36 @@ class CpuReduc(genred):
         fout = self.fout
         outi = self.outi
         acc = self.acc
-        tmp = self.tmp
-        tmp_kahan = self.tmp_kahan
         args = self.args
         table = self.varloader.direct_table(args, i, j)
-        
-        self.code = eval("f'''"+self.get_template_code()+"'''")
+        sum_scheme = self.sum_scheme
+
+        self.code = f"""
+                        #include <cmath>
+
+                        #ifdef USE_OPENMP
+                          #include <omp.h>
+                        #endif
+
+                        extern "C" int Eval(int nx, int ny, {dtype}* out, {signature_list(args)}) {{
+                            #pragma omp parallel for
+                            for (int i = 0; i < nx; i++) {{
+                                {fout.declare()}
+                                {acc.declare()}
+                                {sum_scheme.declare_temporary_accumulator()}
+                                {red_formula.InitializeReduction(acc)}
+                                {sum_scheme.initialize_temporary_accumulator()}
+                                for (int j = 0; j < ny; j++) {{
+                                    {red_formula.formula(fout,table)}
+                                    {sum_scheme.accumulate_result(acc, fout, j)}
+                                    {sum_scheme.periodic_accumulate_temporary(acc)}
+                                }}
+                                {sum_scheme.final_operation(acc)}
+                                {red_formula.FinalizeOutput(acc, outi, i)}
+                            }}
+                            return 0;
+                        }}
+                    """
 
     def __call__(self, nx, ny, out, *args):
         if self.dll is None:
@@ -133,8 +149,7 @@ class CpuReduc(genred):
 
 class GpuReduc1D(genred):
     # class for generating the final C++ code, Gpu version
-    
-    template_source_filename = "GpuReduc1D.cu" 
+
     source_code_extension = "cu"
     compiler = "nvcc"
     compile_options = ["-shared", "-Xcompiler", "-fPIC", "-O3"]
@@ -152,8 +167,6 @@ class GpuReduc1D(genred):
         fout = self.fout
         outi = self.outi
         acc = self.acc
-        tmp = self.tmp
-        tmp_kahan = self.tmp_kahan
         args = self.args
         
         param_loc = self.param_loc
@@ -162,9 +175,99 @@ class GpuReduc1D(genred):
         yjrel = c_array("yjrel", dtype, varloader.dimy)
         table = varloader.table(self.xi, yjrel, self.param_loc)
         jreltile = c_variable("(jrel + tile * blockDim.x)","int")
-        ind_pack_half2 = c_variable("__floats2half2_rn(2*ind,2*ind+1)","half2")
-        
-        self.code = eval("f'''"+self.get_template_code()+"'''")
+            
+        self.code = f"""
+                        #define DIRECT_SUM 0
+                        #define BLOCK_SUM 1
+                        #define KAHAN_SCHEME 2
+
+                        #ifndef USE_HALF
+                          #define USE_HALF 0
+                        #endif
+
+                        #if USE_HALF
+                          #include <cuda_fp16.h>
+                        #endif
+
+                        __global__ void GpuConv1DOnDevice(int nx, int ny, {dtype} *out, {signature_list(args)}) {{
+    
+                          // get the index of the current thread
+                          int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+                          // declare shared mem
+                          extern __shared__ {dtype} yj[];
+
+                          // load parameters variables from global memory to local thread memory
+                          {param_loc.declare()}
+                          {varloader.load_vars("p", param_loc, args)}
+
+                          {fout.declare()}
+                          {xi.declare()}
+                          {acc.declare()}
+                          {sum_scheme.declare_temporary_accumulator()}
+	  
+                          if (i < nx) {{
+                            {red_formula.InitializeReduction(acc)} // acc = 0
+                            {sum_scheme.initialize_temporary_accumulator_first_init()}
+                            {varloader.load_vars('i', xi, args, row_index=i)} // load xi variables from global memory to local thread memory
+                          }}
+
+                          for (int jstart = 0, tile = 0; jstart < ny; jstart += blockDim.x, tile++) {{
+
+                            // get the current column
+                            int j = tile * blockDim.x + threadIdx.x;
+
+                            if (j < ny) {{ // we load yj from device global memory only if j<ny
+                              {varloader.load_vars("j", yjloc, args, row_index=j)} 
+                            }}
+                            __syncthreads();
+
+                            if (i < nx) {{ // we compute x1i only if needed
+                              {dtype} * yjrel = yj;
+                              {sum_scheme.initialize_temporary_accumulator_block_init()}
+                              for (int jrel = 0; (jrel < blockDim.x) && (jrel < ny - jstart); jrel++, yjrel += {varloader.dimy}) {{
+                                {red_formula.formula(fout,table)} // Call the function, which outputs results in fout
+                                {sum_scheme.accumulate_result(acc, fout, jreltile)}
+                              }}
+                              {sum_scheme.final_operation(acc)}
+                            }}
+                            __syncthreads();
+                          }}
+                          if (i < nx) {{
+                            {red_formula.FinalizeOutput(acc, outi, i)} 
+                          }}
+
+                        }}
+
+
+
+                        extern "C" __host__ int Eval(int nx, int ny, int device_id, {dtype} *out, {signature_list(args)}) {{
+
+                            // device_id is provided, so we set the GPU device accordingly
+                            // Warning : is has to be consistent with location of data
+                            cudaSetDevice(device_id);
+	
+                            // Compute on device : grid and block are both 1d
+
+                            //SetGpuProps(devise_id);
+
+                            dim3 blockSize;
+
+                            blockSize.x = 32;
+	
+                            dim3 gridSize;
+                            gridSize.x = nx / blockSize.x + (nx % blockSize.x == 0 ? 0 : 1);
+
+                            GpuConv1DOnDevice <<< gridSize, blockSize, blockSize.x * {varloader.dimy} * sizeof({dtype}) >>> (nx, ny, out, {call_list(args)});
+    
+                            // block until the device has completed
+                            cudaDeviceSynchronize();
+
+                            //CudaCheckError();
+
+                            return 0;
+                        }}
+                    """
 
     def __call__(self, nx, ny, out, *args):
         if self.dll is None:
