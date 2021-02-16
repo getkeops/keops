a5a185656029cc859abf50e7743fef32c5a75b78
GD <gd.dev@libertymail.net>
2021-02-16 16:07:27 +0100

    fix management of dependencies during CI run for rkeops (install requirements only of missing or not up-to-date)

--- rkeops/ci/prepare_ci.R
+++ rkeops/ci/prepare_ci.R
@@ -1,4 +1,4 @@
 pkg_list <- c("devtools", "openssl", "knitr", "Rcpp", "RcppEigen", 
               "RhpcBLASctl", "rmarkdown", 
               "roxygen2", "stringr", "testthat")
-install.packages(pkg_list)
+install_pkg(pkg_list) # function defined in local .Rprofile
--- rkeops/ci/run_ci.sh
+++ rkeops/ci/run_ci.sh
@@ -2,7 +2,10 @@
 
 set -e
 
+# project root dir
 RDIR=$(git rev-parse --show-toplevel)
+
+# rkeops ci dir
 CIDIR=${RDIR}/rkeops/ci
 
 # set up ~/.R/Makevars file for compilation
@@ -10,6 +13,9 @@ bash ${CIDIR}/setup_Makevars.sh
 
 # set up .Rprofile files
 export R_PROFILE_USER=${CIDIR}/.Rprofile
+
+# prepare R requirements
+Rscript ${CIDIR}/prepare_ci.R
 
 # check package build
 Rscript ${CIDIR}/run_check.R
