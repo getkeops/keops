3ff9af1f4b5f524426e40950a1df295b002888fe
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2021-03-09 18:11:01 +0100

    [ci skip] added test script

--- pykeops/sandbox/test_mod.py
+++ pykeops/sandbox/test_mod.py
@@ -8,7 +8,7 @@ def torch_mod(input, modulus, offset=0):
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-offset = -0.1#-math.pi/2
+offset = -math.pi/2
 
 x = torch.rand(10000, 1)*2*math.pi
 y = x.data.clone()
@@ -20,11 +20,12 @@ y.requires_grad = True
 x_i = LazyTensor(x[:, None])
 s1 = x_i.mod(math.pi, offset).sum(0)
 s2 = torch.sum(torch_mod(y, math.pi, offset))
-print("s1 - s2", torch.abs(s1 - s2).item())
-assert torch.abs(s1 - s2) < 1e-3, torch.abs(s1 - s2)
+
+print("relative error : ", (torch.abs(s1 - s2)/torch.abs(s2)).item())
+assert torch.abs(s1 - s2)/torch.abs(s2) < 1e-3
 
 s1.backward()
 s2.backward()
 
-print("grad_s1 - grad_s2", torch.max(torch.abs(x.grad - y.grad)).item())
-assert torch.max(torch.abs(x.grad - y.grad)) < 1e-3
+print("relative error grad : ", (torch.norm(x.grad - y.grad)/torch.norm(y.grad)).item())
+assert torch.norm(x.grad - y.grad)/torch.norm(y.grad) < 1e-3
