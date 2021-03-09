1ed6404cfc56162006b6d31178af4cd733ce729d
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2021-03-09 18:18:37 +0100

    [ci skip] make linter happy

--- pykeops/sandbox/test_mod.py
+++ pykeops/sandbox/test_mod.py
@@ -4,13 +4,13 @@ import math
 from pykeops.torch import LazyTensor
 
 def torch_mod(input, modulus, offset=0):
-    return input - modulus * torch.floor((input - offset)/modulus)
+    return input - modulus * torch.floor((input - offset) / modulus)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-offset = -math.pi/2
+offset = -math.pi / 2
 
-x = torch.rand(10000, 1)*2*math.pi
+x = torch.rand(10000, 1) * 2 * math.pi
 y = x.data.clone()
 x = x.to(device)
 y = y.to(device)
@@ -21,11 +21,11 @@ x_i = LazyTensor(x[:, None])
 s1 = x_i.mod(math.pi, offset).sum(0)
 s2 = torch.sum(torch_mod(y, math.pi, offset))
 
-print("relative error : ", (torch.abs(s1 - s2)/torch.abs(s2)).item())
-assert torch.abs(s1 - s2)/torch.abs(s2) < 1e-3
+print("relative error : ", (torch.abs(s1 - s2) / torch.abs(s2)).item())
+assert torch.abs(s1 - s2) / torch.abs(s2) < 1e-3
 
 s1.backward()
 s2.backward()
 
-print("relative error grad : ", (torch.norm(x.grad - y.grad)/torch.norm(y.grad)).item())
-assert torch.norm(x.grad - y.grad)/torch.norm(y.grad) < 1e-3
+print("relative error grad : ", (torch.norm(x.grad - y.grad) / torch.norm(y.grad)).item())
+assert torch.norm(x.grad - y.grad) / torch.norm(y.grad) < 1e-3
