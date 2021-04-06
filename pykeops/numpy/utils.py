8d23e6c24a3c4fbf209602bf17336ba7dad7b796
Hudson Yeo <44338416+huddyyeo@users.noreply.github.com>
2021-04-06 18:24:49 +0100

    update var name

--- pykeops/numpy/utils.py
+++ pykeops/numpy/utils.py
@@ -166,8 +166,7 @@ class numpytools:
         if approx:
             raise ValueError("Approx not supported on numpy version")
         from pykeops.numpy import LazyTensor
-
-        distance = numpytools.distance_function(metric)
+        distance = numpytools.distance_function(distance)
         N, D = x.shape
         c = np.copy(x[:K, :])
         x_i = LazyTensor(x[:, None, :])
