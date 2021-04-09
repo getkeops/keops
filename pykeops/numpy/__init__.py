03952a7218a80005286f0517cff75e92b91f8628
Hudson Yeo <44338416+huddyyeo@users.noreply.github.com>
2021-04-09 15:53:32 +0100

    test

--- pykeops/numpy/__init__.py
+++ pykeops/numpy/__init__.py
@@ -16,7 +16,7 @@ from .generic.generic_ops import (
     generic_argmin,
     generic_argkmin,
 )
-from .lazytensor.LazyTensor import LazyTensor, ComplexLazyTensor, Vi, Vj, Pm
+from .lazytensor.LazyTensor import LazyTensor, Vi, Vj, Pm
 
 __all__ = sorted(
     [
