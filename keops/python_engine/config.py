62f75d144aebf24551eddc913c773a103406516a
Unknown <joan.glaunes@gmail.com>
2021-05-06 18:21:43 +0200

    [ci skip] added config.py file for managing paths

---
+++ keops/python_engine/config.py
@@ -0,0 +1,6 @@
+import os
+
+base_dir_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
+template_path = base_dir_path + "templates"
+build_path = base_dir_path + "build" + os.path.sep
+
