37a585f63a27fd8ebc7e89ab8e0661f3f324baa3
Unknown <joan.glaunes@gmail.com>
2021-06-07 16:29:21 +0000

    [ci skip] refactoring of the entry point for binders

--- pykeops/common/get_keops_routine.py
+++ pykeops/common/get_keops_routine.py
@@ -1,9 +1,9 @@
 from ctypes import c_int, CDLL
 
 from keops.python_engine.utils.code_gen_utils import get_hash_name
-from keops.python_engine.formulas.variables.Zero import Zero
-from keops.python_engine.formulas.reductions import *
-from keops.python_engine.mapreduce import *
+from keops.python_engine import get_keops_dll
+
+
 
 class create_or_load:
     library = {}
@@ -27,29 +27,13 @@ class get_keops_routine_class:
     
     def __init__(self, map_reduce_id, *args):
         self.dll = None
-        map_reduce_class = eval(map_reduce_id)
-        self.map_reduce_obj = map_reduce_class(*args)
-        
-        # detecting the case of formula being equal to zero, to bypass reduction.
-        rf = self.map_reduce_obj.red_formula
-        if isinstance(rf, Zero_Reduction) or (isinstance(rf.formula, Zero) and isinstance(rf, Sum_Reduction)):
-            self.map_reduce_obj = map_reduce_class.AssignZero(*args)
-                 
-        self.load_dll()
-            
-    def load_dll(self):
-        res = self.map_reduce_obj.get_dll_and_params()
-        self.dllname = res["dllname"]
+        self.dllname, self.tagI, self.dim = get_keops_dll(map_reduce_id, *args)
         self.dll = CDLL(self.dllname)            
-        self.tagI = res["tagI"]
-        self.dim = res["dim"]
-        return self
         
     def __call__(self, nx, ny, device_id, ranges_ctype, out_ctype, *args_ctype):
-        if self.dll is not None:
-            self.dll.launch_keops.argtypes = [c_int, c_int, c_int, ranges_ctype["type"], out_ctype["type"]] + [arg["type"] for arg in args_ctype]
-            c_args = [arg["data"] for arg in args_ctype]
-            self.dll.launch_keops(c_int(nx), c_int(ny), c_int(device_id), ranges_ctype["data"], out_ctype["data"], *c_args)   
+        self.dll.launch_keops.argtypes = [c_int, c_int, c_int, ranges_ctype["type"], out_ctype["type"]] + [arg["type"] for arg in args_ctype]
+        c_args = [arg["data"] for arg in args_ctype]
+        self.dll.launch_keops(c_int(nx), c_int(ny), c_int(device_id), ranges_ctype["data"], out_ctype["data"], *c_args)   
    
             
 def get_keops_routine(*args):
