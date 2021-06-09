5323998298706803a7d4245e65ded3afbb1ea1e9
Unknown <joan.glaunes@gmail.com>
2021-06-09 07:51:19 +0000

    [ci skip] WIP

--- keops/python_engine/config.py
+++ keops/python_engine/config.py
@@ -6,3 +6,13 @@ build_path = base_dir_path + "build" + os.path.sep
 
 # flag for OpenMP support
 use_OpenMP = True  
+
+def get_jit_binary():
+    jit_source_file = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "compilation" + os.path.sep + "keops_nvrtc.cu"
+    jit_binary = build_path + os.path.sep + "keops_nvrtc.so"
+    if not os.path.exists(jit_binary):
+        print("[KeOps] Compiling main dll...", flush=True, end='')
+        jit_compile_command = f"nvcc -shared -Xcompiler -fPIC -lnvrtc -lcuda {jit_source_file} -o {jit_binary}"
+        os.system(jit_compile_command)
+        print("Done.", flush=True)
+    return jit_binary
