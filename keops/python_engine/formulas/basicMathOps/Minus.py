1c9453c569ac3040c602a170522109e5192ed523
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2021-06-21 11:15:04 +0200

    [ci skip] fixed infinite recursion problems

--- keops/python_engine/formulas/basicMathOps/Minus.py
+++ keops/python_engine/formulas/basicMathOps/Minus.py
@@ -6,16 +6,10 @@ from keops.python_engine.formulas.variables.Zero import Zero
 ######    Minus      #####
 ##########################
 
-class Minus(VectorizedScalarOp):
+class Minus_Impl(VectorizedScalarOp):
     """the "minus" vectorized operation"""
     string_id = "Minus"
     print_spec = "-", "pre", 2
-
-    def __new__(cls, arg):
-        if isinstance(arg, Zero):
-            return arg
-        else:
-            return super(Minus, cls).__new__(cls)
 
     def ScalarOp(self, out, arg):
         # returns the atomic piece of c++ code to evaluate the function on arg and return
@@ -25,3 +19,12 @@ class Minus(VectorizedScalarOp):
     def DiffT(self, v, gradin):
         f = self.children[0]
         return -f.Grad(v, gradin)
+
+
+# N.B. The following separate function should theoretically be implemented
+# as a __new__ method of the previous class, but this can generate infinite recursion problems
+def Minus(arg):
+    if isinstance(arg, Zero):
+        return arg
+    else:
+        return Minus_Impl(arg)
--- keops/python_engine/formulas/maths/Square.py
+++ keops/python_engine/formulas/maths/Square.py
@@ -8,16 +8,10 @@ from keops.python_engine.formulas.variables.Zero import Zero
 ##########################
 
 
-class Square(VectorizedScalarOp):
+class Square_Impl(VectorizedScalarOp):
     """the square vectorized operation"""
     string_id = "Square"
     print_spec = "**2", "post", 1
-
-    def __new__(cls, arg):
-        if isinstance(arg, Zero):
-            return arg
-        else:
-            return super(Square, cls).__new__(cls)
 
     def ScalarOp(self, out, arg):
         # returns the atomic piece of c++ code to evaluate the function on arg and return
@@ -29,3 +23,13 @@ class Square(VectorizedScalarOp):
         # [\partial_V (F)**2].gradin = F * [\partial_V F].gradin
         f = self.children[0]
         return IntCst(2) * f.Grad(v, f * gradin)
+
+
+# N.B. The following separate function should theoretically be implemented
+# as a __new__ method of the previous class, but this can generate infinite recursion problems
+def Square(arg):
+    if isinstance(arg, Zero):
+        return arg
+    else:
+        return Square_Impl(arg)
+
