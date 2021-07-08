7c6b51282b8fba50d5dda8a693a69d5ddb098494
Unknown <joan.glaunes@gmail.com>
2021-07-08 13:24:41 +0000

    [ci skip] added detection and use of gpu properties

--- keops/python_engine/config.py
+++ keops/python_engine/config.py
@@ -4,11 +4,11 @@ base_dir_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
 template_path = base_dir_path + "templates"
 build_path = base_dir_path + "build" + os.path.sep
 
+
 # flag for OpenMP support
 use_OpenMP = True
 
-
-def get_jit_binary(check_compile=True):
+def get_jit_binary(gpu_props_compile_flags, check_compile=True):
     # Returns the path to the main KeOps binary (dll) that will be used to JIT compile all formulas.
     # If the dll is not present, it compiles it from source, except if check_compile is False.
     jit_source_file = (
@@ -22,7 +22,8 @@ def get_jit_binary(check_compile=True):
     if check_compile and not os.path.exists(jit_binary):
         print("[KeOps] Compiling main dll...", flush=True, end="")
         bindings_source_dir = base_dir_path + "binders"
-        flags = "-shared -Xcompiler -fPIC -lnvrtc -lcuda"
+        flags = "-shared -Xcompiler -fPIC -lnvrtc -lcuda "
+        flags += gpu_props_compile_flags
         # jit_compile_command = f"nvcc -I {bindings_source_dir} {flags} {jit_source_file} -o {jit_binary}"
         jit_compile_command = f"nvcc {flags} {jit_source_file} -o {jit_binary}"
         os.system(jit_compile_command)
@@ -31,7 +32,8 @@ def get_jit_binary(check_compile=True):
 
 
 def clean_keops(delete_jit_binary=False):
-    jit_binary = get_jit_binary(check_compile=False)
+    from keops.python_engine import gpu_props_compile_flags
+    jit_binary = get_jit_binary(gpu_props_compile_flags, check_compile=False)
     for f in os.scandir(build_path):
         if f.path != jit_binary or delete_jit_binary:
             os.remove(f.path)
