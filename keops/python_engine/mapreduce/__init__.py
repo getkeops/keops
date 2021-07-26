c63b963f25dc5d80acdf2a04d9c2211ae8837bcd
Unknown <joan.glaunes@gmail.com>
2021-07-26 23:49:01 +0000

    [ci skip] final chunks with ranges work

--- keops/python_engine/mapreduce/__init__.py
+++ keops/python_engine/mapreduce/__init__.py
@@ -2,6 +2,8 @@ from .CpuReduc import CpuReduc
 from .CpuReduc_ranges import CpuReduc_ranges
 from .GpuReduc1D import GpuReduc1D
 from .GpuReduc2D import GpuReduc2D
-from .GpuReduc1D_ranges import GpuReduc1D_ranges
 from .GpuReduc1D_chunks import GpuReduc1D_chunks
 from .GpuReduc1D_finalchunks import GpuReduc1D_finalchunks
+from .GpuReduc1D_ranges import GpuReduc1D_ranges
+from .GpuReduc1D_ranges_finalchunks import GpuReduc1D_ranges_finalchunks
+
