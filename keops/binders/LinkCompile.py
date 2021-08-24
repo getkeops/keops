b997db4d22c3b562202103d03790a17fb9418255
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2021-08-24 13:16:53 +0200

    Move oldies to root folder

---
+++ keops/binders/LinkCompile.py
@@ -0,0 +1,117 @@
+import os, time
+
+from keops.utils.code_gen_utils import get_hash_name
+from keops.config.config import build_path
+
+
+class LinkCompile:
+    """
+    Base class for compiling the map_reduce schemes and providing the dll to KeOps bindings.
+    """
+
+    def __init__(self):
+        # N.B. Here self is assumed to be populated by the __init__ of one of the MapReduce classes
+
+        # we create the hash string id corresponding to all parameters, e.g. 7b9a611f7e
+        self.gencode_filename = get_hash_name(
+            type(self),
+            self.red_formula_string,
+            self.aliases,
+            self.nargs,
+            self.dtype,
+            self.dtypeacc,
+            self.sum_scheme_string,
+            self.tagHostDevice,
+            self.tagCpuGpu,
+            self.tag1D2D,
+            self.use_half,
+            self.device_id
+        )
+
+        # info_file is the name of the file that will contain some meta-information required by the bindings, e.g. 7b9a611f7e.nfo
+        self.info_file = build_path + os.path.sep + self.gencode_filename + ".nfo"
+
+        # gencode_file is the name of the source file to be created and then compiled, e.g. 7b9a611f7e.cpp or 7b9a611f7e.cu
+        self.gencode_file = (
+            build_path
+            + os.path.sep
+            + self.gencode_filename
+            + "."
+            + self.source_code_extension
+        )
+
+    def save_info(self):
+        # create info_file to save some parameters : dim (dimension of output vectors),
+        #                                            tagI (O or 1, reduction over i or j indices),
+        #                                            dimy (sum of dimensions of j-indexed vectors)
+        f = open(self.info_file, "w")
+        f.write(f"dim={self.dim}\ntagI={self.tagI}\ndimy={self.dimy}")
+        f.close()
+
+    def read_info(self):
+        # read info_file to retreive dim, tagI, dimy
+        f = open(self.info_file, "r")
+        string = f.read()
+        f.close()
+        tmp = string.split("\n")
+        if len(tmp) != 3:
+            raise ValueError("incorrect info file")
+        tmp_dim, tmp_tag, tmp_dimy = (
+            tmp[0].split("="),
+            tmp[1].split("="),
+            tmp[2].split("="),
+        )
+        if (
+            len(tmp_dim) != 2
+            or tmp_dim[0] != "dim"
+            or len(tmp_tag) != 2
+            or tmp_tag[0] != "tagI"
+            or len(tmp_dimy) != 2
+            or tmp_dimy[0] != "dimy"
+        ):
+            raise ValueError("incorrect info file")
+        self.dim = eval(tmp_dim[1])
+        self.tagI = eval(tmp_tag[1])
+        self.dimy = eval(tmp_dimy[1])
+
+    def write_code(self):
+        # write the generated code in the source file ; this is used as a subfunction of compile_code
+        f = open(self.gencode_file, "w")
+        f.write(self.code)
+        f.close()
+
+    def get_dll_and_params(self):
+        # main method of the class : it compiles - if needed - the code and returns the name of the dll to be run for
+        # performing the reduction, e.g. 7b9a611f7e.so, or in the case of JIT compilation, the name of the main KeOps dll,
+        # and the name of the assembly code file.
+        if not os.path.exists(self.file_to_check):
+            print(
+                "[KeOps] Compiling formula :",
+                self.red_formula,
+                "...",
+                flush=True,
+                end="",
+            )
+            start = time.time()
+            self.compile_code()
+            self.save_info()
+            elapsed = time.time() - start
+            print("Done ({:.2f} s)".format(elapsed))
+        else:
+            self.read_info()
+        return dict(
+            dllname=self.true_dllname,
+            low_level_code_file=self.low_level_code_file,
+            tagI=self.tagI,
+            use_half=self.use_half,
+            tag1D2D=self.tag1D2D,
+            dimred=self.red_formula.dimred,
+            dim=self.dim,
+            dimy=self.dimy,
+            indsi=self.varloader.indsi,
+            indsj=self.varloader.indsj,
+            indsp=self.varloader.indsp,
+            dimsx=self.varloader.dimsx,
+            dimsy=self.varloader.dimsy,
+            dimsp=self.varloader.dimsp,
+        )
---
+++ keops/config/chunks.py
@@ -0,0 +1,71 @@
+# special computation scheme for dim>100
+
+enable_chunk = True
+
+
+def get_enable_chunk():
+    global enable_chunk
+    return enable_chunk
+
+
+def set_enable_chunk(val):
+    global enable_chunk
+    if val==1:
+        enable_chunk = True
+    elif val==0:
+        enable_chunk = False
+
+
+dimchunk = 64
+dim_treshold_chunk = 143
+specdim_use_chunk1 = -1 # originally 80 but deactivated for release 1.4.2
+specdim_use_chunk2 = 109
+specdim_use_chunk3 = 112
+specdim_use_chunk4 = 114
+
+
+# special mode for formula of the type sum_j k(x_i,y_j)*b_j with high dimensional b_j
+enable_final_chunk = True
+
+
+def get_enable_finalchunk():
+    global enable_finalchunk
+    return enable_finalchunk
+
+
+def set_enable_finalchunk(val):
+    global enable_finalchunk
+    if val==1:
+        enable_finalchunk = True
+    elif val==0:
+        enable_finalchunk = False
+
+
+dimfinalchunk = 64
+
+
+def get_dimfinalchunk():
+    global dimfinalchunk
+    return dimfinalchunk
+
+
+def set_dimfinalchunk(val):
+    global dimfinalchunk
+    dimfinalchunk = val
+
+
+def use_final_chunks():
+    global enable_final_chunk
+    global mult_var_highdim
+    return (enable_final_chunk and mult_var_highdim)
+
+
+mult_var_highdim = False
+
+
+def set_mult_var_highdim(val):
+    global mult_var_highdim
+    if val==1:
+        mult_var_highdim = True
+    elif val==0:
+        mult_var_highdim = False
