f8fc6ba79c9fb2227b02075247e697df2bb17468
Joan Alexis Glaunès <alexis.glaunes@parisdescartes.fr>
2022-05-31 11:40:49 +0200

    linting

--- pykeops/pykeops/sandbox/issue_248.py
+++ pykeops/pykeops/sandbox/issue_248.py
@@ -2,17 +2,19 @@ import torch
 import pykeops
 from pykeops.torch import Genred
 
+
 def kernel(v):
-    formula = 'Rsqrt(v)'
-    fn = Genred(formula, ['v=Vi(2)'], reduction_op='Sum', axis=1)
-    res = fn(v, backend='CPU')
+    formula = "Rsqrt(v)"
+    fn = Genred(formula, ["v=Vi(2)"], reduction_op="Sum", axis=1)
+    res = fn(v, backend="CPU")
     return res
+
 
 def test():
     assert torch.cuda.is_available
     v = torch.randn(100, 10)
     return kernel(v)
 
+
 if __name__ == "__main__":
     test()
-
