7fa5012fc5f4fe300eb77cfe50b47b2de73cc8b1
Joan Glaunès <joan.glaunes@gmail.com>
2023-02-17 15:39:41 +0000

    fixed for ranges mode also

--- pykeops/pykeops/sandbox/issue_294.py
+++ pykeops/pykeops/sandbox/issue_294.py
@@ -1,17 +1,18 @@
 import torch
 from pykeops.torch import Genred
 
-X1 = torch.rand(20, 100).cuda().requires_grad_()
-X2 = torch.rand(5, 100).cuda().requires_grad_()
-v = torch.randn(5, 120).cuda().requires_grad_()
-sigma = (.5+torch.rand(100)).cuda().requires_grad_()
+B = 3
+X1 = torch.rand(B, 20, 1, 100).cuda().requires_grad_()
+X2 = torch.rand(B,  1, 5, 100).cuda().requires_grad_()
+v = torch.randn(B,  1, 5, 120).cuda().requires_grad_()
+sigma = (.5+torch.rand(B, 1, 1, 100)).cuda().requires_grad_()
 
 formula = 'Exp(SqDist((x1 / s), (x2 / s)) * IntInv(-2)) * v'
 aliases = [
-    'x1 = Vi(%d)' % (X1.shape[1]),
-    'x2 = Vj(%d)' % (X2.shape[1]),
-    'v = Vj(%d)' % (v.shape[1]),
-    's = Pm(%d)' % (sigma.shape[0])
+    'x1 = Vi(%d)' % (X1.shape[-1]),
+    'x2 = Vj(%d)' % (X2.shape[-1]),
+    'v = Vj(%d)' % (v.shape[-1]),
+    's = Pm(%d)' % (sigma.shape[-1])
 ]
 other_vars = [sigma]
 
@@ -19,7 +20,7 @@ fn = Genred(formula, aliases, reduction_op="Sum", axis=1)
 out = fn(X1, X2, v, *other_vars, backend="GPU_1D")
 print(out.shape)
 
-out_torch = (-.5*((X1[:,None,:]/sigma[None,None,:]-X2[None,:,:]/sigma[None,None,:])**2).sum(axis=2)).exp() @ v
+out_torch = ((-.5*((X1/sigma-X2/sigma)**2).sum(axis=3, keepdim=True)).exp() * v).sum(axis=2, keepdim=True)
 print(out_torch.shape)
 
 print(torch.norm(out).item())
