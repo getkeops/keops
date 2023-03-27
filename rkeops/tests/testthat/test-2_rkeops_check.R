cab603ee289a7fb4c407f1366c2793e274b7e6f5
Ghislain Durif <gd.dev@libertymail.net>
2023-03-27 17:18:19 +0200

    more thorough test of check_os

--- rkeops/tests/testthat/test-2_rkeops_check.R
+++ rkeops/tests/testthat/test-2_rkeops_check.R
@@ -1,5 +1,11 @@
 test_that("check_os", {
     expect_logical(check_os(onLoad = FALSE), len = 1, any.missing = FALSE)
+    
+    if(.Platform$OS.type != "unix") {
+        expect_warning(check_os())
+    } else {
+        expect_true(check_os())
+    }
 })
 
 test_that("check_pypkg", {
