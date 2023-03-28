7e3be8b70a9a7cd701a1373891dc0f943be10f22
Ghislain Durif <gd.dev@libertymail.net>
2023-03-28 15:44:55 +0200

    improve rkeops check_os() function test

--- rkeops/tests/testthat/test-5_rkeops_check.R
+++ rkeops/tests/testthat/test-5_rkeops_check.R
@@ -1,10 +1,13 @@
 test_that("check_os", {
-    expect_logical(check_os(onLoad = FALSE), len = 1, any.missing = FALSE)
+    expect_logical(
+        rkeops:::check_os(onLoad = FALSE), len = 1, any.missing = FALSE)
     
     if(.Platform$OS.type != "unix") {
-        expect_warning(check_os())
+        expect_error(rkeops:::check_os())
+        expect_message(rkeops:::check_os(onLoad = TRUE))
+        expect_false(rkeops:::check_os(onLoad = TRUE))
     } else {
-        expect_true(check_os())
+        expect_true(rkeops:::check_os())
     }
 })
 
