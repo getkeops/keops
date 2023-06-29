2fb38b25e44c098d2d17c3b3950d28313420caa3
Joan Glaunès <joan.glaunes@gmail.com>
2023-06-29 19:44:06 +0200

    update

--- pykeops/pykeops/sandbox/issue_303.py
+++ pykeops/pykeops/sandbox/issue_303.py
@@ -3,19 +3,23 @@ from pykeops.torch import LazyTensor
 
 
 # Details of this function don't matter
-def fn(x_i, x_j, y_j):
+def fn(x_i, x_j, y_j, nbatchdims=0):
     x_i = LazyTensor(x_i)
     x_j = LazyTensor(x_j)
     y_j = LazyTensor(y_j)
     K_ij = (-((x_i - x_j) ** 2).sum(-1)).exp()
-    return (K_ij * y_j).sum(1)
+    return (K_ij * y_j).sum(nbatchdims + 1)
 
 
-# Batching with KeOps: runs
+# Batching with KeOps
 x_i = torch.randn(5, 10, 1, 2)
 x_j = torch.randn(5, 1, 20, 2)
 y_j = torch.randn(5, 1, 20, 1)
-fn(x_i, x_j, y_j)
+res1 = fn(x_i, x_j, y_j, nbatchdims=1)
+print(res1.shape)
 
-# Batching with vmap: raises exception
-torch.vmap(fn)(x_i, x_j, y_j)
+# Batching with vmap
+res2 = torch.vmap(fn)(x_i, x_j, y_j)
+print(res2.shape)
+
+print(torch.norm(res1 - res2) / torch.norm(res1))
