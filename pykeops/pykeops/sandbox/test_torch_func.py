fixing bug when formula is zero, WIP

pykeops/pykeops/sandbox/test_torch_func.py
@@ -1,14 +1,20 @@
 import torch
 from pykeops.torch import LazyTensor
 
-x_i = torch.randn(5, 4, 10, 1, 2)
-y_j = torch.randn(5, 1, 1, 20, 2)
-b_j = torch.randn(5, 4, 1, 20, 1)
-p = torch.rand(5, 1, 1, 1, 1)
-dx_i = torch.randn(5, 4, 10, 1, 2)
-dy_j = torch.randn(5, 1, 1, 20, 2)
-db_j = torch.randn(5, 4, 1, 20, 1)
-dp = torch.randn(5, 1, 1, 1, 1)
+import keopscore
+
+keopscore.auto_factorize = False
+
+B1, B2, M, N, D = 5, 4, 10, 20, 2
+
+x_i = torch.randn(B1, B2, M, 1, D)
+y_j = torch.randn(B1, 1, 1, N, D)
+b_j = torch.randn(B1, B2, 1, N, 1)
+p = torch.rand(B1, 1, 1, 1, 1)
+dx_i = torch.randn(B1, B2, M, 1, D)
+dy_j = torch.randn(B1, 1, 1, N, D)
+db_j = torch.randn(B1, B2, 1, N, 1)
+dp = torch.randn(B1, 1, 1, 1, 1)
 
 
 def fn_torch(x_i, y_j, b_j, p):
@@ -58,7 +64,10 @@ res1 = torch.func.jacfwd(fn_torch)(x_i, y_j, b_j, p)
 res2 = torch.func.jacfwd(fn_keops)(x_i, y_j, b_j, p)
 print("testing jacfwd, error=", torch.norm(res1 - res2) / torch.norm(res1))
 
+
 # 7) testing torch.func.hessian.
 res1 = torch.func.hessian(fn_torch)(x_i, y_j, b_j, p)
+print("norm(res1)=", torch.norm(res1))
 res2 = torch.func.hessian(fn_keops)(x_i, y_j, b_j, p)
+print("norm(res2)=", torch.norm(res2))
 print("testing hessian, error=", torch.norm(res1 - res2) / torch.norm(res1))

pykeops/pykeops/torch/generic/generic_red.py
@@ -124,6 +124,7 @@ class GenredAutograd_base:
             params.out,
             *args,
         )
+
         return result, torch.tensor([myconv.dimout, myconv.tagIJ])
 
     @staticmethod
@@ -333,7 +334,7 @@ else:
             check_AD_supported(formula)
 
             # we define a new variable for the formula's output
-            resvar = f"Var({nargs},{dimout},{tagIJ})"
+            resvar = f"Var({nargs+1},{dimout},{tagIJ})"
 
             # We define Var objects corresponding to grad_inputs tensors.
             # Each Var has the same dim and cat as the corresponding input arg.
@@ -346,10 +347,10 @@ else:
                     grad_input = grad_input.contiguous()
                     _, cat, dim, pos = get_type(sig, position_in_list=var_ind)
                     var = f"Var({pos},{dim},{cat})"
-                    eta = f"Var({nargs+1},{dim},{cat})"
+                    eta = f"Var({nargs},{dim},{cat})"
                     formula_d = f"Diff_WithSavedForward({formula},{var},{eta},{resvar})"
-                    aliases_d = aliases + [resvar] + [eta]
-                    args_d = (*args, result, grad_input)
+                    aliases_d = aliases + [eta] + [resvar]
+                    args_d = (*args, grad_input, result)
                     genconv = GenredAutograd_fun
 
                     params_d = copy.copy(params)
@@ -365,15 +366,14 @@ else:
                         and pos == params.rec_multVar_highdim
                     ):
                         params_d.rec_multVar_highdim = (
-                            nargs + 1  # nargs+1 is the position of variable eta.
+                            nargs  # nargs is the position of variable eta.
                         )
                     else:
                         params_d.rec_multVar_highdim = None
 
                     diff = genconv(params_d, *args_d)
-                    out = (
-                        out + diff
-                    )  # N.B. in-place "out+=diff" produces a bug, should investigate...
+
+                    out = out + diff
 
             return out, None
 