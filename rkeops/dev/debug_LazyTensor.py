89daaf2212a06d245aec6fdac608a30a4e6d5faa
Ghislain Durif <gd.dev@libertymail.net>
2023-11-02 16:04:48 +0100

    additional tests in Python

--- rkeops/dev/debug_LazyTensor.py
+++ rkeops/dev/debug_LazyTensor.py
@@ -75,3 +75,71 @@ expected_res = np.log(
 
 np.sum(np.square(res.flatten() - expected_res))
 
+## LogSumExpWeight
+formula = "Sum(V0-V1)"
+variables = ["V0=Vi(3)", "V1=Vj(3)", "V2=Vj(3)"]
+
+# Compile corresponding operator
+op = Genred(formula, variables, reduction_op="LogSumExpWeight", axis=0, formula2 = "V2")
+
+# data
+x = np.random.randn(4, 3)
+y = np.random.randn(5, 3)
+w = np.ones([5,3])
+
+# Perform reduction
+res = op(x, y, w, backend="CPU")
+res.shape
+
+expected_res = np.log(
+    np.sum(
+        np.exp(np.sum(x.T[:,:,np.newaxis] - y.T[:,np.newaxis,:], axis = 0)), 
+        axis = 0
+    )
+)
+
+np.sum(np.square(res.flatten() - expected_res))
+
+## SumSoftMaxWeight
+formula = "Sum(V0-V1)"
+variables = ["V0=Vi(3)", "V1=Vj(3)", "V2=Vj(3)"]
+
+# Compile corresponding operator
+op = Genred(formula, variables, reduction_op="SumSoftMaxWeight", axis=1, formula2 = "V2")
+
+# data
+x = np.random.randn(4, 3)
+y = np.random.randn(5, 3)
+w = np.ones([5,3])
+
+# Perform reduction
+res = op(x, y, w, backend="CPU")
+res.shape
+
+tmp = np.sum((x[:, None, :] - y[None, :, :]), axis=2)
+tmp -= np.max(tmp, axis=1)[:, None]  # Subtract the max to prevent numeric overflows
+tmp = np.exp(tmp) @ w / np.sum(np.exp(tmp), axis=1)[:, None]
+
+expected_res = tmp
+
+np.sum(np.square(res - expected_res))
+
+# data
+x = np.random.randn(4, 3)
+y = np.random.randn(5, 3)
+w = np.random.randn(5, 3)
+
+# Perform reduction
+res = op(x, y, w, backend="CPU")
+res.shape
+
+tmp = np.sum((x[:, None, :] - y[None, :, :]), axis=2)
+tmp -= np.max(tmp, axis=1)[:, None]  # Subtract the max to prevent numeric overflows
+tmp = np.exp(tmp) @ w / np.sum(np.exp(tmp), axis=1)[:, None]
+
+expected_res = tmp
+
+np.sum(np.square(res - expected_res))
+
+
+
