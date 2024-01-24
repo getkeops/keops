06dda273df128440b7bc4101c85b3649de10d8a8
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2024-01-24 18:42:50 +0100

    Add an example without lazyTensor

--- pykeops/pykeops/sandbox/issue_353.py
+++ pykeops/pykeops/sandbox/issue_353.py
@@ -1,22 +1,38 @@
 import torch.cuda
-from pykeops.torch import Vi, Vj, LazyTensor
+from pykeops.torch import Vi, Vj, LazyTensor, Genred
 import pykeops
 
-def keops_knn(x, y):
-    xi = LazyTensor(x[:, None, :])
-    xj = LazyTensor(y[None, :, :])
-    dij = ((xi - xj) ** 4).sum(-1)
-    knn_func = dij.sum_reduction(dim=0)
-    return knn_func
+
 
 d = 1
 n, m = 1, 1
 q_points = torch.rand((n, d)).requires_grad_(True).to("cuda")
 s_points = torch.rand((m, d)).to("cuda")
 
+# LazyTensor
+def keops_knn(x, y):
+    xi = LazyTensor(x[:, None, :])
+    xj = LazyTensor(y[None, :, :])
+    dij = ((xi - xj) ** 2).sum(-1)
+    knn_func = dij.sum_reduction(dim=0)
+    return knn_func
+
+# Genred
+formula = "Sum((a-b)**2)"
+aliases = [
+    f"a=Vi({d})",
+    f"b=Vj({d})",
+]
+fn = Genred(formula, aliases, reduction_op="Sum", axis=1)
+
+
+
 s = [0]
 for i in range(100):
-    result = keops_knn(q_points, s_points)
+
+    # out_ = keops_knn(q_points, s_points)
+    out = fn(q_points, s_points, backend = "GPU_1D")
+
     s.append(torch.cuda.memory.memory_stats()["allocated_bytes.all.current"])
     print("Reserved mem: ", torch.cuda.memory.memory_stats()["reserved_bytes.all.current"], ", Allocated mem: ", s[-1], "diff: ", s[-1] - s[-2])
     torch.cuda.synchronize()
