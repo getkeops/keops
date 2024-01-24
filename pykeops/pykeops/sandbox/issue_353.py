bae183054f2a09c6e9d8d8226ce081eb53414a1a
Joan Alexis Glaunès <joan.glaunes@gmail.com>
2024-01-24 20:20:43 +0000

    linting

--- pykeops/pykeops/sandbox/issue_353.py
+++ pykeops/pykeops/sandbox/issue_353.py
@@ -3,11 +3,11 @@ from pykeops.torch import Vi, Vj, LazyTensor, Genred
 import pykeops
 
 
-
 d = 1
 n, m = 1, 1
 q_points = torch.rand((n, d)).requires_grad_(True).to("cuda")
 s_points = torch.rand((m, d)).to("cuda")
+
 
 # LazyTensor
 def keops_knn(x, y):
@@ -16,6 +16,7 @@ def keops_knn(x, y):
     dij = ((xi - xj) ** 2).sum(-1)
     knn_func = dij.sum_reduction(dim=0)
     return knn_func
+
 
 # Genred
 formula = "Sum((a-b)**2)"
@@ -26,16 +27,18 @@ aliases = [
 fn = Genred(formula, aliases, reduction_op="Sum", axis=1)
 
 
-
 s = [0]
 for i in range(100):
-
     # out_ = keops_knn(q_points, s_points)
-    out = fn(q_points, s_points, backend = "GPU_1D")
+    out = fn(q_points, s_points, backend="GPU_1D")
 
     s.append(torch.cuda.memory.memory_stats()["allocated_bytes.all.current"])
-    print("Reserved mem: ", torch.cuda.memory.memory_stats()["reserved_bytes.all.current"], ", Allocated mem: ", s[-1], "diff: ", s[-1] - s[-2])
+    print(
+        "Reserved mem: ",
+        torch.cuda.memory.memory_stats()["reserved_bytes.all.current"],
+        ", Allocated mem: ",
+        s[-1],
+        "diff: ",
+        s[-1] - s[-2],
+    )
     torch.cuda.synchronize()
-
-
-
