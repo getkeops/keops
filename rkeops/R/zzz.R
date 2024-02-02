0a2023294c64778beb8469a76e47dfa9ebe19095
Ghislain Durif <gd.dev@libertymail.net>
2024-02-02 14:44:25 +0100

    document global reference to pykeops module

--- rkeops/R/zzz.R
+++ rkeops/R/zzz.R
@@ -7,14 +7,22 @@
     )
 }
 
-# global reference to pykeops (will be initialized in .onLoad)
+#' Glbal reference to main PyKeOps module
+#' @keywords internal
+#' @description
+#' The `pykeops` module object is an internal reference to the PyKeOps
+#' Python package that is used under the hood by `rkeops`. 
+#'
+#' @return the `pykeops` Python module
+#' @usage NULL
+#' @format An object of class `python.builtin.module`
 pykeops <- NULL
 
 .onLoad <- function(libname, pkgname) {
     # check os
     check_os(onLoad=TRUE)
-    # use superassignment to update global reference to pykeops
-    pykeops <<- reticulate::import("pykeops", delay_load = TRUE)
     # set up rkeops global options
     set_rkeops_options()
+    # use superassignment to update global reference to pykeops
+    pykeops <<- reticulate::import("pykeops", delay_load = TRUE)
 }
