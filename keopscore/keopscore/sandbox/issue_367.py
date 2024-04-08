731437c7ff32d5d32bba8d429e87da22a1fef35e
Joan Alexis Glaunès <joan.glaunes@gmail.com>
2024-04-08 21:12:32 +0000

    restart of global_var branch

--- keopscore/keopscore/sandbox/issue_367.py
+++ keopscore/keopscore/sandbox/issue_367.py
@@ -1,5 +1,6 @@
 import torch
 from pykeops.torch import LazyTensor
+from time import time
 
 
 def fun_torch(A, I, J):
@@ -9,28 +10,47 @@ def fun_torch(A, I, J):
 def fun_keops(A, I, J):
     ncol = A.shape[1]
     A = LazyTensor(A.flatten())
-    I = LazyTensor((I + 0.0)[..., None])
-    J = LazyTensor((J + 0.0)[..., None])
+    I = LazyTensor(I.to(dtype)[..., None])
+    J = LazyTensor(J.to(dtype)[..., None])
     K = A[I * ncol + J]
     return K.sum(axis=1).flatten()
 
 
 P, Q = 12, 5
-M, N = 3000, 2000
+M, N = 1000, 1000
 device = "cuda" if torch.cuda.is_available() else "cpu"
-A = torch.randn((P, Q), requires_grad=True, device=device)
+dtype = torch.float32
+A = torch.randn((P, Q), requires_grad=True, device=device, dtype=dtype)
 I = torch.randint(P, (M, 1), device=device)
 J = torch.randint(Q, (1, N), device=device)
 
-res_torch = fun_torch(A, I, J)
-print(res_torch)
+test_torch = True
 
+if test_torch:
+    start = time()
+    res_torch = fun_torch(A, I, J)
+    end = time()
+    print("time for torch:", end - start)
+    # print(res_torch)
+
+start = time()
 res_keops = fun_keops(A, I, J)
-print(res_keops)
+end = time()
+print("time for keops:", end - start)
+# print(res_keops)
+
+if test_torch:
+    print(torch.norm(res_keops - res_torch) / torch.norm(res_torch))
+
 
 # testing gradients
-loss_torch = (res_torch**2).sum()
-print(torch.autograd.grad(loss_torch, [A]))
+if test_torch:
+    loss_torch = (res_torch**2).sum()
+    res_torch = torch.autograd.grad(loss_torch, [A])[0]
 
 loss_keops = (res_keops**2).sum()
-print(torch.autograd.grad(loss_keops, [A]))
+res_keops = torch.autograd.grad(loss_keops, [A])[0]
+
+if test_torch:
+    print(torch.norm(res_keops - res_torch) / torch.norm(res_torch))
+
