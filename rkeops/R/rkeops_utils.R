3fa8e5148a1ffa9b5150db8fb3d61ce9cc4f083d
Ghislain Durif <gd.dev@libertymail.net>
2024-04-11 16:59:11 +0200

    new dedicated file for functions to get cache dir usage and clean it

---
+++ rkeops/R/rkeops_utils.R
@@ -0,0 +1,135 @@
+#' Disk usage of RKeOps cache directory content
+#' 
+#' @description
+#' Get the disk usage in bytes for all files in the folder where rkeops operator 
+#' compilation byproducts are stored to be re-used for further use 
+#' (and avoid unnecessary recompilation).
+#' 
+#' @details
+#' When compiling a user-defined operators or running a LazyTensor-based 
+#' operations, a shared object (`.so`) library (or dynamic link library, 
+#' `.dll`) file is created in RKeOps cache directory. 
+#' 
+#' For every new operators or new LazyTensor operations, such a file is 
+#' created, to be reused next time the same computations is done (then saving
+#' compilation time).
+#' 
+#' Calling `stat_rkeops_cache_dir()` gives RKeOps disk usage in bytes.
+#' cache directory.
+#' 
+#' You can use [rkeops::get_rkeops_cache_dir()] to get the path to RKeOps cache 
+#' directory, and you can use [rkeops::clean_rkeops()] to (fully or partially) 
+#' delete its content.
+#' 
+#' @param verbose logical, indicates whether a message about rkeops cache
+#' directory disk usage should be printed or not.
+#' @param startup logical, internal use for verbosity at package loading.
+#'
+#' @return string character, disk usage in bytes of the rkeops cache
+#' directory.
+#' 
+#' @author Ghislain Durif
+#' 
+#' @seealso [rkeops::get_rkeops_build_dir()], [rkeops::clean_rkeops()]
+#' 
+#' @importFrom stringr str_c
+#' @importFrom checkmate expect_flag
+#' 
+#' @examples
+#' \dontrun{
+#' stat_rkeops_cache_dir()
+#' }
+#' @export
+stat_rkeops_cache_dir <- function(verbose = TRUE, startup = FALSE) {
+    expect_flag(verbose)
+    expect_flag(startup)
+    cache_dir <- get_rkeops_cache_dir()
+    dir_du <- stat_dir(cache_dir)
+    if(verbose) {
+        msg <- str_c(
+            str_c("- rkeops cache directory:", cache_dir, sep = " "),
+            str_c("- disk usage:", dir_du, sep = " "),
+            str_c(
+                "You can use 'clean_rkeops()'",
+                "to clean it and reduce its disk usage.", sep = " "
+            ),
+            sep = "\n"
+        )
+        msg_warn_error(msg, type = "msg", startup = startup)
+    }
+    return(dir_du)
+}
+
+#' Clean RKeOps build directory
+#' 
+#' @description
+#' Remove all dynamic library files generated from compilations of user-defined 
+#' operators.
+#' 
+#' @details
+#' When compiling a user-defined operators, a shared object (`.so`) library 
+#' (or dynamic link library, `.dll`) file is created in RKeOps build 
+#' directory (located in the `.cache` folder in your home). For every 
+#' new operators, such a file is created.
+#' 
+#' Calling `clean_rkeops()` allows you to empty RKeOps build directory.
+#' 
+#' You can [rkeops::get_rkeops_build_dir()] to get the path to RKeOps 
+#' build directory, and you can use [rkeops::stat_rkeops_cache_dir()] to 
+#' verify its disk usage.
+#' 
+#' @author Ghislain Durif
+#' 
+#' @param verbose logical, if `TRUE` (default), give user information about 
+#' cleaning.
+#' @param all logical, if `TRUE` (default), all cached files are removed, 
+#' otherwise only out-dated files are removed.
+#' 
+#' @seealso [rkeops::get_rkeops_build_dir()], [rkeops::stat_rkeops_build_dir()]
+#' 
+#' @return None
+#' 
+#' @importFrom stringr str_c
+#' @importFrom checkmate expect_flag
+#' @importFrom fs dir_ls file_delete
+#' 
+#' @examples
+#' \dontrun{
+#' clean_rkeops()
+#' }
+#' @export
+clean_rkeops <- function(verbose = TRUE, all = TRUE) {
+    # check input
+    expect_flag(verbose)
+    expect_flag(all)
+    # get cache directory
+    cache_dir <- get_rkeops_cache_dir()
+    # list cache dir content
+    dir_list <- fs::dir_ls(cache_dir)
+    # remove all or not?
+    if(!all) {
+        # remove all but latest build dir
+        latest_build_dir <- def_pykeops_build_dir()
+        dir_list <- setdiff(dir_list, latest_build_dir)
+    }
+    # remove
+    if(length(dir_list) > 0) {
+        fs::file_delete(dir_list)
+    }
+    # verbosity
+    msg <- NULL
+    if(all) {
+        msg <- str_c(
+            "rkeops cache directory '", cache_dir, "' has been cleaned.\n",
+            "You should restard your R session and reload rkeops after cleaning."
+        )
+    } else {
+        msg <- str_c(
+            "rkeops cache directory '", cache_dir, 
+            "' has been partially cleaned ", 
+            "(only out-dated contents have been removed)."
+        )
+    }
+        
+    if(verbose) msg_warn_error(msg, type = "msg", startup = FALSE)
+}
---
+++ rkeops/tests/testthat/test-03_rkeops_utils.R
@@ -0,0 +1,86 @@
+test_that("stat_rkeops_cache_dir", {
+    
+    withr::with_options(list(rkeops = NULL), {
+        set_rkeops_options()
+
+        withr::with_tempdir({
+            # get temp dir
+            tmp_dir <- getwd()
+            # set cache dir
+            set_rkeops_cache_dir(tmp_dir, verbose = FALSE)
+            # cache dir du
+            res <- stat_rkeops_cache_dir(verbose = FALSE)
+            expect_equal(res, "0")
+            # create a file
+            tmp_data <- rnorm(1000)
+            save(tmp_data, file = file.path(tmp_dir, "file.RData"))
+            
+            # cache dir du
+            res <- stat_rkeops_cache_dir(verbose = FALSE)
+            expect_equal(
+                as.integer(fs::as_fs_bytes(res)), 
+                file.size(file.path(tmp_dir, "file.RData")),
+                tolerance = 10
+            )
+        })
+    })
+})
+
+test_that("clean_rkeops", {
+    
+    # full clean
+    withr::with_options(list(rkeops = NULL), {
+        set_rkeops_options()
+    
+        withr::with_tempdir({
+            # temp cache dir
+            tmp_cache_dir <- getwd()
+            set_rkeops_cache_dir(tmp_cache_dir, verbose = FALSE)
+            # create a file
+            tmp_data <- rnorm(1000)
+            save(tmp_data, file = file.path(tmp_cache_dir, "file.RData"))
+            
+            # cache dir du
+            res <- stat_rkeops_cache_dir(verbose = FALSE)
+            expect_true(as.integer(fs::as_fs_bytes(res)) > 0)
+            
+            # clean cache dir
+            clean_rkeops(verbose = FALSE, all = TRUE)
+            
+            # check
+            file_list <- fs::dir_ls(get_rkeops_cache_dir())
+            expect_true(length(file_list) == 0)
+        })
+    })
+    
+    # partial clean
+    withr::with_options(list(rkeops = NULL), {
+        set_rkeops_options()
+        
+        withr::with_tempdir({
+            # temp cache dir
+            tmp_cache_dir <- getwd()
+            set_rkeops_cache_dir(tmp_cache_dir, verbose = FALSE)
+            
+            # create a file
+            tmp_data <- rnorm(1000)
+            save(tmp_data, file = file.path(tmp_cache_dir, "file.RData"))
+            
+            # create default build dir
+            build_dir <- def_pykeops_build_dir()
+            
+            # cache dir du
+            res <- stat_rkeops_cache_dir(verbose = FALSE)
+            expect_true(as.integer(fs::as_fs_bytes(res)) > 0)
+            
+            # clean cache dir
+            clean_rkeops(verbose = FALSE, all = FALSE)
+            
+            # check
+            file_list <- fs::dir_ls(get_rkeops_cache_dir())
+            expect_true(length(file_list) == 1)
+            expect_equal(as.character(file_list), build_dir)
+            
+        })
+    })
+})
