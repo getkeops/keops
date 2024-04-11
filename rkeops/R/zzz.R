95cc99a7c9974647b348c2c98e7cb5d73af36f24
Ghislain Durif <gd.dev@libertymail.net>
2024-04-11 17:33:48 +0200

    add startup message about cache dir disk usage + setup pykeops env variables at startup

--- rkeops/R/zzz.R
+++ rkeops/R/zzz.R
@@ -5,9 +5,11 @@
             "\nYou are using rkeops version", packageVersion("rkeops"), "\n"
         )
     )
+    # check cache size
+    tmp <- stat_rkeops_cache_dir(verbose = TRUE, startup = TRUE)
 }
 
-#' Glbal reference to main PyKeOps module
+#' Global reference to main PyKeOps module
 #' @keywords internal
 #' @description
 #' The `pykeops` module object is an internal reference to the PyKeOps
@@ -20,9 +22,18 @@ pykeops <- NULL
 
 .onLoad <- function(libname, pkgname) {
     # check os
-    check_os(onLoad=TRUE)
+    check_os(startup = TRUE)
     # set up rkeops global options
     set_rkeops_options()
+    # disable pykeops import verbosity
+    Sys.setenv("PYKEOPS_VERBOSE" = "0")
+    if(reticulate::py_available()) 
+        import("os")$environ$update(list("PYKEOPS_VERBOSE" = "0"))
+    # set keops cache dir
+    Sys.setenv("KEOPS_CACHE_FOLDER" = get_rkeops_cache_dir())
+    if(reticulate::py_available()) 
+        import("os")$environ$update(
+            list("KEOPS_CACHE_FOLDER" = get_rkeops_cache_dir()))
     # use superassignment to update global reference to pykeops
     pykeops <<- reticulate::import("pykeops", delay_load = TRUE)
 }
