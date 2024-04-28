b9e10a19f13906128ec9a4e56737bbd2db686726
Joan Alexis Glaunès <joan.glaunes@gmail.com>
2024-04-28 14:40:47 +0000

    WIP

---
+++ keopscore/keopscore/sandbox/auto_factorize_simple.py
@@ -0,0 +1,17 @@
+# Testing auto factorize
+
+from keopscore.formulas import *
+
+D = 3
+x = Var(0, D, 0, "x")
+y = Var(1, D, 1, "y")
+
+Dv = 3
+b = Var(2, Dv, 1, "b")
+f = SqNorm2(Exp(-SqDist(x, y)) * b)+b
+
+g = AutoFactorize(f)
+
+print("f =", f)
+print("g =", g)
+print()
