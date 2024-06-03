966ea8a728a1fe1a6033da9e375a43b67bf989ef
Ghislain Durif <gd.dev@libertymail.net>
2024-06-03 16:47:25 +0200

    test new cleaning strategy (remove cache dir entirely) + minor modification regarding cache dir usage during tests

--- rkeops/tests/testthat/test-04_keops_setup.R
+++ rkeops/tests/testthat/test-04_keops_setup.R
@@ -2,8 +2,12 @@ test_that("def_pykeops_build_dir", {
     skip_if_no_python()
     skip_if_no_pykeops()
     
-    res <- def_pykeops_build_dir()
-    checkmate::expect_directory(res)
+    withr::with_options(list(rkeops = NULL), {
+        set_rkeops_options(list(cache_dir = testing_cache_dir))
+    
+        res <- def_pykeops_build_dir()
+        checkmate::expect_directory(res)
+    })
 })
 
 test_that("set_pykeops_verbosity", {
@@ -11,6 +15,8 @@ test_that("set_pykeops_verbosity", {
     skip_if_no_pykeops()
     
     withr::with_options(list(rkeops = NULL), {
+        set_rkeops_options(list(cache_dir = testing_cache_dir))
+        
         # enable verbosity
         rkeops_enable_verbosity()
         set_pykeops_verbosity()
@@ -30,7 +36,8 @@ test_that("set_pykeops_build_dir", {
     skip_if_no_pykeops()
     
     withr::with_options(list(rkeops = NULL), {
-        set_rkeops_options()
+        set_rkeops_options(list(cache_dir = testing_cache_dir))
+        
         set_pykeops_build_dir()
         expect_equal(def_pykeops_build_dir(), pykeops$get_build_folder())
         
@@ -43,11 +50,10 @@ test_that("setup_pykeops", {
     skip_if_no_pykeops()
     
     withr::with_options(list(rkeops = NULL), {
-        set_rkeops_options()
+        set_rkeops_options(list(cache_dir = testing_cache_dir))
         setup_pykeops()
         expect_equal(def_pykeops_build_dir(), pykeops$get_build_folder())
         expect_equal(
             as.logical(get_rkeops_options("verbosity")), pykeops$verbose)
-        
     })
 })
