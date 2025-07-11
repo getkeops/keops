a03bc72e238f114cc010217994e2d46cfaa1ebfd
Ghislain Durif <gd.dev@libertymail.net>
2025-07-11 10:44:10 +0200

    remove useless helloWorld function

--- rkeops/R/starting.R
+++
@@ -1,9 +0,0 @@
-#' @title helloWorld
-#' @keywords internal
-#' @name helloWorld
-#' @description dummy function to init package
-#' @return None
-#' @export
-helloWorld <- function() {
-    print("Hello user of KeOps")
-}
