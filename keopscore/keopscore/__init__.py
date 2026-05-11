f9c042adc87a9569cbde3c221da324f2808b8719
Benjamin Charlier <benjamin.charlier@inrae.fr>
2026-05-11 12:37:42 +0200

    protect keopcore compilation with a try

--- keopscore/keopscore/__init__.py
+++ keopscore/keopscore/__init__.py
@@ -9,7 +9,15 @@ with open(os.path.join(here, "keops_version"), encoding="utf-8") as v:
 import keopscore.config
 
 # Create build folder if it does not exist and set it as the default build folder
-keopscore.config.path.set_build_folder(reset_all=False)
+try:
+    keopscore.config.path.set_build_folder(reset_all=False)
+except Exception as e:
+    from .utils.messages import KeOps_Warning
+
+    KeOps_Warning(
+        f"An error occurred while setting up keopcore: {e}. Use keopscore.config.check_health() to get details on the current configuration.",
+        level=1,
+    )
 
 # expose to the user
 set_build_folder = keopscore.config.path.set_build_folder
